e68d03c65931cd1d45a628d073ce085fb790ae8d
Feng Tang <feng.tang@intel.com>
2011-07-15 14:39:22 +0100

    vrtc: change its year offset from 1960 to 1972
    Real world year equals the value in vrtc YEAR register plus an offset.
    We used 1960 as the offset to make leap year consistent, but for a
    device's first use, its YEAR register is 0 and the system year will
    be parsed as 1960 which is not a valid UNIX time and will cause many
    applications to fail mysteriously. So use 1972 instead to fix this
    issue.
    
    Updated patch which adds a sanity check suggested by Mathias
    
    Change-Id: I521271ac79b5657e9b9eb4a81fe6b2e5f5e94e0b
    Signed-off-by: Feng Tang <feng.tang@intel.com>
    Signed-off-by: Alan Cox <alan@linux.intel.com>

--- arch/x86/platform/mrst/vrtc.c
+++ arch/x86/platform/mrst/vrtc.c
@@ -70,8 +70,8 @@ unsigned long vrtc_get_time(void)
 	mon = vrtc_cmos_read(RTC_MONTH);
 	year = vrtc_cmos_read(RTC_YEAR);
 
-	/* vRTC YEAR reg contains the offset to 1960 */
-	year += 1960;
+	/* vRTC YEAR reg contains the offset to 1972 */
+	year += 1972;
 
 	printk(KERN_INFO "vRTC: sec: %d min: %d hour: %d day: %d "
 		"mon: %d year: %d\n", sec, min, hour, mday, mon, year);
