d127caebacb409b6eb36e4c6935a872698fb3f0c
Daniel Drake <dsd@laptop.org>
2011-07-17 16:38:41 +0100

    mmc: print debug messages for runtime PM actions
    At http://www.mail-archive.com/linux-mmc@vger.kernel.org/msg08371.html
    (thread: "mmc: sdio: reset card during power_restore") we found and
    fixed a bug where mmc's runtime power management functions were not being
    called. We have now also made improvements to the SDIO powerup routine
    which could possibly mask this kind of issue in future.
    
    Add debug messages to the runtime PM hooks so that it is easy to verify
    if and when runtime PM is happening.
    
    Change-Id: I563bcd5b7f9902b1ec54ad6d1d44af58c416b039
    Signed-off-by: Daniel Drake <dsd@laptop.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1905,6 +1905,10 @@ int mmc_power_save_host(struct mmc_host *host)
 {
 	int ret = 0;
 
+#ifdef CONFIG_MMC_DEBUG
+	pr_info("%s: %s: powering down\n", mmc_hostname(host), __func__);
+#endif
+
 	mmc_bus_get(host);
 
 	if (!host->bus_ops || host->bus_dead || !host->bus_ops->power_restore) {
@@ -1926,6 +1930,10 @@ EXPORT_SYMBOL(mmc_power_save_host);
 int mmc_power_restore_host(struct mmc_host *host)
 {
 	int ret;
+
+#ifdef CONFIG_MMC_DEBUG
+	pr_info("%s: %s: powering up\n", mmc_hostname(host), __func__);
+#endif
 
 	mmc_bus_get(host);
 
