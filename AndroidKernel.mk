93c3d10d12ff4fec3e64ac39fce4528431f45188
German Monroy <german.monroy@intel.com>
2011-09-29 17:39:30 -0700

    merge of reated changes 	kernel build: add Android makefile
    	So that this kernel can be compiled together with the rest of Android
    	(i.e. build automatically creates boot.bin)
    
    	Change-Id: I7f59d0d17ed267e101f0cee2f75a5893de5cfbcb
    
    	kbuild: add a make defoldconfig make option
    
    	The oldconfig option is asking user to choose when new config options are defined in KConfig files, and not in .config
    	This prevents automatic builds.
    
    	This make target will use default values for all options not specified in .config
    
    	xxChange - Id: I713a5cc248130056441060dccfcb492ad0d8acb5
    	Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>
    
    Change-Id: I5f12ca9bc66ba7ac54bc205319c23d2875c0ea6f

---
+++ AndroidKernel.mk
@@ -0,0 +1,22 @@
+# This makefile is included from vendor/intel/*/AndroidBoard.mk.
+# If the kernel source is present, AndroidBoard.mk will perform a kernel build
+# otherise, AndroidBoard.mk will find the kernel binaries in a tarball.
+
+# This can be overridden on the make command line.
+TARGET_KERNEL_SOURCE_IS_PRESENT ?= true
+
+.PHONY: get_kernel_from_source menuconfig
+menuconfig get_kernel_from_source:
+	TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" DIFFCONFIGS="$(DIFFCONFIGS)" vendor/intel/support/kernel-build.sh -c $(CUSTOM_BOARD) -o $@
+
+# This rule is useful for creating a kernel that will be
+# shared with a tree that does not have kernel source.
+make_kernel_tarball: get_kernel_from_source bootimage
+	@echo Building kernel tarball: $(TARGET_KERNEL_TARBALL)
+	@rm -rf $(PRODUCT_OUT)/kerneltarball
+	@mkdir -p $(PRODUCT_OUT)/kerneltarball/root/lib/modules
+	@cp $(PRODUCT_OUT)/root/lib/modules/* $(PRODUCT_OUT)/kerneltarball/lib/modules
+	@cp $(PRODUCT_OUT)/bzImage $(PRODUCT_OUT)/kerneltarball/
+	tar cvzf $(TARGET_KERNEL_TARBALL) -C $(PRODUCT_OUT)/kerneltarball bzImage root/lib/modules
+
+$(PRODUCT_OUT)/bzImage: get_kernel_from_source
