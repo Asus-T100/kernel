6873e432a18a64febd5a38236bdb3bef1d6c4389
Fei Yang <fei.yang@intel.com>
2011-10-04 09:46:59 -0700

    PMU: register choose_state fucntion for MID PCI platform ops with pmu_pci_choose_state() implemented in the pmu driver.
    The following limitations apply to Medfield SoC,
    LSS_04: SECURITY supports only D0i2
    LSS_06: USB OTG supports only D0i1
    LSS_07: USB HOST supports only D0i1
    LSS_41: HSU supports only D0i1
    
    Change-Id: I9ddcd198a3eed09efd7be307e8bc6fefa96e3a59
    Signed-off-by: Fei Yang <fei.yang@intel.com>

--- arch/x86/include/asm/mrst.h
+++ arch/x86/include/asm/mrst.h
@@ -82,6 +82,10 @@ extern void mrst_rtc_init(void);
 extern int mrst_pmu_pci_set_power_state(struct pci_dev *pdev, pci_power_t state);
 extern int mfld_pmu_pci_set_power_state(struct pci_dev *pdev, pci_power_t state);
 
+#ifdef CONFIG_X86_MDFLD
+extern pci_power_t pmu_pci_choose_state(struct pci_dev *pdev);
+#endif /* !CONFIG_X86_MDFLD */
+
 #ifdef CONFIG_X86_MRST
 extern int mrst_pmu_s0i3_entry(void);
 extern void mrst_pmu_disable_msi(void);
