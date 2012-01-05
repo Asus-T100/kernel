7f8b6a42c2a0358a25c9be20e1aaf10c001dd086
Li Zeng <li.zeng@intel.com>
2012-01-05 14:56:35 +0800

    [PORT FROM R2]ved/vec: reduce ttm fence timeout length
    BZ: 19044
    
    When ved got invalid/wrong bitstream and no WDT enabled by ved, driver has to wait ttm fence timeout. Current length of timeout is 3s, reduce it ot 1s to improve respond time.
    
    Change-Id: I1b6b0face2699215fc7ede32cafde4c665ee7999
    Signed-off-by: Li Zeng <li.zeng@intel.com>
    Reviewed-on: http://android.intel.com:8080/32572
    Reviewed-by: Ding, Haitao <haitao.ding@intel.com>
    Tested-by: Ding, Haitao <haitao.ding@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/staging/mrst/imgv/psb_fence.c
+++ drivers/staging/mrst/imgv/psb_fence.c
@@ -111,7 +111,7 @@ int psb_fence_emit_sequence(struct ttm_fence_device *fdev,
 	}
 
 	*sequence = seq;
-	*timeout_jiffies = jiffies + DRM_HZ * 3;
+	*timeout_jiffies = jiffies + DRM_HZ;
 
 	return 0;
 }
