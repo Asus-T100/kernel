4fd5d88cd382d16ab9f07277d217fa431fd4c685
Surya P Sahukar <surya.p.sahukar@intel.com>
2012-01-12 13:32:30 -0800

    intel_mid_umip: Sysfs Interface for the USB Host Enable UMIP value
    BZ:13299
    
    Added functionality to expose the USB Host Enable UMIP value in
    sysfs. We can read the current value and modify the existing value
    by writing to the sysfs interface.
    
    Usage:
    
    cd /sys/devices/platform/intel_mid_umip
    
    To list the available timeouts
    ===========
    cat available_timeouts
    
    To list the current programmed timeout
    ===================
    cat current_timeout
    
    To change the current programmed timeout
    ===================
    echo <timeout> >  current_timeout
    
    Change-Id: Ia8a43f85388beeb2b644bffd16d0374457af36e3
    Signed-off-by: Surya P Sahukar <surya.p.sahukar@intel.com>
    Signed-off-by: Hari K Kanigeri <harik.k.kanigeri@intel.com>
    Reviewed-on: http://android.intel.com:8080/31586
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Kuppuswamy, Sathyanarayanan <sathyanarayanan.kuppuswamy@intel.com>
    Reviewed-by: Dong, Chuanxiao <chuanxiao.dong@intel.com>
    Tested-by: Sun, Jianhua <jianhua.sun@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ drivers/platform/x86/intel_mid_umip.c
@@ -0,0 +1,188 @@
+/*
+ *  intel_mid_umip.c - Driver to modify the USB umip values
+ *
+ * Copyright (c) 2011, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <linux/string.h>
+#include <linux/sysfs.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <asm/intel_scu_ipc.h>
+#include <linux/platform_device.h>
+
+#define USB_HOST_ENABLE_TIMEOUT_INFINITE      0x3F
+#define USB_HOST_ENABLE_TIMEOUT_THREE         0x1B
+#define USB_HOST_ENABLE_TIMEOUT_ZERO          0X00
+#define USB_ENABLE_UMIP_OFFSET                0x400
+#define MAX_USB_TIMEOUT_LEN			14
+#define MAX_NUM_TIMEOUTS			3
+#define ERROR_MSG				"Could not read right value"
+
+struct usb_timeout {
+	char name[MAX_USB_TIMEOUT_LEN];
+	u8 umip_value;
+};
+
+static struct
+	usb_timeout available_usb_host_enable_timeouts[MAX_NUM_TIMEOUTS] = {
+	 {"infinite", USB_HOST_ENABLE_TIMEOUT_INFINITE},
+	 {"3seconds", USB_HOST_ENABLE_TIMEOUT_THREE},
+	 {"zeroseconds", USB_HOST_ENABLE_TIMEOUT_ZERO}
+};
+
+static struct platform_device *umip_mid_pdev;
+/*
+	 The "availabe usb host enable timeouts" file where a static variable
+	 is read from and written to.
+*/
+static ssize_t available_usb_host_enable_timeouts_show(struct device *dev,
+				struct device_attribute *attr, char *buffer)
+{
+	return sprintf(buffer, "%s\t%s\t%s\n",
+				available_usb_host_enable_timeouts[0].name,
+				available_usb_host_enable_timeouts[1].name,
+				available_usb_host_enable_timeouts[2].name);
+}
+
+/*
+	The "current usb host enable timeout" shows the current value of
+	the variable.
+*/
+static ssize_t usb_host_enable_timeout_show(struct device *dev,
+				struct device_attribute *attr, char *buffer)
+{
+	int ret_ipc;
+	u8 data_read;
+	int i = 0;
+	char timeout[MAX_USB_TIMEOUT_LEN];
+
+	ret_ipc = intel_scu_ipc_read_mip(&data_read,
+					1,
+					USB_ENABLE_UMIP_OFFSET,
+					0);
+	if (ret_ipc)
+		pr_err("Could not write to UMIP USB Host Enable\n");
+
+	for (i = 0; i < MAX_NUM_TIMEOUTS; i++) {
+		if (data_read ==
+			available_usb_host_enable_timeouts[i].umip_value) {
+			strcpy(timeout,
+				available_usb_host_enable_timeouts[i].name);
+			break;
+		}
+	}
+
+	if (i == MAX_NUM_TIMEOUTS)
+		return sprintf(buffer, "%s\n", ERROR_MSG);
+	else
+		return sprintf(buffer, "%s\n", &timeout);
+}
+
+ssize_t usb_host_enable_timeout_store(struct device *dev,
+					struct device_attribute *attr,
+					const char *buffer, size_t count)
+{
+	int ret_ipc, i, string_length;
+	char timeout[MAX_USB_TIMEOUT_LEN];
+
+	string_length = strlen(buffer);
+
+	if (string_length < MAX_USB_TIMEOUT_LEN) {
+		snprintf(timeout, string_length, "%s", buffer);
+	} else {
+		pr_err("Invalid value written."
+		"Check the Availabe values that can be used\n");
+		return count;
+	}
+
+	for (i = 0; i < MAX_NUM_TIMEOUTS; i++) {
+		if (strcmp(timeout, available_usb_host_enable_timeouts[i].name))
+			continue;
+
+		ret_ipc = intel_scu_ipc_write_umip(
+			&available_usb_host_enable_timeouts[i].umip_value,
+			1, USB_ENABLE_UMIP_OFFSET);
+		if (ret_ipc)
+			pr_err("Could not write to UMIP USB Host Enable\n");
+
+		break;
+	}
+
+	/* if i is equal to arr_len, that means there is no match */
+	if (i == MAX_NUM_TIMEOUTS) {
+		pr_err("Invalid value written."
+			"Check the Availabe values that can be used\n");
+	}
+
+	return count;
+}
+
+/* Attach the sysfs write method */
+DEVICE_ATTR(available_timeouts, S_IRUGO,
+		available_usb_host_enable_timeouts_show, NULL);
+DEVICE_ATTR(current_timeout, S_IRUGO|S_IWUSR,
+		 usb_host_enable_timeout_show, usb_host_enable_timeout_store);
+
+/* Attribute Descriptor */
+static struct attribute *umip_mid_attrs[] = {
+		&dev_attr_available_timeouts.attr,
+		&dev_attr_current_timeout.attr,
+		NULL
+};
+
+/* Attribute group */
+static struct attribute_group umip_attrs_group = {
+	.attrs = umip_mid_attrs,
+};
+
+static int __init intel_mid_umip_init(void)
+{
+	int retval;
+
+	/* Register a platform device */
+	umip_mid_pdev = platform_device_register_simple("intel_mid_umip", -1,
+								NULL, 0);
+	if (IS_ERR(umip_mid_pdev)) {
+		pr_err("UMIP_MID: platform_device_register_simple error\n");
+		return PTR_ERR(umip_mid_pdev);
+	}
+
+	/* Create a sysfs node to read simulated coordinates */
+	retval = sysfs_create_group(&umip_mid_pdev->dev.kobj,
+					&umip_attrs_group);
+	if (retval) {
+		pr_err("UMIP_MID: sysfs create group error\n");
+		platform_device_unregister(umip_mid_pdev);
+		umip_mid_pdev = NULL;
+		return retval;
+	}
+
+	return 0;
+}
+
+static void __exit intel_mid_umip_exit(void)
+{
+	sysfs_remove_group(&umip_mid_pdev->dev.kobj, &umip_attrs_group);
+	platform_device_unregister(umip_mid_pdev);
+}
+
+module_init(intel_mid_umip_init);
+module_exit(intel_mid_umip_exit);
+
+MODULE_DESCRIPTION("intel mid usb umip programming");
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Surya P Sahukar <surya.p.sahukar@intel.com");
