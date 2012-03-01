fec83b0974f25d92c4398965dccd56d52d3302b8
Mark F. Brown <mark.f.brown@intel.com>
2012-03-01 14:13:59 -0800

    merrifield: added board-vp support for SSP UART
    BZ: 25983
    
    There are special requirements for tangier simulation:
    DMA is disabled
    PCI capability ADID value (Bus number) is wrong in simulator
    
    SFI entry needs to be overridden
    
    Change-Id: I7a1c27d2898ec8bb962999c7f21b8f85ca1911e3
    Signed-off-by: Mark F. Brown <mark.f.brown@intel.com>
    Reviewed-on: http://android.intel.com:8080/37364
    Reviewed-by: Ng, Cheon-woei <cheon-woei.ng@intel.com>
    Tested-by: Ng, Cheon-woei <cheon-woei.ng@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- arch/x86/platform/intel-mid/board-vp.c
+++ arch/x86/platform/intel-mid/board-vp.c
@@ -10,8 +10,54 @@
  * of the License.
  */
 
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/spi/spi.h>
+#include <linux/spi/intel_mid_ssp_spi.h>
+
+/* SSP5 debug port settings */
+#define SPI_MODALIAS		"spi_max3111"
+#define SPI_MAX_SPEED_HZ	3125000
+#define SPI_IRQ			0xFF
+
+static struct intel_mid_ssp_spi_chip chip = {
+		.burst_size = DFLT_FIFO_BURST_SIZE,
+		.timeout = DFLT_TIMEOUT_VAL,
+		.dma_enabled = 0,
+};
+
+static void __init ssp_uart_init(void)
+{
+	struct spi_board_info spi_info;
+
+	memset(&spi_info, 0, sizeof(spi_info));
+	strncpy(spi_info.modalias, SPI_MODALIAS, sizeof(spi_info.modalias)-1);
+	spi_info.irq = SPI_IRQ;
+	spi_info.bus_num = 0;
+	spi_info.chip_select = 0;
+	spi_info.max_speed_hz = SPI_MAX_SPEED_HZ;
+	spi_info.controller_data = &chip;
+	pr_info("info: SPI bus = %d, name = %16.16s, "
+		"irq = 0x%2x, max_freq = %d, cs = %d\n",
+		spi_info.bus_num,
+		spi_info.modalias,
+		spi_info.irq,
+		spi_info.max_speed_hz,
+		spi_info.chip_select);
+
+	spi_info.mode = SPI_MODE_0;
+	spi_info.platform_data = NULL;
+	spi_register_board_info(&spi_info, 1);
+}
 /* not supported */
 int penwell_otg_query_charging_cap(void *dummy)
 {
 	return -1;
 }
+
+static void __init vp_board_init(void)
+{
+	ssp_uart_init();
+}
+
+arch_initcall(vp_board_init);
