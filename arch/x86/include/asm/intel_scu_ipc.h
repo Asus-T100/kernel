4980f2672554c9e1f7c2c55010e9aa8b92442321
Fei Yang <fei.yang@intel.com>
2012-03-19 18:00:58 -0700

    INTEL_SCU_IPC: forklift from UMG ICS kernel tree
    Resolved merge conflict due to pm_qos change. Also forklift the following
    patches from Linux-3.0.34 based kernel tree for Intel mobile platforms.
    
    intel_scu_ipc: calculate divisor properly for osc clk
    Critical Klocwork issues for KERNEL domain
    intel_scu_ipc: Support 19.2M Hz crystal for CLV+ boards
    intel_scu_ipc: remove the un-neccessary capability check
    intel_scu_ipc: calculate divisor for osc clk
    [PORT FROM R2] mid_pmu: remove sysfs interface (and create kernel interface)
    [PORT FROM R2] intel_scu_ipc: fix scu_ready_sem rlease
    [PORT FROM R2] mid_pmu: ipc and s0ix commands may race to hang system
    [PORT FROM R2] IPC Driver Patches
    	BZ: 17485
    	This BZ includes the following BZs:
    	10321, 11749, 8773, 9027, 9446, 9459, 10685, 14922, 16961, 16789
    [PORT FROM R2]Dump RESETIRQ1 and RESETIRQ2 at boot time
    intel_scu_ipc: add cloverview support
    [PORT FROM R2]Factor code for writing the reboot reason in OSNIB
    mrst.c: add for camera part
    SCU_IPC: Found the following patch is needed
    IPC: Fix merge problems.
    Add MIP header update when FW is upgraded
    intel_scu_ipc: Drop to C6s0i0 when there is IPC communication ongoing
    ipc: upgrade ifwi at reboot
    ipc: support to write umip unaligned
    ipc: workarround for SCU bug to retry umip/smip cmd
    ipc: enable IPC interrupt mode
    intel_scu_ipc_utils: Support for reflashing Medfield firmware
    
    Change-Id: I74e093af127dabbf342ac6526b825ac6bbc8a14d
    Signed-off-by: Fei Yang <fei.yang@intel.com>

--- arch/x86/include/asm/intel_scu_ipc.h
+++ arch/x86/include/asm/intel_scu_ipc.h
@@ -2,17 +2,50 @@
 #define  _ASM_X86_INTEL_SCU_IPC_H_
 
 #include <linux/notifier.h>
+#include <asm/intel-mid.h>
 
+/* IPC defines the following message types */
+#define IPCMSG_BATTERY          0xEF /* Coulomb Counter Accumulator */
+#define IPCMSG_MIP_ACCESS       0xEC /* IA MIP access */
 #define IPCMSG_WARM_RESET	0xF0
 #define IPCMSG_COLD_RESET	0xF1
 #define IPCMSG_SOFT_RESET	0xF2
 #define IPCMSG_COLD_BOOT	0xF3
-
+#define IPCMSG_FW_REVISION      0xF4 /* Get firmware revision */
+#define IPCMSG_WATCHDOG_TIMER   0xF8 /* Set Kernel Watchdog Threshold */
 #define IPCMSG_VRTC		0xFA	 /* Set vRTC device */
-	/* Command id associated with message IPCMSG_VRTC */
-	#define IPC_CMD_VRTC_SETTIME      1 /* Set time */
-	#define IPC_CMD_VRTC_SETALARM     2 /* Set alarm */
+#define IPCMSG_FW_UPDATE        0xFE /* Firmware update */
+#define IPCMSG_PCNTRL           0xFF /* Power controller unit read/write */
+#define IPCMSG_OSC_CLK		0xE6 /* Turn on/off osc clock */
 
+#define IPC_CMD_UMIP_RD     0
+#define IPC_CMD_UMIP_WR     1
+#define IPC_CMD_SMIP_RD     2
+
+/* Command id associated with message IPCMSG_PCNTRL */
+#define IPC_CMD_PCNTRL_W      0 /* Register write */
+#define IPC_CMD_PCNTRL_R      1 /* Register read */
+#define IPC_CMD_PCNTRL_M      2 /* Register read-modify-write */
+
+#define IPC_ERR_NONE			0
+#define IPC_ERR_CMD_NOT_SUPPORTED	1
+#define IPC_ERR_CMD_NOT_SERVICED	2
+#define IPC_ERR_UNABLE_TO_SERVICE	3
+#define IPC_ERR_CMD_INVALID		4
+#define IPC_ERR_CMD_FAILED		5
+#define IPC_ERR_EMSECURITY		6
+
+#define MSIC_DEBUG_FILE "msic"
+#define MSIC_ALL_DEBUG_FILE "msic_all"
+#define MAX_MSIC_REG   0x3FF
+#define MIN_MSIC_REG   0x0
+
+
+
+/* Command id associated with message IPCMSG_VRTC */
+#define IPC_CMD_VRTC_SETTIME      1 /* Set time */
+#define IPC_CMD_VRTC_SETALARM     2 /* Set alarm */
+#define IPC_CMD_VRTC_SYNC_RTC     3 /* Sync MSIC/PMIC RTC to VRTC */
 /* Read single register */
 int intel_scu_ipc_ioread8(u16 addr, u8 *data);
 
@@ -48,7 +81,28 @@ int intel_scu_ipc_command(int cmd, int sub, u32 *in, int inlen,
 int intel_scu_ipc_i2c_cntrl(u32 addr, u32 *data);
 
 /* Update FW version */
-int intel_scu_ipc_fw_update(u8 *buffer, u32 length);
+int intel_scu_ipc_mrstfw_update(u8 *buffer, u32 length);
+int intel_scu_ipc_medfw_upgrade(void);
+int intel_scu_ipc_medfw_prepare(void __user *arg);
+
+int intel_scu_ipc_read_mip(u8 *data, int len, int offset, int issigned);
+int intel_scu_ipc_write_umip(u8 *data, int len, int offset);
+
+/* OSHOB-OS Handoff Buffer read */
+int intel_scu_ipc_read_oshob(u8 *data, int len, int offset);
+/* OSNIB-OS No Init Buffer write */
+#define OSNIB_OFFSET		0x0C
+int intel_scu_ipc_write_osnib(u8 *data, int len, int offset, u32 mask);
+int intel_scu_ipc_write_osnib_rr(u8 rr);
+int intel_scu_ipc_read_osnib_rr(u8 *rr);
+
+/* Penwell has 4 osc clocks */
+#define OSC_CLK_AUDIO	0	/* Audio */
+#define OSC_CLK_CAM0	1	/* Primary camera */
+#define OSC_CLK_CAM1	2	/* Secondary camera */
+#define OSC_CLK_DISP	3	/* Display buffer */
+
+int intel_scu_ipc_osc_clk(u8 clk, unsigned int khz);
 
 extern struct blocking_notifier_head intel_scu_notifier;
 
@@ -69,5 +123,23 @@ static inline int intel_scu_notifier_post(unsigned long v, void *p)
 
 #define		SCU_AVAILABLE		1
 #define		SCU_DOWN		2
+
+#define MSIC_VPROG1_CTRL	0xD6
+#define MSIC_VPROG2_CTRL	0xD7
+#define MSIC_VPROG_ON		0xFF
+#define MSIC_VPROG_OFF		0
+
+/* Helpers to turn on/off msic vprog1 and vprog2 */
+static inline int intel_scu_ipc_msic_vprog1(int on)
+{
+	return intel_scu_ipc_iowrite8(MSIC_VPROG1_CTRL,
+			on ? MSIC_VPROG_ON : MSIC_VPROG_OFF);
+}
+
+static inline int intel_scu_ipc_msic_vprog2(int on)
+{
+	return intel_scu_ipc_iowrite8(MSIC_VPROG2_CTRL,
+			on ? MSIC_VPROG_ON : MSIC_VPROG_OFF);
+}
 
 #endif
--- drivers/platform/x86/intel_scu_ipcutil.c
+++ drivers/platform/x86/intel_scu_ipcutil.c
@@ -3,6 +3,8 @@
  *
  * (C) Copyright 2008-2010 Intel Corporation
  * Author: Sreedhara DS (sreedhara.ds@intel.com)
+ * (C) Copyright 2010 Intel Corporation
+ * Author: Sudha Krishnakumar (sudha.krishnakumar@intel.com)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,20 +24,54 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/init.h>
+#include <linux/miscdevice.h>
+#include <linux/io.h>
 #include <asm/intel_scu_ipc.h>
-
-static int major;
+#include <asm/intel-mid.h>
+#include <linux/pm_runtime.h>
 
 /* ioctl commnds */
-#define	INTE_SCU_IPC_REGISTER_READ	0
-#define INTE_SCU_IPC_REGISTER_WRITE	1
-#define INTE_SCU_IPC_REGISTER_UPDATE	2
+#define INTEL_SCU_IPC_REGISTER_READ	0
+#define INTEL_SCU_IPC_REGISTER_WRITE	1
+#define INTEL_SCU_IPC_REGISTER_UPDATE	2
+#define INTEL_SCU_IPC_FW_UPDATE		0xA2
+#define INTEL_SCU_IPC_MEDFIELD_FW_UPDATE	0xA3
+#define INTEL_SCU_IPC_FW_REVISION_GET	0xB0
+#define INTEL_SCU_IPC_READ_RR_FROM_OSNIB	0xC1
+#define INTEL_SCU_IPC_WRITE_RR_TO_OSNIB	0xC2
+#define INTEL_SCU_IPC_READ_VBATTCRIT	0xC4
+#define INTEL_SCU_IPC_WRITE_ALARM_FLAG_TO_OSNIB 0xC5
+
+#define MAX_FW_SIZE 264192
+
+#define OSHOB_PMIT_OFFSET		0x0000002c
+#define OSNIB_RR_OFFSET			OSNIB_OFFSET
+#define OSNIB_WD_OFFSET			(OSNIB_OFFSET + 1)
+#define OSNIB_ALARM_OFFSET		(OSNIB_OFFSET + 2)
+#define OSNIB_WAKESRC_OFFSET		(OSNIB_OFFSET + 3)
+#define OSNIB_RESETIRQ1_OFFSET		(OSNIB_OFFSET + 4)
+#define OSNIB_RESETIRQ2_OFFSET		(OSNIB_OFFSET + 5)
+#define OSNIB_RR_MASK			0x00000001
+#define OSNIB_WD_MASK			0x00000002
+#define OSNIB_ALARM_MASK		0x00000004
+#define OSNIB_WAKESRC_MASK		0x00000008
+#define OSNIB_RESETIRQ1_MASK		0x00000010
+#define OSNIB_RESETIRQ2_MASK		0x00000020
+#define PMIT_RESETIRQ1_OFFSET		14
+#define PMIT_RESETIRQ2_OFFSET		15
+
+#define DUMP_OSNIB
 
 struct scu_ipc_data {
 	u32     count;  /* No. of registers */
 	u16     addr[5]; /* Register addresses */
 	u8      data[5]; /* Register data */
 	u8      mask; /* Valid for read-modify-write */
+};
+
+struct scu_ipc_version {
+	u32     count;  /* length of version info */
+	u8      data[16]; /* version data */
 };
 
 /**
@@ -49,22 +85,27 @@ struct scu_ipc_data {
 
 static int scu_reg_access(u32 cmd, struct scu_ipc_data  *data)
 {
-	int count = data->count;
+	int ret;
 
-	if (count == 0 || count == 3 || count > 4)
+	if (data->count == 0 || data->count > 5)
 		return -EINVAL;
 
 	switch (cmd) {
-	case INTE_SCU_IPC_REGISTER_READ:
-		return intel_scu_ipc_readv(data->addr, data->data, count);
-	case INTE_SCU_IPC_REGISTER_WRITE:
-		return intel_scu_ipc_writev(data->addr, data->data, count);
-	case INTE_SCU_IPC_REGISTER_UPDATE:
-		return intel_scu_ipc_update_register(data->addr[0],
-						    data->data[0], data->mask);
+	case INTEL_SCU_IPC_REGISTER_READ:
+		ret = intel_scu_ipc_readv(data->addr, data->data, data->count);
+		break;
+	case INTEL_SCU_IPC_REGISTER_WRITE:
+		ret = intel_scu_ipc_writev(data->addr, data->data, data->count);
+		break;
+	case INTEL_SCU_IPC_REGISTER_UPDATE:
+		ret = intel_scu_ipc_update_register(data->addr[0],
+							data->data[0],
+							data->mask);
+		break;
 	default:
 		return -ENOTTY;
 	}
+	return ret;
 }
 
 /**
@@ -78,39 +119,226 @@ static int scu_reg_access(u32 cmd, struct scu_ipc_data  *data)
 static long scu_ipc_ioctl(struct file *fp, unsigned int cmd,
 							unsigned long arg)
 {
-	int ret;
+	int ret = -EINVAL;
 	struct scu_ipc_data  data;
 	void __user *argp = (void __user *)arg;
+	int platform;
 
-	if (!capable(CAP_SYS_RAWIO))
+	/* Only IOCTL cmd allowed to pass through without capability check */
+	/* is getting fw version info, all others need to check to prevent */
+	/* arbitrary access to all sort of bit of the hardware exposed here*/
+
+	if (cmd != INTEL_SCU_IPC_FW_REVISION_GET && !capable(CAP_SYS_RAWIO))
 		return -EPERM;
 
-	if (copy_from_user(&data, argp, sizeof(struct scu_ipc_data)))
-		return -EFAULT;
-	ret = scu_reg_access(cmd, &data);
-	if (ret < 0)
-		return ret;
-	if (copy_to_user(argp, &data, sizeof(struct scu_ipc_data)))
-		return -EFAULT;
-	return 0;
+	platform = intel_mid_identify_cpu();
+	switch (cmd) {
+	case INTEL_SCU_IPC_FW_UPDATE:
+	{
+		if (platform == INTEL_MID_CPU_CHIP_LINCROFT) {
+			u8 *fwbuf = kmalloc(MAX_FW_SIZE, GFP_KERNEL);
+			if (fwbuf == NULL)
+				return -ENOMEM;
+			if (copy_from_user(fwbuf, (u8 *)arg, MAX_FW_SIZE)) {
+				kfree(fwbuf);
+				return -EFAULT;
+			}
+			ret = intel_scu_ipc_mrstfw_update(fwbuf, MAX_FW_SIZE);
+			kfree(fwbuf);
+		}
+		break;
+	}
+	case INTEL_SCU_IPC_READ_RR_FROM_OSNIB:
+	{
+		u8 reboot_reason;
+		ret = intel_scu_ipc_read_osnib_rr(&reboot_reason);
+		if (ret < 0)
+			return ret;
+		ret = copy_to_user(argp, &reboot_reason, 1);
+		break;
+	}
+	case INTEL_SCU_IPC_WRITE_RR_TO_OSNIB:
+	{
+		u8 data;
+
+		ret = copy_from_user(&data, (u8 *)arg, 1);
+		if (ret < 0) {
+			pr_err("copy from user failed!!\n");
+			return ret;
+		}
+		ret = intel_scu_ipc_write_osnib_rr(data);
+		break;
+	}
+	case INTEL_SCU_IPC_WRITE_ALARM_FLAG_TO_OSNIB:
+	{
+		u8 flag, data;
+		ret = copy_from_user(&flag, (u8 *)arg, 1);
+		if (ret < 0) {
+			pr_err("copy from user failed!!\n");
+			return ret;
+		}
+		ret = intel_scu_ipc_read_oshob(&data, 1, OSNIB_ALARM_OFFSET);
+		if (ret < 0)
+			return ret;
+		if (flag)
+			data = data | 0x1; /* set alarm flag */
+		else
+			data = data & 0xFE; /* clear alarm flag */
+		ret = intel_scu_ipc_write_osnib(&data, 1, 2, OSNIB_ALARM_MASK);
+		break;
+	}
+	case INTEL_SCU_IPC_READ_VBATTCRIT:
+	{
+		u32 value;
+
+		pr_debug("cmd = INTEL_SCU_IPC_READ_VBATTCRIT");
+		ret = intel_scu_ipc_read_mip((u8 *)&value, 4, 0x318, 1);
+		if (ret < 0)
+			return ret;
+		pr_debug("VBATTCRIT VALUE = %x\n", value);
+		ret = copy_to_user(argp, &value, 4);
+		break;
+	}
+	case INTEL_SCU_IPC_MEDFIELD_FW_UPDATE:
+	{
+		if (platform == INTEL_MID_CPU_CHIP_PENWELL) {
+			ret = intel_scu_ipc_medfw_prepare(argp);
+
+			if (ret < 0) {
+				pr_err("ipc_device_medfw_upgrade failed!!\n");
+				return ret;
+			}
+		}
+		break;
+	}
+	case INTEL_SCU_IPC_FW_REVISION_GET:
+	{
+		struct scu_ipc_version version;
+
+		if (copy_from_user(&version, argp, sizeof(u32)))
+			return -EFAULT;
+
+		if (version.count > 16)
+			return -EINVAL;
+
+		ret = intel_scu_ipc_command(IPCMSG_FW_REVISION, 0,
+					NULL, 0, (u32 *)version.data, 4);
+		if (ret < 0)
+			return ret;
+
+		if (copy_to_user(argp + sizeof(u32),
+					version.data, version.count))
+			ret = -EFAULT;
+		break;
+	}
+	default:
+		if (copy_from_user(&data, argp, sizeof(struct scu_ipc_data)))
+			return -EFAULT;
+		ret = scu_reg_access(cmd, &data);
+		if (ret < 0)
+			return ret;
+		if (copy_to_user(argp, &data, sizeof(struct scu_ipc_data)))
+			return -EFAULT;
+		return 0;
+	}
+
+	return ret;
 }
+
+/*
+ * This writes the reboot reason in the OSNIB (factor and avoid any overlap)
+ */
+int intel_scu_ipc_write_osnib_rr(u8 rr)
+{
+	return intel_scu_ipc_write_osnib(&rr, 1, OSNIB_RR_OFFSET-OSNIB_OFFSET,
+						OSNIB_RR_MASK);
+}
+EXPORT_SYMBOL_GPL(intel_scu_ipc_write_osnib_rr);
+
+/*
+ * This reads the reboot reason from the OSNIB (factor)
+ */
+int intel_scu_ipc_read_osnib_rr(u8 *rr)
+{
+	return intel_scu_ipc_read_oshob(rr, 1, OSNIB_RR_OFFSET);
+}
+EXPORT_SYMBOL_GPL(intel_scu_ipc_read_osnib_rr);
+
+/*
+ * This reads the PMIT from the OSHOB (pointer to interrupt tree)
+ */
+#ifdef DUMP_OSNIB
+static int intel_scu_ipc_read_oshob_it_tree(u32 *ptr)
+{
+	return intel_scu_ipc_read_oshob((u8 *) ptr, 4, OSHOB_PMIT_OFFSET);
+}
+#endif
+
+/*
+ * This reads the RESETIRQ1 from the OSNIB
+ */
+#ifdef DUMP_OSNIB
+static int intel_scu_ipc_read_osnib_resetirq1(u8 *rirq1)
+{
+	return intel_scu_ipc_read_oshob(rirq1, 1, OSNIB_RESETIRQ1_OFFSET);
+}
+#endif
+
+/*
+ * This reads the RESETIRQ2 from the OSNIB
+ */
+#ifdef DUMP_OSNIB
+static int intel_scu_ipc_read_osnib_resetirq2(u8 *rirq2)
+{
+	return intel_scu_ipc_read_oshob(rirq2, 1, OSNIB_RESETIRQ2_OFFSET);
+}
+#endif
 
 static const struct file_operations scu_ipc_fops = {
 	.unlocked_ioctl = scu_ipc_ioctl,
 };
 
+static struct miscdevice scu_ipcutil = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "mid_ipc",
+	.fops = &scu_ipc_fops,
+};
+
 static int __init ipc_module_init(void)
 {
-	major = register_chrdev(0, "intel_mid_scu", &scu_ipc_fops);
-	if (major < 0)
-		return major;
+#ifdef DUMP_OSNIB
+	u8 rr, resetirq1, resetirq2, *ptr;
+	u32 pmit;
+#endif
 
-	return 0;
+#ifdef DUMP_OSNIB
+	/* Dumping RESETIRQ1 and 2 from the interrupt tree */
+	intel_scu_ipc_read_oshob_it_tree(&pmit);
+	ptr = ioremap_nocache(pmit + PMIT_RESETIRQ1_OFFSET, 2);
+	if (ptr) {
+		resetirq1 = readb(ptr);
+		resetirq2 = readb(ptr+1);
+		pr_warn("[BOOT] RESETIRQ1=0x%02x RESETIRQ2=0x%02x "
+			"(interrupt tree)\n",
+			resetirq1, resetirq2);
+		iounmap(ptr);
+	}
+
+	/* Dumping OSNIB content */
+	intel_scu_ipc_read_osnib_rr(&rr);
+	intel_scu_ipc_read_osnib_resetirq1(&resetirq1);
+	intel_scu_ipc_read_osnib_resetirq2(&resetirq2);
+	pr_warn("[BOOT] RR=0x%02x (osnib)\n", rr);
+	pr_warn("[BOOT] RESETIRQ1=0x%02x RESETIRQ2=0x%02x (osnib)\n",
+		resetirq1, resetirq2);
+#endif
+
+	return misc_register(&scu_ipcutil);
 }
 
 static void __exit ipc_module_exit(void)
 {
-	unregister_chrdev(major, "intel_mid_scu");
+	misc_deregister(&scu_ipcutil);
 }
 
 module_init(ipc_module_init);
