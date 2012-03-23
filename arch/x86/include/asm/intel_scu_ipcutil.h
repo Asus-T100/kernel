2ffc475d9a6f704646567304d775b77b8bdd342a
lwan89x <liangx.wang@intel.com>
2012-03-23 16:13:17 +0800

    intel_scu_ipc.c: Write chksum data when writing osnib data.
    BZ: 24109
    
    Change-Id: I1c102eed594b4b661fefdf989ff95d4e60488800
    Signed-off-by: lwan89x <liangx.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/40435
    Reviewed-by: Puech, Yann <yann.puech@intel.com>
    Reviewed-by: Romieu, Benoit <benoit.romieu@intel.com>
    Tested-by: Romieu, Benoit <benoit.romieu@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- arch/x86/include/asm/intel_scu_ipcutil.h
+++ arch/x86/include/asm/intel_scu_ipcutil.h
@@ -62,7 +62,8 @@ int intel_scu_ipc_msic_vprog2(int on);
 int intel_scu_ipc_read_oshob(u8 *data, int len, int offset);
 /* OSNIB-OS No Init Buffer write */
 #define OSNIB_OFFSET           0x0C
-int intel_scu_ipc_write_osnib(u8 *data, int len, int offset, u32 mask);
+int intel_scu_ipc_write_osnib(u8 *data, int len, int offset);
+int intel_scu_ipc_read_osnib(u8 *data, int len, int offset);
 int intel_scu_ipc_write_osnib_rr(u8 rr);
 int intel_scu_ipc_read_osnib_rr(u8 *rr);
 
