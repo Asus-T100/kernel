317b88368bbe6c2eb09e7e1c836015bcbcb6d801
Amar Shankar <amarx.shankar@intel.com>
2012-04-12 09:58:27 +0530

    EM/Charger: Enable Basin Cove Charger Driver
    BZ: 15397
    
    Enable basin cove charger driver with initialization flows and
    register with power supply subsystem
    
    Change-Id: I241c0044e3f806285e76a8f6aa4a01e3055e5299
    Signed-off-by: Amar Shankar <amarx.shankar@intel.com>
    Reviewed-on: http://android.intel.com:8080/43224
    Tested-by: P, Eshwar N <eshwar.n.p@intel.com>
    Reviewed-by: Pujol, BenjaminX <benjaminx.pujol@intel.com>
    Reviewed-by: Tc, Jenny <jenny.tc@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>
    
    Conflicts:
    
    	drivers/power/Makefile

---
+++ drivers/power/basin_cove_charger.c
@@ -0,0 +1,502 @@
+/*
+ * basin_cove_charger.c - Intel MID Basin Cove PMIC Charger Driver
+ *
+ * Copyright (C) 2011 Intel Corporation
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Author: Jenny TC <jenny.tc@intel.com>
+ */
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/err.h>
+#include <linux/interrupt.h>
+#include <linux/workqueue.h>
+#include <linux/jiffies.h>
+#include <linux/slab.h>
+#include <linux/kfifo.h>
+#include <linux/param.h>
+#include <linux/device.h>
+#include <linux/platform_device.h>
+#include <linux/power_supply.h>
+#include <linux/io.h>
+#include <linux/sched.h>
+#include <linux/pm_runtime.h>
+#include <linux/sfi.h>
+#include <linux/wakelock.h>
+#include <linux/async.h>
+#include <linux/reboot.h>
+
+#include "basin_cove_charger.h"
+
+#define CHARGER_PS_NAME "bcove_charger"
+#define DRIVER_NAME "bcove_charger"
+
+#define DEVICE_NAME "bc_charger"
+
+static struct device *bc_chrgr_dev;
+
+/*
+ * Basin Cove Charger power supply  properties
+ */
+static enum power_supply_property bc_chrgr_ps_props[] = {
+	POWER_SUPPLY_PROP_TYPE,
+	POWER_SUPPLY_PROP_PRESENT,
+	POWER_SUPPLY_PROP_ONLINE,
+	POWER_SUPPLY_PROP_HEALTH,
+	POWER_SUPPLY_PROP_VOLTAGE_NOW,
+	POWER_SUPPLY_PROP_MODEL_NAME,
+	POWER_SUPPLY_PROP_MANUFACTURER,
+};
+
+/*FIXME: Remove this IPC call stubs */
+static int intel_scu_ipc_update_register(u16 addr, u8 bits, u8 mask)
+{
+	return 0;
+}
+
+static int bc_enable_charger(void)
+{
+	return intel_scu_ipc_update_register(CHGRCTRL0_ADDR, EXTCHRDIS_ENABLE,
+					     CHGRCTRL0_EXTCHRDIS_MASK);
+
+}
+
+static int bc_disbale_charger(void)
+{
+	return intel_scu_ipc_update_register(CHGRCTRL0_ADDR, ~EXTCHRDIS_ENABLE,
+					     CHGRCTRL0_EXTCHRDIS_MASK);
+
+}
+
+static int bc_set_chrgr_current_limit(int cur_inlmt)
+{
+	int inlmt_mask;
+
+	if (cur_inlmt <= 100)
+		inlmt_mask = CHGRCTRL1_FUSB_INLMT_100_MASK;
+	else if (cur_inlmt <= 150)
+		inlmt_mask = CHGRCTRL1_FUSB_INLMT_150_MASK;
+	else if (cur_inlmt <= 500)
+		inlmt_mask = CHGRCTRL1_FUSB_INLMT_500_MASK;
+	else if (cur_inlmt <= 900)
+		inlmt_mask = CHGRCTRL1_FUSB_INLMT_900_MASK;
+	else if (cur_inlmt <= 1500)
+		inlmt_mask = CHGRCTRL1_FUSB_INLMT_1500_MASK;
+	else
+		return -EINVAL;
+	return intel_scu_ipc_update_register(CHGRCTRL1_ADDR, 0xFF, inlmt_mask);
+}
+
+/**
+ * bc_chrgr_get_property - charger power supply get property
+ * @psy: charger power supply context
+ * @psp: charger property
+ * @val: charger property value
+ * Context: can sleep
+ *
+ * Basin Cove power supply property needs to be provided to power_supply
+ * subsystem for it to provide the information to users.
+ */
+static int bc_chrgr_ps_get_property(struct power_supply *psy,
+				    enum power_supply_property psp,
+				    union power_supply_propval *val)
+{
+	struct bc_chrgr_drv_context *chrgr_drv_cxt =
+	    container_of(psy, struct bc_chrgr_drv_context, bc_chrgr_ps);
+
+	mutex_lock(&chrgr_drv_cxt->bc_chrgr_lock);
+
+	switch (psp) {
+	case POWER_SUPPLY_PROP_PRESENT:
+		val->intval = chrgr_drv_cxt->chrgr_props_cxt.charger_present;
+		break;
+	case POWER_SUPPLY_PROP_ONLINE:
+		if (chrgr_drv_cxt->chrgr_props_cxt.charging_mode !=
+		    POWER_SUPPLY_STATUS_NOT_CHARGING)
+			val->intval =
+			    chrgr_drv_cxt->chrgr_props_cxt.charger_present;
+		else
+			val->intval = 0;
+		break;
+	case POWER_SUPPLY_PROP_HEALTH:
+		val->intval = chrgr_drv_cxt->chrgr_props_cxt.charger_health;
+		break;
+	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
+		/* TODO: ADC sampling of voltage */
+		val->intval = chrgr_drv_cxt->chrgr_props_cxt.vbus_vol * 1000;
+		break;
+	case POWER_SUPPLY_PROP_MODEL_NAME:
+		val->strval = chrgr_drv_cxt->chrgr_props_cxt.charger_model;
+		break;
+	case POWER_SUPPLY_PROP_MANUFACTURER:
+		val->strval = chrgr_drv_cxt->chrgr_props_cxt.charger_vender;
+		break;
+	default:
+		mutex_unlock(&chrgr_drv_cxt->bc_chrgr_lock);
+		return -EINVAL;
+	}
+
+	mutex_unlock(&chrgr_drv_cxt->bc_chrgr_lock);
+	return 0;
+}
+
+static void init_charger_ps_context(struct bc_chrgr_drv_context *chrgr_drv_cxt)
+{
+	chrgr_drv_cxt->bc_chrgr_ps.name = CHARGER_PS_NAME;
+	chrgr_drv_cxt->bc_chrgr_ps.type = POWER_SUPPLY_TYPE_USB;
+	chrgr_drv_cxt->bc_chrgr_ps.properties = bc_chrgr_ps_props;
+	chrgr_drv_cxt->bc_chrgr_ps.num_properties =
+	    ARRAY_SIZE(bc_chrgr_ps_props);
+	chrgr_drv_cxt->bc_chrgr_ps.get_property = bc_chrgr_ps_get_property;
+
+	chrgr_drv_cxt->chrgr_props_cxt.charger_present = CHARGER_NOT_PRESENT;
+	chrgr_drv_cxt->chrgr_props_cxt.charger_health =
+	    POWER_SUPPLY_HEALTH_UNKNOWN;
+	memcpy(chrgr_drv_cxt->chrgr_props_cxt.charger_model, "Unknown",
+	       sizeof("Unknown"));
+	memcpy(chrgr_drv_cxt->chrgr_props_cxt.charger_vender, "Unknown",
+	       sizeof("Unknown"));
+	/*TODO remove this assignment. No default val. Read from ADC */
+	chrgr_drv_cxt->chrgr_props_cxt.vbus_vol = 4200;
+}
+
+static int get_charging_profile(struct charging_profile *chrg_profile)
+{
+	/*
+	 * TODO: Get battery profile from platfrom layer. Using hardcoded sample
+	 * value till this is done.
+	 */
+
+	memcpy(chrg_profile->batt_id, "UNKNOWN", sizeof("UNKNOWN"));
+	chrg_profile->voltage_max = 4200;
+	chrg_profile->capacity = 1500;
+	chrg_profile->battery_type = 2;	/* POWER_SUPPLY_TECHNOLOGY_LION */
+	chrg_profile->temp_mon_ranges = 4;
+
+	chrg_profile->temp_mon_range[0].temp_low_lim = 45;
+	chrg_profile->temp_mon_range[0].temp_up_lim = 60;
+	chrg_profile->temp_mon_range[0].rbatt = 120;
+	chrg_profile->temp_mon_range[0].full_chrg_cur = 950;
+	chrg_profile->temp_mon_range[0].full_chrg_vol = 4100;
+	chrg_profile->temp_mon_range[0].maint_chrg_cur = 950;
+	chrg_profile->temp_mon_range[0].maint_chrg_vol_ll = 4000;
+	chrg_profile->temp_mon_range[0].maint_chrg_vol_ul = 4050;
+
+	chrg_profile->temp_mon_range[1].temp_low_lim = 10;
+	chrg_profile->temp_mon_range[1].temp_up_lim = 45;
+	chrg_profile->temp_mon_range[1].rbatt = 120;
+	chrg_profile->temp_mon_range[1].full_chrg_cur = 950;
+	chrg_profile->temp_mon_range[1].full_chrg_vol = 4200;
+	chrg_profile->temp_mon_range[1].maint_chrg_cur = 950;
+	chrg_profile->temp_mon_range[1].maint_chrg_vol_ll = 4100;
+	chrg_profile->temp_mon_range[1].maint_chrg_vol_ul = 4150;
+
+	chrg_profile->temp_mon_range[2].temp_low_lim = 0;
+	chrg_profile->temp_mon_range[2].temp_up_lim = 10;
+	chrg_profile->temp_mon_range[2].rbatt = 120;
+	chrg_profile->temp_mon_range[2].full_chrg_cur = 950;
+	chrg_profile->temp_mon_range[2].full_chrg_vol = 4100;
+	chrg_profile->temp_mon_range[2].maint_chrg_cur = 950;
+	chrg_profile->temp_mon_range[2].maint_chrg_vol_ll = 4000;
+	chrg_profile->temp_mon_range[2].maint_chrg_vol_ul = 4050;
+
+	chrg_profile->temp_mon_range[3].temp_low_lim = -10;
+	chrg_profile->temp_mon_range[3].temp_up_lim = 0;
+	chrg_profile->temp_mon_range[3].rbatt = 120;
+	chrg_profile->temp_mon_range[3].full_chrg_cur = 950;
+	chrg_profile->temp_mon_range[3].full_chrg_vol = 3900;
+	chrg_profile->temp_mon_range[3].maint_chrg_cur = 950;
+	chrg_profile->temp_mon_range[3].maint_chrg_vol_ll = 3950;
+	chrg_profile->temp_mon_range[3].maint_chrg_vol_ul = 3950;
+
+	return 0;
+
+}
+
+static void populate_invalid_chrg_profile(struct charging_profile *chrg_profile)
+{
+
+	/*
+	 * In case of invalid battery we manually set
+	 * the battery parameters and limit the battery from
+	 * charging, so platform will be in discharging mode
+	 */
+
+	memcpy(chrg_profile->batt_id, "UNKNOWN", sizeof("UNKNOWN"));
+	chrg_profile->temp_mon_ranges = 0;
+
+}
+
+static int get_batt_config(struct battery_config *batt_config)
+{
+
+	/*TODO: Get battery configuration from platfrom layer.Using default
+	   hardcoded values till this is done */
+	batt_config->vbatt_sh_min = BATT_DEAD_CUTOFF_VOLT;
+	batt_config->vbatt_crit = BATT_CRIT_CUTOFF_VOLT;
+	batt_config->temp_high = MSIC_BATT_TEMP_MAX;
+	batt_config->temp_low = MSIC_BATT_TEMP_MIN;
+	batt_config->itc = 50;
+
+	return 0;
+}
+
+static void populate_default_battery_config(struct battery_config *batt_config)
+{
+	batt_config->vbatt_sh_min = BATT_DEAD_CUTOFF_VOLT;
+	batt_config->vbatt_crit = BATT_CRIT_CUTOFF_VOLT;
+	batt_config->temp_high = MSIC_BATT_TEMP_MAX;
+	batt_config->temp_low = MSIC_BATT_TEMP_MIN;
+}
+
+/**
+ * bc_charger_probe - basin cove charger probe function
+ * @pdev: basin cove platform device structure
+ * Context: can sleep
+ *
+ * basin cove charger driver initializes its internal data
+ * structure and other  infrastructure components for it
+ * to work as expected.
+ */
+static int bc_chrgr_probe(struct platform_device *pdev)
+{
+	struct bc_chrgr_drv_context *chrgr_drv_cxt = NULL;
+	int retval = 0;
+
+	chrgr_drv_cxt =
+	    kzalloc(sizeof(struct bc_chrgr_drv_context), GFP_KERNEL);
+	if (!chrgr_drv_cxt) {
+		dev_err(&pdev->dev, "%s(): memory allocation failed\n",
+			__func__);
+		return -ENOMEM;
+	}
+	chrgr_drv_cxt->chrg_profile =
+	    kzalloc(sizeof(struct charging_profile), GFP_KERNEL);
+	if (!chrgr_drv_cxt->chrg_profile) {
+
+		dev_err(&pdev->dev, "%s(): memory allocation failed: Unable to"
+			"allocate memory for battery profile\n", __func__);
+		retval = -ENOMEM;
+		goto chrg_profile_alloc_failed;
+	}
+
+	chrgr_drv_cxt->batt_config =
+	    kzalloc(sizeof(struct battery_config), GFP_KERNEL);
+	if (!chrgr_drv_cxt->batt_config) {
+
+		dev_err(&pdev->dev, "%s(): memory allocation failed: Unable to"
+			"allocate memory for battery configuration\n",
+			__func__);
+		retval = -ENOMEM;
+		goto batt_config_alloc_failed;
+	}
+
+	chrgr_drv_cxt->pdev = pdev;
+	chrgr_drv_cxt->irq = platform_get_irq(pdev, 0);
+	platform_set_drvdata(pdev, chrgr_drv_cxt);
+	bc_chrgr_dev = &pdev->dev;
+
+	if (get_charging_profile(chrgr_drv_cxt->chrg_profile)) {
+		dev_err(&pdev->dev, "%s() :Failed to get battery properties\n",
+			__func__);
+		populate_invalid_chrg_profile(chrgr_drv_cxt->chrg_profile);
+		chrgr_drv_cxt->invalid_batt = true;
+	}
+
+	if (get_batt_config(chrgr_drv_cxt->batt_config)) {
+		dev_err(&pdev->dev, "%s() :Failed to get battery settings\n",
+			__func__);
+		populate_default_battery_config(chrgr_drv_cxt->batt_config);
+	}
+
+	init_charger_ps_context(chrgr_drv_cxt);
+
+	/*TODO: Interrupt tree mapping */
+	/*TODO: Register IRQ handler */
+
+	/* initialize mutexes */
+	mutex_init(&chrgr_drv_cxt->bc_chrgr_lock);
+
+	retval = power_supply_register(&pdev->dev, &chrgr_drv_cxt->bc_chrgr_ps);
+	if (retval) {
+		dev_err(&pdev->dev,
+			"%s(): failed to register basin cove charger"
+			"device with power supply subsystem\n", __func__);
+		goto ps_reg_failed;
+	}
+	/*TODO: register OTG callback handle */
+
+	/* Enable SWCONTROL bit to handle USB events from SW */
+	retval = intel_scu_ipc_update_register(CHGRCTRL0_ADDR, SWCONTROL_ENABLE,
+					       CHGRCTRL0_SWCONTROL_MASK);
+	if (retval) {
+
+		dev_err(&pdev->dev, "%s(): failed to set SWCONTROL bit"
+			"Continuing in HW charging mode\n", __func__);
+	}
+	return retval;
+
+ps_reg_failed:
+	kfree(chrgr_drv_cxt->batt_config);
+batt_config_alloc_failed:
+	kfree(chrgr_drv_cxt->chrg_profile);
+chrg_profile_alloc_failed:
+	kfree(chrgr_drv_cxt);
+
+	return retval;
+
+}
+
+static void bc_chrgr_do_exit_ops(struct bc_chrgr_drv_context *chrgr_drv_cxt)
+{
+	/*TODO:
+	 * If charger is connected send IPC message to SCU to continue charging
+	 */
+
+}
+
+/**
+ * bc_charger_remove - basin cove charger finalize
+ * @pdev: basin cove charger platform  device structure
+ * Context: can sleep
+ *
+ * Basin cove charger finalizes its internal data structure and other
+ * infrastructure components that it initialized in
+ * bc_chrgr_probe.
+ */
+static int bc_chrgr_remove(struct platform_device *pdev)
+{
+	struct bc_chrgr_drv_context *chrgr_drv_cxt = platform_get_drvdata(pdev);
+
+	if (chrgr_drv_cxt) {
+		bc_chrgr_do_exit_ops(chrgr_drv_cxt);
+
+		power_supply_unregister(&chrgr_drv_cxt->bc_chrgr_ps);
+
+		kfree(chrgr_drv_cxt->chrg_profile);
+		kfree(chrgr_drv_cxt->batt_config);
+		mutex_destroy(&chrgr_drv_cxt->bc_chrgr_lock);
+		kfree(chrgr_drv_cxt);
+	}
+
+	return 0;
+}
+
+#ifdef CONFIG_PM
+static int bc_chrgr_suspend(struct device *dev)
+{
+	dev_dbg(dev, "%s called\n", __func__);
+	return 0;
+}
+
+static int bc_chrgr_resume(struct device *dev)
+{
+	dev_dbg(dev, "%s called\n", __func__);
+	return 0;
+}
+#else
+#define bc_chrgr_suspend    NULL
+#define bc_chgr_resume     NULL
+#endif
+
+#ifdef CONFIG_PM_RUNTIME
+static int bc_chrgr_runtime_suspend(struct device *dev)
+{
+
+	dev_dbg(dev, "%s called\n", __func__);
+	return 0;
+}
+
+static int bc_chrgr_runtime_resume(struct device *dev)
+{
+	dev_dbg(dev, "%s called\n", __func__);
+	return 0;
+}
+
+static int bc_chrgr_runtime_idle(struct device *dev)
+{
+	dev_dbg(dev, "%s called\n", __func__);
+	return 0;
+}
+#else
+#define bc_chrgr_runtime_suspend	NULL
+#define bc_chrgr_runtime_resume		NULL
+#define bc_chrgr_runtime_idle		NULL
+#endif
+/*********************************************************************
+ *		Driver initialisation and finalization
+ *********************************************************************/
+
+static const struct platform_device_id bc_chrgr_id_table[] = {
+	{DEVICE_NAME, 1},
+};
+
+static const struct dev_pm_ops bc_chrgr_pm_ops = {
+	.suspend = bc_chrgr_suspend,
+	.resume = bc_chrgr_resume,
+	.runtime_suspend = bc_chrgr_runtime_suspend,
+	.runtime_resume = bc_chrgr_runtime_resume,
+	.runtime_idle = bc_chrgr_runtime_idle,
+};
+
+static struct platform_driver bc_chrgr_driver = {
+	.driver = {
+		   .name = DRIVER_NAME,
+		   .owner = THIS_MODULE,
+		   .pm = &bc_chrgr_pm_ops,
+		   },
+	.probe = bc_chrgr_probe,
+	.remove = __devexit_p(bc_chrgr_remove),
+	.id_table = bc_chrgr_id_table,
+};
+
+static int __init bc_chrgr_init(void)
+{
+	int ret;
+	struct platform_device *pdev;
+
+	/*FIXME: move platfrom device registartion to platfrom layer
+	   once bc_charger device is populated in platform device list */
+
+	pdev = platform_device_alloc(DEVICE_NAME, 0);
+	if (!pdev)
+		return PTR_ERR(pdev);
+	ret = platform_device_add(pdev);
+	if (ret) {
+		kfree(pdev);
+		return ret;
+	}
+
+	ret = platform_driver_register(&bc_chrgr_driver);
+
+	return ret;
+}
+
+static void __exit bc_chrgr_exit(void)
+{
+
+	platform_driver_unregister(&bc_chrgr_driver);
+}
+/* Defer init call so that dependant drivers will be loaded. Using  async
+ * for parallel driver initialization */
+late_initcall_async(bc_chrgr_init);
+module_exit(bc_chrgr_exit);
+
+MODULE_AUTHOR("Jenny TC <jenny.tc@intel.com>");
+MODULE_DESCRIPTION("Basin Cove Charger  Driver");
+MODULE_LICENSE("GPL");
---
+++ drivers/power/basin_cove_charger.h
@@ -0,0 +1,263 @@
+
+/*
+ * basin_cove_charger.h - Intel MID Basin Cove PMIC Charger Driver header file
+ *
+ * Copyright (C) 2011 Intel Corporation
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Author: Jenny TC <jenny.tc@intel.com>
+ */
+
+#ifndef __BASIN_COVE_CHARGER_H_
+#define __BASIN_COVE_CHARGER_H_
+
+
+/*********************************************************************
+ *		Generic defines
+ *********************************************************************/
+
+#define D7 (1 << 7)
+#define D6 (1 << 6)
+#define D5 (1 << 5)
+#define D4 (1 << 4)
+#define D3 (1 << 3)
+#define D2 (1 << 2)
+#define D1 (1 << 1)
+#define D0 (1 << 0)
+
+
+#define CHGRIRQ0_ADDR			0x07
+#define CHGIRQ0_BZIRQ_MASK		D7
+#define CHGIRQ0_BAT_CRIT_MASK		D6
+#define CHGIRQ0_BAT1_ALRT_MASK		D5
+#define CHGIRQ0_BAT0_ALRT_MASK		D4
+#define CHGIRQ0_NACK_MASK		D3
+#define CHGIRQ0_I2CRDP_CMP_MASK		D2
+#define CHGIRQ0_I2CWR_CMP_MASK		D1
+#define CHGIRQ0_CHG_INTB_MASK		D0
+
+#define MCHGRIRQ0_ADDR			0x12
+#define MCHGIRQ0_RSVD_MASK		D7
+#define MCHGIRQ0_MBAT_CRIT_MASK		D6
+#define MCHGIRQ0_MBAT1_ALRT_MASK	D5
+#define MCHGIRQ0_MBAT0_ALRT_MASK	D4
+#define MCHGIRQ0_MNACK_MASK		D3
+#define MCHGIRQ0_MI2CRDP_CMP_MASK	D2
+#define MCHGIRQ0_MI2CWR_CMP_MASK	D1
+#define MCHGIRQ0_MCHG_INTB_MASK		D0
+
+#define SCHGRIRQ0_ADDR			0x13
+#define SCHGIRQ0_RSVD_MASK		D7
+#define SCHGIRQ0_SBAT_CRIT_MASK		D6
+#define SCHGIRQ0_SBAT1_ALRT_MASK	D5
+#define SCHGIRQ0_SBAT0_ALRT_MASK	D4
+#define SCHGIRQ0_SNACK_MASK		D3
+#define SCHGIRQ0_SI2CRDP_CMP_MASK	D2
+#define SCHGIRQ0_SI2CWR_CMP_MASK	D1
+#define SCHGIRQ0_SCHG_INTB	D0
+
+#define CHGRCTRL0_ADDR			0x4B
+#define CHGRCTRL0_RSVD_MASK		(D7|D6|D5)
+#define CHGRCTRL0_TTLCK_MASK		D4
+#define CHGRCTRL0_SWCONTROL_MASK	D3
+#define CHGRCTRL0_EXTCHRDIS_MASK	D2
+#define	CHRCTRL0_EMRGCHREN_MASK		D1
+#define	CHRCTRL0_CHGRRESET_MASK		D0
+
+#define EXTCHRDIS_ENABLE		1
+#define SWCONTROL_ENABLE		1
+
+#define CHGRCTRL1_ADDR			0x4C
+#define CHGRCTRL1_RSVD_MASK			(D7|D6)
+#define CHGRCTRL1_FTEMP_EVENT_MASK		D5
+#define CHGRCTRL1_FUSB_INLMT_1500_MASK		D4
+#define CHGRCTRL1_FUSB_INLMT_900_MASK		D3
+#define CHGRCTRL1_FUSB_INLMT_500_MASK		D2
+#define CHGRCTRL1_FUSB_INLMT_150_MASK		D1
+#define CHGRCTRL1_FUSB_INLMT_100_MASK		D0
+
+#define CHGRSTATUS_ADDR				0x4D
+#define CHGRSTATUS_RSVD_MASK			(D7|D6|D5|D4|D3|D2)
+#define CHGRSTATUS_CHGDETB_LATCH_MASK		D1
+#define CHGDETB_MASK				D0
+
+#define I2COVRCTRL_ADDR		0x58
+#define I2COVRDADDR_ADDR	0x59
+#define I2COVROFFSET_ADDR	0x5A
+#define I2COVRWRDATA_ADDR	0x5B
+#define I2COVRRDDATA_ADDR	0x5C
+
+#define CHRTTADDR_ADDR		0x56
+#define CHRTTDATA_ADDR		0x57
+
+#define TT_I2CDADDR_ADDR		0x00
+#define TT_CHGRINIT0OS_ADDR		0x01
+#define TT_CHGRINIT1OS_ADDR		0x02
+#define TT_CHGRINIT2OS_ADDR		0x03
+#define TT_CHGRINIT3OS_ADDR		0x04
+#define TT_CHGRINIT4OS_ADDR		0x05
+#define TT_CHGRINIT5OS_ADDR		0x06
+#define TT_CHGRINIT6OS_ADDR		0x07
+#define TT_CHGRINIT7OS_ADDR		0x08
+#define TT_USBINPUTICCOS_ADDR		0x09
+#define TT_USBINPUTICCMASK_ADDR		0x0A
+#define TT_CHRCVOS_ADDR			0X0B
+#define TT_CHRCVMASK_ADDR		0X0C
+#define TT_CHRCCOS_ADDR			0X0D
+#define TT_CHRCCMASK_ADDR		0X0E
+#define TT_LOWCHROS_ADDR		0X0F
+#define TT_LOWCHRMASK_ADDR		0X10
+#define TT_WDOGRSTOS_ADDR		0X11
+#define TT_WDOGRSTMASK_ADDR		0X12
+#define TT_CHGRENOS_ADDR		0X13
+#define TT_CHGRENMASK_ADDR		0X14
+#define TT_CUSTOMFIELDEN_ADDR		0X15
+#define TT_CHGRINIT0VAL_ADDR		0X16
+#define TT_CHGRINIT1VAL_ADDR		0X17
+#define TT_CHGRINIT2VAL_ADDR		0X18
+#define TT_CHGRINIT3VAL_ADDR		0X19
+#define TT_CHGRINIT4VAL_ADDR		0X1A
+#define TT_CHGRINIT5VAL_ADDR		0X1B
+#define TT_CHGRINIT6VAL_ADDR		0X1C
+#define TT_CHGRINIT7VAL_ADDR		0X1D
+#define TT_USBINPUTICC100VAL_ADDR	0X1E
+#define TT_USBINPUTICC150VAL_ADDR	0X1F
+#define TT_USBINPUTICC500VAL_ADDR	0X20
+#define TT_USBINPUTICC900VAL_ADDR	0X21
+#define TT_USBINPUTICC1500VAL_ADDR	0X22
+#define TT_CHRCVEMRGLOWVAL_ADDR		0X23
+#define TT_CHRCVCOLDVAL_ADDR		0X24
+#define TT_CHRCVCOOLVAL_ADDR		0X25
+#define TT_CHRCVWARMVAL_ADDR		0X26
+#define TT_CHRCVHOTVAL_ADDR		0X27
+#define TT_CHRCVEMRGHIVAL_ADDR		0X28
+#define TT_CHRCCEMRGLOWVAL_ADDR		0X29
+#define TT_CHRCCCOLDVAL_ADDR		0X2A
+#define TT_CHRCCOOLVAL_ADDR		0X2B
+#define TT_CHRCCWARMVAL_ADDR		0X2C
+#define TT_CHRCCHOTVAL_ADDR		0X2D
+#define TT_CHRCCEMRGHIVAL_ADDR		0X2E
+#define TT_LOWCHRENVAL_ADDR		0X2F
+#define TT_LOWCHRDISVAL_ADDR		0X30
+#define TT_WDOGRSTVAL_ADDR		0X31
+#define TT_CHGRENVAL_ADDR		0X32
+#define TT_CHGRDISVAL_ADDR		0X33
+
+#define BATT_STRING_MAX		8
+#define BATTID_STR_LEN		8
+#define BATT_PROF_MAX_TEMP_NR_RNG	6
+
+#define CHARGER_PRESENT		1
+#define CHARGER_NOT_PRESENT	0
+
+/*FIXME: Modify default values */
+#define BATT_DEAD_CUTOFF_VOLT		3400	/* 3400 mV */
+#define BATT_CRIT_CUTOFF_VOLT		3700	/* 3700 mV */
+
+#define MSIC_BATT_TEMP_MAX		60	/* 60 degrees */
+#define MSIC_BATT_TEMP_MIN		0
+
+/* Bit definitions */
+
+/* Temperature Monitoring Table */
+struct temp_mon_table {
+	short int temp_up_lim;
+	short int temp_low_lim;
+	short int rbatt;
+	short int full_chrg_vol;
+	short int full_chrg_cur;
+	short int maint_chrg_vol_ll;
+	short int maint_chrg_vol_ul;
+	short int maint_chrg_cur;
+} __packed;
+
+/*Battery properties */
+struct charging_profile {
+	char batt_id[BATTID_STR_LEN];
+	unsigned short int voltage_max;
+	unsigned int capacity;
+	u8 battery_type;
+	u8 temp_mon_ranges;
+	struct temp_mon_table temp_mon_range[BATT_PROF_MAX_TEMP_NR_RNG];
+} __packed;
+
+struct bc_charger_props_cxt {
+	unsigned int charging_mode;
+	unsigned int charger_present;
+	unsigned int charger_health;
+	unsigned int vbus_vol;
+	char charger_model[BATT_STRING_MAX];
+	char charger_vender[BATT_STRING_MAX];
+};
+
+/* Battery Settings info from platfrom layer*/
+struct battery_config {
+	u8 smip_rev;
+	u8 fpo;			/* fixed implementation options */
+	u8 fpo1;		/* fixed implementation options1 */
+	u8 rsys;		/* System Resistance for Fuel gauging */
+
+	/* Minimum voltage necessary to
+	 * be able to safely shut down */
+	short int vbatt_sh_min;
+
+	/* Voltage at which the battery driver
+	 * should report the LEVEL as CRITICAL */
+	short int vbatt_crit;
+
+	short int itc;		/* Charge termination current */
+	short int temp_high;	/* Safe Temp Upper Limit */
+	short int temp_low;	/* Safe Temp lower Limit */
+	u8 brd_id;		/* Unique Board ID */
+} __packed;
+
+/*
+ * basinc cove charger driver info
+ */
+struct bc_chrgr_drv_context {
+
+	struct platform_device *pdev;
+	bool invalid_batt;
+	bool is_batt_present;
+
+	struct charging_profile *chrg_profile;
+	struct battery_config *batt_config;
+
+	/* lock to protect the charger properties
+	 * locking is applied wherever read or write
+	 * operation is being performed to the
+	 * charger property structure.
+	 */
+
+	struct mutex bc_chrgr_lock;
+
+	struct bc_charger_props_cxt chrgr_props_cxt;
+	struct power_supply bc_chrgr_ps;
+
+	unsigned int irq;		/* GPE_ID or IRQ# */
+
+
+	/* lock to avoid concurrent  access to HW Registers.
+	 * As some charger control and parameter registers
+	 * can be read or write at same time, bc_ipc_rw_lock lock
+	 * is used to synchronize those Charger IPC read or write calls.
+	 */
+	struct mutex bc_ipc_rw_lock;
+
+};
+#endif
