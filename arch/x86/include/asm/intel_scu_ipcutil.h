aec38a2ae9d227b7d876054d8daf318d6891f742
Shijie Zhang <shijie.zhang@intel.com>
2012-04-25 19:37:38 +0800

    intel_scu_ipcutil: re-arch IPC ioctl struct
    BZ: 33355
    
    Userspace IPC ioctl is using self defined command and struct, it will
    cause inconsistent problem if ethier kernel or userspace side is making
    some changes. Correct way should be exporting the kernel ioctl command
    and struct to the userspace with kernel header file to keep them
    consisent. This patch fixes this problem.
    
    Change-Id: Icdc6e51f254279fba980935610e837bf6208f12b
    Signed-off-by: Shijie Zhang <shijie.zhang@intel.com>
    Reviewed-on: http://android.intel.com:8080/46003
    Reviewed-by: Chen, Jie D <jie.d.chen@intel.com>
    Reviewed-by: Li, Ning <ning.li@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- arch/x86/include/asm/intel_scu_ipcutil.h
+++ arch/x86/include/asm/intel_scu_ipcutil.h
@@ -1,11 +1,46 @@
 #ifndef _ASM_X86_INTEL_SCU_IPCUTIL_H_
 #define _ASM_X86_INTEL_SCU_IPCUTIL_H_
 
+#include <linux/types.h>
+
+/* ioctl commnds */
+#define INTEL_SCU_IPC_REGISTER_READ	0
+#define INTEL_SCU_IPC_REGISTER_WRITE	1
+#define INTEL_SCU_IPC_REGISTER_UPDATE	2
+#define INTEL_SCU_IPC_FW_UPDATE		0xA2
+#define INTEL_SCU_IPC_MEDFIELD_FW_UPDATE	0xA3
+#define INTEL_SCU_IPC_FW_REVISION_GET	0xB0
+#define INTEL_SCU_IPC_READ_RR_FROM_OSNIB	0xC1
+#define INTEL_SCU_IPC_WRITE_RR_TO_OSNIB	0xC2
+#define INTEL_SCU_IPC_READ_VBATTCRIT	0xC4
+#define INTEL_SCU_IPC_WRITE_ALARM_FLAG_TO_OSNIB	0xC5
+#define INTEL_SCU_IPC_OSC_CLK_CNTL	0xC6
+
+struct scu_ipc_data {
+	__u32	count;  /* No. of registers */
+	__u16	addr[5]; /* Register addresses */
+	__u8	data[5]; /* Register data */
+	__u8	mask; /* Valid for read-modify-write */
+};
+
+struct scu_ipc_version {
+	__u32	count;  /* length of version info */
+	__u8	data[16]; /* version data */
+};
+
+struct osc_clk_t {
+	__u32	id; /* clock id */
+	__u32	khz; /* clock frequency */
+};
+
+
 /* Penwell has 4 osc clocks */
 #define OSC_CLK_AUDIO	0	/* Audio */
 #define OSC_CLK_CAM0	1	/* Primary camera */
 #define OSC_CLK_CAM1	2	/* Secondary camera */
 #define OSC_CLK_DISP	3	/* Display buffer */
+
+#ifdef __KERNEL__
 
 int intel_scu_ipc_osc_clk(u8 clk, unsigned int khz);
 
@@ -30,5 +65,7 @@ int intel_scu_ipc_read_oshob(u8 *data, int len, int offset);
 int intel_scu_ipc_write_osnib(u8 *data, int len, int offset, u32 mask);
 int intel_scu_ipc_write_osnib_rr(u8 rr);
 int intel_scu_ipc_read_osnib_rr(u8 *rr);
+
+#endif
 
 #endif
