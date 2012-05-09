84877f9bf1fb20b8d6e403f1ea6219b382416574
Li, Ning <ning.li@intel.com>
2012-05-09 19:15:21 +0800

    bcove_adc: update basincove adc driver
    BZ: 35529
    
    Update basincove gpadc driver to clear MIRQLVL1 mask each time
    after irq handled.
    Add basincove gpadc sysfs interface for debugging.
    Usage:
        cd /sys/devices/ipc/bcove_adc.0/basincove_gpadc/
        /* do sampling at channel 1 and 2 */
        echo 0x6 > channel
        echo 1 > sample /* echo 1 to commit "sample" */
        cat result
    
    Change-Id: I37efed090321e524430fc09966d911bf69ccb446
    Signed-off-by: Li, Ning <ning.li@intel.com>
    Reviewed-on: http://android.intel.com:8080/47987
    Reviewed-by: Chen, Jie D <jie.d.chen@intel.com>
    Reviewed-by: Yang, Bin <bin.yang@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Tested-by: Tang, HaifengX <haifengx.tang@intel.com>
    Reviewed-by: Koskinen, Ilkka <ilkka.koskinen@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/hwmon/intel_basincove_gpadc.c
+++ drivers/hwmon/intel_basincove_gpadc.c
@@ -38,6 +38,8 @@
 #define GPADCREQ	0xDC
 #define GPADCREQ_IRQEN	(1 << 1)
 #define GPADCREQ_BUSY	(1 << 0)
+#define MIRQLVL1_ADC	(1 << 4)
+#define MIRQLVL1	0x0C
 #define ADC1CNTL	0xDD
 #define ADCIRQ		0x06
 #define MADCIRQ		0x11
@@ -72,6 +74,7 @@ struct gpadc_info {
 	u8 irq_status;
 	wait_queue_head_t wait;
 	int sample_done;
+	void __iomem *intr;
 };
 
 static struct gpadc_info gpadc_info;
@@ -133,7 +136,7 @@ static irqreturn_t gpadc_isr(int irq, void *data)
 {
 	struct gpadc_info *info = data;
 
-	gpadc_read(GPADCREQ, &info->irq_status);
+	info->irq_status = ioread8(info->intr);
 	info->sample_done = 1;
 	wake_up(&info->wait);
 	return IRQ_HANDLED;
@@ -197,40 +200,162 @@ int intel_basincove_gpadc_sample(int ch, struct gpadc_result *res)
 	}
 
 done:
+	gpadc_clear_bits(MIRQLVL1, MIRQLVL1_ADC);
 	mutex_unlock(&info->lock);
 	return ret;
 }
 EXPORT_SYMBOL(intel_basincove_gpadc_sample);
 
+static struct gpadc_result sample_result;
+static int chs;
+
+static ssize_t intel_basincove_gpadc_store_channel(struct device *dev,
+					struct device_attribute *attr,
+					const char *buf, size_t size)
+{
+	if (sscanf(buf, "%x", &chs) != 1) {
+		dev_err(dev, "one channel argument is needed\n");
+		return -EINVAL;
+	}
+
+	if (chs < (1 << 0) || chs >= (1 << GPADC_CH_NUM)) {
+		dev_err(dev, "invalid channel, should be in [0x1 - 0x1FF]\n");
+		return -EINVAL;
+	}
+
+	return size;
+}
+
+static ssize_t intel_basincove_gpadc_show_channel(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "0x%x\n", chs);
+}
+
+static ssize_t intel_basincove_gpadc_store_sample(struct device *dev,
+					struct device_attribute *attr,
+					const char *buf, size_t size)
+{
+	int value, ret;
+
+	memset(sample_result.data, 0, sizeof(sample_result.data));
+
+	if (sscanf(buf, "%d", &value) != 1) {
+		dev_err(dev, "one argument is needed\n");
+		return -EINVAL;
+	}
+
+	if (value == 1) {
+		ret = intel_basincove_gpadc_sample(chs, &sample_result);
+		if (ret) {
+			dev_err(dev, "sample failed\n");
+			return ret;
+		}
+	} else {
+		dev_err(dev, "input '1' to sample\n");
+		return -EINVAL;
+	}
+
+	return size;
+}
+
+static ssize_t intel_basincove_gpadc_show_result(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	int i;
+	int used = 0;
+
+	for (i = 0; i < GPADC_CH_NUM; i++) {
+		used += snprintf(buf + used, PAGE_SIZE - used,
+			 "sample_result[%d] = %d\n", i, sample_result.data[i]);
+	}
+
+	return used;
+}
+
+
+static DEVICE_ATTR(channel, S_IWUSR | S_IRUGO,
+		intel_basincove_gpadc_show_channel,
+		intel_basincove_gpadc_store_channel);
+static DEVICE_ATTR(sample, S_IWUSR, NULL, intel_basincove_gpadc_store_sample);
+static DEVICE_ATTR(result, S_IRUGO, intel_basincove_gpadc_show_result, NULL);
+
+static struct attribute *intel_basincove_gpadc_attrs[] = {
+	&dev_attr_channel.attr,
+	&dev_attr_sample.attr,
+	&dev_attr_result.attr,
+	NULL,
+};
+
+static struct attribute_group intel_basincove_gpadc_attr_group = {
+	.name = "basincove_gpadc",
+	.attrs = intel_basincove_gpadc_attrs,
+};
+
 static int __devinit gpadc_probe(struct ipc_device *ipcdev)
 {
 	struct gpadc_info *info = &gpadc_info;
+	struct intel_basincove_gpadc_platform_data *pdata =
+			ipcdev->dev.platform_data;
 	int err;
 	u8 mask;
+
+	if (!pdata) {
+		dev_err(&ipcdev->dev, "no platform data supplied\n");
+		return -EINVAL;
+	}
 
 	mutex_init(&info->lock);
 	init_waitqueue_head(&info->wait);
 	info->dev = &ipcdev->dev;
 	info->irq = ipc_get_irq(ipcdev, 0);
+	info->intr = ioremap_nocache(pdata->intr, 1);
+	if (!info->intr) {
+		dev_err(&ipcdev->dev, "ioremap of ADCIRQ failed\n");
+		err = -ENOMEM;
+		goto exit;
+	}
+
 	mask = MBATTEMP | MSYSTEMP | MBATT | MVIBATT | MCCTICK;
 	gpadc_clear_bits(MADCIRQ, mask);
+	gpadc_clear_bits(MIRQLVL1, MIRQLVL1_ADC);
 
 	err = request_threaded_irq(info->irq, NULL, gpadc_isr,
 			IRQF_ONESHOT, "adc", info);
 	if (err) {
 		gpadc_dump(info);
 		dev_err(&ipcdev->dev, "unable to register irq %d\n", info->irq);
-		return err;
+		goto err_iounmap;
+	}
+
+	err = sysfs_create_group(&ipcdev->dev.kobj,
+			&intel_basincove_gpadc_attr_group);
+	if (err) {
+		dev_err(&ipcdev->dev, "Unable to export sysfs interface, error: %d\n",
+			err);
+		goto err_release_irq;
 	}
 
 	info->initialized = 1;
+
 	return 0;
+
+err_release_irq:
+	free_irq(info->irq, info);
+err_iounmap:
+	iounmap(info->intr);
+exit:
+	return err;
 }
 
 static int __devexit gpadc_remove(struct ipc_device *ipcdev)
 {
 	struct gpadc_info *info = &gpadc_info;
+
+	sysfs_remove_group(&ipcdev->dev.kobj,
+			&intel_basincove_gpadc_attr_group);
 	free_irq(info->irq, info);
+	iounmap(info->intr);
 	return 0;
 }
 
