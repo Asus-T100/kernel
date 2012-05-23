527d2e1c0a0d8d3308fa4359ededd1790f4f1556
Dale Stimson <dale.b.stimson@intel.com>
2012-05-23 12:37:26 -0700

    drivers/gpu/drm/ttm: correct ttm_get_pages page clear for highmem
    If flag TTM_PAGE_FLAG_ZERO_ALLOC is set then function ttm_get_pages
    attempts to clear pages taken from the ttm page pool.  If such a page
    is in highmem, then it has no virtual address and the clear_page attempt
    resulted in a NULL pointer reference.
    
    Change-Id: I6fef2a704550144bd0851a0e5a3d5783350254bd
    Signed-off-by: Dale Stimson <dale.b.stimson@intel.com>

--- drivers/gpu/drm/ttm/ttm_page_alloc.c
+++ drivers/gpu/drm/ttm/ttm_page_alloc.c
@@ -708,7 +708,14 @@ int ttm_get_pages(struct list_head *pages, int flags,
 	/* clear the pages coming from the pool if requested */
 	if (flags & TTM_PAGE_FLAG_ZERO_ALLOC) {
 		list_for_each_entry(p, pages, lru) {
-			clear_page(page_address(p));
+#ifdef CONFIG_HIGHMEM
+			if (PageHighMem(p))
+				clear_highpage(p);
+			else
+#endif /* ifdef CONFIG_HIGHMEM */
+			{
+				clear_page(page_address(p));
+			}
 		}
 	}
 
