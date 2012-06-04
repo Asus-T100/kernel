f04d9531a481f5c84e90860a42289a47def4db53
Wu, Hao <hao.wu@intel.com>
2012-06-04 19:20:22 +0800

    [PORT FROM R3]usb/penwell_otg_test: use usb warning code instead of error code
    BZ: 39874
    
    Use usb warning code will be more readable than just error code.
    
    Change-Id: Ieef174ac5a9064e5a8243712a9dcc0ec88eecf30
    Signed-off-by: Wu, Hao <hao.wu@intel.com>
    Reviewed-on: http://android.intel.com:8080/57554
    Reviewed-by: Tang, Jianqiang <jianqiang.tang@intel.com>
    Reviewed-by: Tang, Richard <richard.tang@intel.com>
    Reviewed-by: Zhuang, Jin Can <jin.can.zhuang@intel.com>
    Reviewed-by: Meng, Zhe <zhe.meng@intel.com>
    Tested-by: Meng, Zhe <zhe.meng@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/usb/otg/penwell_otg_test.c
+++ drivers/usb/otg/penwell_otg_test.c
@@ -131,7 +131,7 @@ pnwotg_test_probe(struct usb_interface *intf, const struct usb_device_id *id)
 	default:
 		dev_info(&intf->dev, "unknown test mode with PID 0x%04x",
 			id->idProduct);
-		usb_notify_warning(dev->udev, -EINVAL);
+		usb_notify_warning(dev->udev, USB_WARNING_NOT_SUPPORT);
 	}
 
 	return 0;
