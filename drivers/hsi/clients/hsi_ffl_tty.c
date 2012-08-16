59428f47deb96f364f1901fb6f48c33931f3efa3
xiao jin <jin.xiao@intel.com>
2012-08-16 17:07:17 +0800

    [PORT FROM R3] [REVERT ME] hsi-ffl: increase tx hangup timer to 500ms
    BZ: 52533
    
    We have seen HSI TX timeout existed for a long time, it will ends
    the IPC between AP and modem.
    
    We find that HSI TX timeout happened because other driver disable
    local irq for a long time, usually more than 100ms.
    For example, now HSU serial qwork disable local irq about 150ms in
    some cases.
    
    To increase tx hangup timer is an effective way to decrease HSI TX
    timeout.
    
    By increasing hangup timer to 500ms, we don't see the HSI TX
    timeout within HTA component test.
    
    Change-Id: I060fdb912cda78bd1a51cf51b35175dbf725031f
    Orig-Change-Id: Ic2e5923b3b9ae6f63e598e1cd7f535a70bf3c421
    Signed-off-by: xiao jin <jin.xiao@intel.com>
    Reviewed-on: http://android.intel.com:8080/63570
    Reviewed-by: Predon, Frederic <frederic.predon@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/hsi/clients/hsi_ffl_tty.c
+++ drivers/hsi/clients/hsi_ffl_tty.c
@@ -66,7 +66,7 @@
 #define FFL_RX_DELAY		100000
 
 /* Defaut TX timeout delay expressed in microseconds */
-#define TTY_HANGUP_DELAY	100000
+#define TTY_HANGUP_DELAY	500000
 
 /* Delays for powering up/resetting the modem, ms */
 #define PO_INTERLINE_DELAY	1
