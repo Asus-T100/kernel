612e5a8153cee4f65292d7e52d444d9d54bcb957
xiaobing tu <xiaobing.tu@intel.com>
2012-09-21 11:18:36 +0800

    [HACK :REVERT ME]atomisp:IPANIC happened when capturing image in touch focus  mode
    BZ: 57812
    
    use kmap and kunmap on atomic context, it will trigger a panic.
    
    use kmap_atomic and kunmap_atomic, these function can be used in
    atomic context
    
    Signed-off-by: xiaobing tu <xiaobing.tu@intel.com>
    Signed-off-by: yanmin zhang <yanmin.zhang@intel.com>
    
    Change-Id: I7f3a2ac1d29987e3a88231729db349e42107bf48
    Reviewed-on: http://android.intel.com:8080/67302
    Reviewed-by: Ailus, Sakari <sakari.ailus@intel.com>
    Reviewed-by: Toivonen, Tuukka <tuukka.toivonen@intel.com>
    Reviewed-by: Cohen, David A <david.a.cohen@intel.com>
    Reviewed-by: Tao, Jing <jing.tao@intel.com>
    Reviewed-by: Tu, Xiaobing <xiaobing.tu@intel.com>
    Reviewed-by: Koski, Anttu <anttu.koski@intel.com>
    Tested-by: Koski, Anttu <anttu.koski@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/media/video/atomisp/hmm/hmm.c
+++ drivers/media/video/atomisp/hmm/hmm.c
@@ -346,7 +346,11 @@ int hmm_set(void *virt, int c, unsigned int bytes)
 		idx = (ptr - bo->vm_node->start) >> PAGE_SHIFT;
 		offset = (ptr - bo->vm_node->start) - (idx << PAGE_SHIFT);
 
-		des = (char *)kmap(bo->pages[idx]);
+		if (in_atomic())
+			des = (char *)kmap_atomic(bo->pages[idx]);
+		else
+			des = (char *)kmap(bo->pages[idx]);
+
 		if (!des) {
 			v4l2_err(&atomisp_dev,
 				    "kmap buffer object page failed: "
@@ -367,7 +371,20 @@ int hmm_set(void *virt, int c, unsigned int bytes)
 
 		memset(des, c, len);
 
-		kunmap(bo->pages[idx]);
+		/*
+		 * Note: kunmap_atomic requires return addr from
+		 * kmap_atomic, not the page. See linux/highmem.h
+		 */
+		if (in_atomic())
+			/*
+			 * Note: kunmap_atomic requires return addr from
+			 * kmap_atomic, not the page. See linux/highmem.h
+			 */
+			kunmap_atomic(des - offset);
+		else
+			kunmap(bo->pages[idx]);
+
+
 	}
 
 	return 0;
