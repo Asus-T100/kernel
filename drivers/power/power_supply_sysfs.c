3700cf364cd3fc0492a04722fcfa66e462dc5b66
Jenny TC <jenny.tc@intel.com>
2012-10-15 21:05:52 +0530

    power_supply: Add support for CHARGE_CONTROL_* attributes
    BZ: 61954
    
    Add support for power supply attributes CHARGE_CONTROL_LIMIT
    and CHARGE_CONTROL_LIMIT_MAX.
    
    Change-Id: I274b512b2be3d5b14e5efbb223050d6f47b2e493
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    Reviewed-on: http://android.intel.com:8080/71608
    Reviewed-by: Tc, Jenny <jenny.tc@intel.com>
    Reviewed-by: Jena, TapanX <tapanx.jena@intel.com>
    Tested-by: Kallappa Manjanna, MadhukumarX <madhukumarx.kallappa.manjanna@intel.com>
    Reviewed-by: lab_cactus <lab_cactus@intel.com>
    Tested-by: lab_cactus <lab_cactus@intel.com>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -153,6 +153,8 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(charge_avg),
 	POWER_SUPPLY_ATTR(charge_counter),
 	POWER_SUPPLY_ATTR(charge_current_limit),
+	POWER_SUPPLY_ATTR(charge_control_limit),
+	POWER_SUPPLY_ATTR(charge_control_limit_max),
 	POWER_SUPPLY_ATTR(energy_full_design),
 	POWER_SUPPLY_ATTR(energy_empty_design),
 	POWER_SUPPLY_ATTR(energy_full),
