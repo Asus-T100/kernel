bfc0b8633f3a95a316476bdc5c8241172496f5c6
Yun Tu(Mark Tu) <yun.tu@intel.com>
2012-10-17 17:33:14 +0800

    [GFX-3D]add service queue flip order check
    BZ:62633
    
    detect is there a  wrong order of completeness in
    queue service/flip code
    
    Change-Id: Ie1c818013478f3c5c42f2b0bd77d634efd4c7dc2
    Signed-off-by: Yun Tu(Mark Tu) <yun.tu@intel.com>
    Reviewed-on: http://android.intel.com:8080/71812
    Reviewed-by: Xu, Randy <randy.xu@intel.com>
    Tested-by: Xu, Randy <randy.xu@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/staging/mrst/pvr/services4/srvkm/common/queue.c
+++ drivers/staging/mrst/pvr/services4/srvkm/common/queue.c
@@ -70,6 +70,11 @@ typedef struct _DEVICE_COMMAND_DATA_
 } DEVICE_COMMAND_DATA;
 
 
+static IMG_UINT32 g_ui32OutStamp = 0;
+static IMG_UINT32 g_ui32InStamp = 0;
+//static IMG_HANDLE g_TimerHandle = IMG_NULL;
+
+
 #if defined(__linux__) && defined(__KERNEL__)
 
 #include "proc.h"
@@ -973,6 +978,35 @@ PVRSRV_ERROR CheckIfSyncIsQueued(PVRSRV_SYNC_OBJECT *psSync, COMMAND_COMPLETE_DA
 	return PVRSRV_ERROR_FAILED_DEPENDENCIES;
 }
 
+#if 0
+static IMG_VOID _CommandCompleteTimeout(IMG_PVOID pvData)
+{
+	COMMAND_COMPLETE_DATA *psCmdCompleteData = pvData;
+	IMG_UINT32 ui32SyncCounter;
+
+	PVR_DPF((PVR_DBG_ERROR, "Timeout fired for operation %d", psCmdCompleteData->ui32Stamp));
+
+	for (ui32SyncCounter = 0;
+		 ui32SyncCounter < psCmdCompleteData->ui32SrcSyncCount;
+		 ui32SyncCounter++)
+	{
+		QueueDumpCmdComplete(psCmdCompleteData, ui32SyncCounter, IMG_TRUE);
+	}
+
+	for (ui32SyncCounter = 0;
+		 ui32SyncCounter < psCmdCompleteData->ui32DstSyncCount;
+		 ui32SyncCounter++)
+	{
+		QueueDumpCmdComplete(psCmdCompleteData, ui32SyncCounter, IMG_FALSE);
+	}
+	/*Don't delete here, as the Flip timer will flush flip queue, and will be deleted there
+	 *or race condition may happen*/
+	/*OSDisableTimer(g_TimerHandle);
+	OSRemoveTimer(g_TimerHandle);
+	g_TimerHandle = IMG_NULL;*/
+}
+#endif
+
 /*!
 ******************************************************************************
 
@@ -1126,6 +1160,8 @@ PVRSRV_ERROR PVRSRVProcessCommand(SYS_DATA			*psSysData,
 				ui32CCBOffset));
 	}
 
+	psCmdCompleteData->ui32Stamp = g_ui32OutStamp++;
+
 	/*
 		call the cmd specific handler:
 		it should:
@@ -1146,8 +1182,27 @@ PVRSRV_ERROR PVRSRVProcessCommand(SYS_DATA			*psSysData,
 			free cmd complete structure
 		*/
 		psCmdCompleteData->bInUse = IMG_FALSE;
+		g_ui32InStamp++;
 		eError = PVRSRV_ERROR_CMD_NOT_PROCESSED;
 	}
+/*temporarily disable the timer as we have flip timer and they have race now*/
+#if 0
+	if ((g_ui32OutStamp - g_ui32InStamp) == DC_NUM_COMMANDS_PER_TYPE)
+	{
+		/*
+			We've just sent out a new flip which has filled the DC's pipeline.
+			This means that we expect a complete within a VSync period, start
+			a timer that will print out a message if we haven't got a complete
+			within a reasonable period (200ms)
+		*/
+		if (g_TimerHandle != IMG_NULL) {
+			PVR_DPF((PVR_DBG_ERROR, "service queue debug timer is already in use"));
+		} else {
+			g_TimerHandle = OSAddTimer(_CommandCompleteTimeout, psCmdCompleteData, 200);
+			OSEnableTimer(g_TimerHandle);
+		}
+	}
+#endif
 	
 	/* Increment the CCB offset */
 	psDeviceCommandData[psCommand->CommandType].ui32CCBOffset = (ui32CCBOffset + 1) % DC_NUM_COMMANDS_PER_TYPE;
@@ -1302,6 +1357,24 @@ IMG_VOID PVRSRVCommandCompleteKM(IMG_HANDLE	hCmdCookie,
 	SYS_DATA				*psSysData;
 
 	SysAcquireData(&psSysData);
+
+#if 0
+	if (g_TimerHandle)
+	{
+		OSDisableTimer(g_TimerHandle);
+		OSRemoveTimer(g_TimerHandle);
+		g_TimerHandle = IMG_NULL;
+	}
+#endif
+
+	if (psCmdCompleteData->ui32Stamp != g_ui32InStamp)
+	{
+		PVR_DPF((PVR_DBG_ERROR, "PVRSRVCommandCompleteKM: Complete arrived in unexpected order (got %d expecting %d)",
+				psCmdCompleteData->ui32Stamp,
+				g_ui32InStamp));
+	}
+
+	g_ui32InStamp++;
 
 	PVR_TTRACE(PVRSRV_TRACE_GROUP_QUEUE, PVRSRV_TRACE_CLASS_CMD_COMP_START,
 			QUEUE_TOKEN_COMMAND_COMPLETE);
