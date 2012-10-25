adae54084fd1c540bc78525e2f20982ea69a1c78
Marco Sinigaglia <msinigaX>
2012-10-25 18:15:38 +0200

    mmc: BRCM WLAN Virtual detect and gpio enable Int
    BZ: 64763
    
    BRCM advised to implement a new mechanism for wifi
    sdio card detect and power enabling.
    
    The BRCM driver drives the WLAN enable line,
    hence the gpio wlan enable line is not controlled
    anymore by the fixed voltage regulator.
    
    The BRCM driver has to force the mmc_detect.
    A new function, called "sdhci_hsmmc_virtual_detect"
    and defined in sdhci-pci.c, calls mmc_detect forcing
    to attach/detach the WLAN from the sdio bus.
    The function pointer to this function is passed to
    the BRCM driver.
    
    When the wlan is disbaled, the mmc_detect call has
    to detach the bus. This is allowed by the kernel only
    when MMC_CAP_NONREMOVABLE is not defined.
    
    Change-Id: I94cf5b0da5b93de7606d9fd0261fe8d721c6f886
    Signed-off-by: Marco Sinigaglia <marcox.sinigaglia@intel.com>
    Reviewed-on: http://android.intel.com:8080/71853
    Reviewed-by: lab_cactus <lab_cactus@intel.com>
    Reviewed-by: Gao, Yunpeng <yunpeng.gao@intel.com>
    Reviewed-by: Casasnovas, QuentinX <quentinx.casasnovas@intel.com>
    Reviewed-by: Trivelly, Jean <jean.trivelly@intel.com>
    Reviewed-by: Gross, Mark <mark.gross@intel.com>
    Reviewed-by: Bachot, Jean-michel <jean-michel.bachot@intel.com>
    Tested-by: Sun, Jianhua <jianhua.sun@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_mmc_sdhci_pci.c
+++ arch/x86/platform/intel-mid/device_libs/platform_mmc_sdhci_pci.c
@@ -16,6 +16,7 @@
 #include <linux/mmc/sdhci-pci-data.h>
 #include "platform_mmc_sdhci_pci.h"
 
+
 static struct sdhci_pci_data mfd_clv_emmc0_data;
 static struct sdhci_pci_data mfd_clv_emmc1_data;
 static struct sdhci_pci_data mfd_clv_sd_data;
@@ -31,6 +32,17 @@ static struct sdhci_pci_data mrfl_sdio_data;
 int bcmdhd_get_sdhci_quirk(void)
 {
 	return 0;
+}
+
+int bcmdhd_get_sdhci_mmc_caps(void)
+{
+	return 0;
+}
+
+void bcmdhd_register_embedded_control(void *dev_id, void (*virtual_cd)
+					(void *dev_id, int card_present))
+{
+	return;
 }
 
 static struct sdhci_pci_data *sdhci_host_get_data(struct pci_dev *pdev,
@@ -132,7 +144,12 @@ static int __init sdhci_pci_platform_data_init(void)
 
 	mfd_clv_sdio_data.rst_n_gpio = -EINVAL;
 	mfd_clv_sdio_data.cd_gpio = -EINVAL;
+
 	mfd_clv_sdio_data.quirks = bcmdhd_get_sdhci_quirk();
+	mrfl_sdio_data.register_embedded_control =
+					bcmdhd_register_embedded_control;
+	mrfl_sdio_data.mmc_caps = bcmdhd_get_sdhci_mmc_caps();
+
 
 	memset(&mrfl_emmc0_data, 0, sizeof(struct sdhci_pci_data));
 	memset(&mrfl_emmc1_data, 0, sizeof(struct sdhci_pci_data));
@@ -157,12 +174,18 @@ static int __init sdhci_pci_platform_data_init(void)
 	mrfl_sdio_data.setup = mrfl_sdio_setup;
 	mrfl_sdio_data.cleanup = mrfl_sdio_cleanup;
 	mrfl_sdio_data.quirks = bcmdhd_get_sdhci_quirk();
+	mrfl_sdio_data.register_embedded_control =
+				bcmdhd_register_embedded_control;
+	mrfl_sdio_data.mmc_caps = bcmdhd_get_sdhci_mmc_caps();
 
 	memset(&mfld_sdio_data, 0, sizeof(struct sdhci_pci_data));
 
 	mfld_sdio_data.rst_n_gpio = -EINVAL;
 	mfld_sdio_data.cd_gpio = -EINVAL;
 	mfld_sdio_data.quirks = bcmdhd_get_sdhci_quirk();
+	mfld_sdio_data.register_embedded_control =
+				bcmdhd_register_embedded_control;
+	mfld_sdio_data.mmc_caps = bcmdhd_get_sdhci_mmc_caps();
 
 	sdhci_pci_get_data = sdhci_host_get_data;
 
--- drivers/mmc/host/sdhci-pci.c
+++ drivers/mmc/host/sdhci-pci.c
@@ -33,6 +33,9 @@
 
 #include "sdhci.h"
 
+#define DELAY_CARD_INSERTED		200
+#define DELAY_CARD_REMOVED		50
+
 /*
  * PCI registers
  */
@@ -1428,6 +1431,19 @@ static const struct dev_pm_ops sdhci_pci_pm_ops = {
 	.runtime_idle = sdhci_pci_runtime_idle,
 };
 
+
+static void sdhci_hsmmc_virtual_detect(void *dev_id, int carddetect)
+{
+	struct sdhci_host *host = dev_id;
+
+	if (carddetect)
+		mmc_detect_change(host->mmc,
+				msecs_to_jiffies(DELAY_CARD_INSERTED));
+	else
+		mmc_detect_change(host->mmc,
+				msecs_to_jiffies(DELAY_CARD_REMOVED));
+}
+
 /*****************************************************************************\
  *                                                                           *
  * Device probing/removal                                                    *
@@ -1498,6 +1514,10 @@ static struct sdhci_pci_slot * __devinit sdhci_pci_probe_slot(
 
 		if (slot->data->quirks)
 			host->quirks2 |= slot->data->quirks;
+
+		if (slot->data->register_embedded_control)
+			slot->data->register_embedded_control(host,
+						sdhci_hsmmc_virtual_detect);
 	}
 
 	host->irq = pdev->irq;
@@ -1536,6 +1556,9 @@ static struct sdhci_pci_slot * __devinit sdhci_pci_probe_slot(
 	ret = sdhci_add_host(host);
 	if (ret)
 		goto remove;
+
+	if (slot->data && slot->data->mmc_caps)
+		host->mmc->caps &= !slot->data->mmc_caps;
 
 	sdhci_pci_add_own_cd(slot);
 
