00d5f9fd3b632a9158db92d53c052f2ac8a3ad8c
Dale Stimson <dale.b.stimson@intel.com>
2012-11-01 08:55:07 -0700

    intel-mid: Support msgbus addresses over 8 bits.
    BZ: 65904
    
    The msgbus access functions in intel_mid.c supported only 8-bit
    msgbus addresses.  Add support for 32-bit addresses.
    
    Change-Id: I89232f553e479927a893c86c50477342a82d9720
    Signed-off-by: Dale Stimson <dale.b.stimson@intel.com>
    Reviewed-on: http://android.intel.com:8080/72488
    Reviewed-by: Gao, Bin <bin.gao@intel.com>
    Reviewed-by: Gross, Mark <mark.gross@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Tested-by: Ng, Cheon-woei <cheon-woei.ng@intel.com>
    Reviewed-by: Mahe, Erwan <erwan.mahe@intel.com>
    Tested-by: Mahe, Erwan <erwan.mahe@intel.com>

--- arch/x86/include/asm/intel-mid.h
+++ arch/x86/include/asm/intel-mid.h
@@ -16,18 +16,21 @@
 #include <linux/platform_device.h>
 
 /*
- * Access to message bus through these 2 registers
+ * Access to message bus through three registers
  * in CUNIT(0:0:0) PCI configuration space.
  * MSGBUS_CTRL_REG(0xD0):
  *   31:24	= message bus opcode
  *   23:16	= message bus port
- *   15:8	= message bus address
+ *   15:8	= message bus address, low 8 bits.
  *   7:4	= message bus byte enables
- * MSGBUS_DTAT_REG(0xD4):
+ * MSGBUS_CTRL_EXT_REG(0xD8):
+ *   31:8	= message bus address, high 24 bits.
+ * MSGBUS_DATA_REG(0xD4):
  *   hold the data for write or read
  */
 #define PCI_ROOT_MSGBUS_CTRL_REG	0xD0
 #define PCI_ROOT_MSGBUS_DATA_REG	0xD4
+#define PCI_ROOT_MSGBUS_CTRL_EXT_REG	0xD8
 #define PCI_ROOT_MSGBUS_READ		0x10
 #define PCI_ROOT_MSGBUS_WRITE		0x11
 #define PCI_ROOT_MSGBUS_DWORD_ENABLE	0xf0
@@ -49,8 +52,8 @@ extern struct sfi_rtc_table_entry sfi_mrtc_array[];
 extern int intel_mid_create_property(const struct attribute *attr);
 extern u32 intel_mid_msgbus_read32_raw(u32 cmd);
 extern void intel_mid_msgbus_write32_raw(u32 cmd, u32 data);
-extern u32 intel_mid_msgbus_read32(u8 port, u8 addr);
-extern void intel_mid_msgbus_write32(u8 port, u8 addr, u32 data);
+extern u32 intel_mid_msgbus_read32(u8 port, u32 addr);
+extern void intel_mid_msgbus_write32(u8 port, u32 addr, u32 data);
 
 /*
  * Here defines the array of devices platform data that IAFW would export
