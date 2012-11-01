3f67b063339e0ea368ef0fede61dc0973834a4ef
Yunpeng Gao <yunpeng.gao@intel.com>
2012-11-01 19:42:17 +0800

    Allow SDIO v3.0 device recognized
    BZ: 65886
    
    Change check condition in sdio core driver to allow
    SDIO v3.0 device recognized.
    
    Change-Id: I8e7894800c989242789844d2b19ad21f145ce1f3
    Signed-off-by: Yunpeng Gao <yunpeng.gao@intel.com>
    Reviewed-on: http://android.intel.com:8080/72473
    Reviewed-by: Trivelly, Jean <jean.trivelly@intel.com>
    Reviewed-by: Bachot, Jean-michel <jean-michel.bachot@intel.com>
    Reviewed-by: lab_cactus <lab_cactus@intel.com>
    Tested-by: lab_cactus <lab_cactus@intel.com>

--- include/linux/mmc/sdio.h
+++ include/linux/mmc/sdio.h
@@ -79,6 +79,7 @@
 #define  SDIO_SDIO_REV_1_10	1	/* SDIO Spec Version 1.10 */
 #define  SDIO_SDIO_REV_1_20	2	/* SDIO Spec Version 1.20 */
 #define  SDIO_SDIO_REV_2_00	3	/* SDIO Spec Version 2.00 */
+#define  SDIO_SDIO_REV_3_00	4	/* SDIO Spec Version 3.00 */
 
 #define SDIO_CCCR_SD		0x01
 
