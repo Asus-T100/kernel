e5896674eabac597f586462099b783868b09b00a
Chuanxiao Dong <chuanxiao.dong@intel.com>
2012-11-12 10:04:10 +0800

    mmc: block: check stop command errors
    BZ: 67260
    
    This patch is ported from ENZO
    
    For some bad SD cards, it will always stay in program state, which
    can result in mmcqd thread dead loop in mmc_blk_err_check();
    
    This change will check the stop command error code to avoid the dead
    loop and break the loop in case.
    
    Change-Id: I9b30ba19df75db7659f2850eac23ee1f196ee97a
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-on: http://android.intel.com:8080/73516
    Reviewed-by: lab_cactus <lab_cactus@intel.com>
    Tested-by: Sun, Jianhua <jianhua.sun@intel.com>
    Reviewed-by: Tang, Richard <richard.tang@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -749,6 +749,11 @@ static int mmc_blk_cmd_recovery(struct mmc_card *card, struct request *req,
 		return mmc_blk_cmd_error(req, "r/w cmd", brq->cmd.error,
 				prev_cmd_status_valid, status);
 
+	/* Check for stop cmd errors */
+	if (mmc_card_sd(card) && brq->stop.error == -ETIMEDOUT)
+		return mmc_blk_cmd_error(req, "stop cmd", brq->stop.error,
+				prev_cmd_status_valid, status);
+
 	/* Data errors */
 	if (!brq->stop.error) {
 		/*
@@ -1187,6 +1192,7 @@ static int mmc_blk_err_check(struct mmc_card *card,
 	 */
 	if (!mmc_host_is_spi(card->host) && rq_data_dir(req) != READ) {
 		u32 status;
+		u32 timeout = 100; /* 10s timeout for PRG state */
 		do {
 			int err = get_card_status(card, &status, 5);
 			if (err) {
@@ -1194,13 +1200,20 @@ static int mmc_blk_err_check(struct mmc_card *card,
 				       req->rq_disk->disk_name, err);
 				return MMC_BLK_CMD_ERR;
 			}
+			if (!(status & R1_READY_FOR_DATA) ||
+				(R1_CURRENT_STATE(status) == R1_STATE_PRG)) {
+				timeout--;
+				msleep(100);
+			} else
+				break;
 			/*
 			 * Some cards mishandle the status bits,
 			 * so make sure to check both the busy
 			 * indication and the card state.
 			 */
-		} while (!(status & R1_READY_FOR_DATA) ||
-			 (R1_CURRENT_STATE(status) == R1_STATE_PRG));
+		} while (timeout);
+		if (timeout == 0)
+			return MMC_BLK_ABORT;
 	}
 
 	if (brq->data.error) {
