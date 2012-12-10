f90e656c5a6224923572fa86661609c4fafd63af
Chuanxiao Dong <chuanxiao.dong@intel.com>
2012-12-10 11:30:06 +0800

    [PORT FROM K3.0]mmc: host: disable led support
    BZ: 47302
    
    In phone/table project, there is no led which indicate that
    eMMC/SD card is used for transferring data. So disable led support
    
    Change-Id: I29587c601843db5d1b50276b00d9196c287c40eb
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-on: http://android.intel.com:8080/80173
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Tested-by: Yang, Fei <fei.yang@intel.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -276,6 +276,7 @@ struct mmc_host {
 #define MMC_CAP2_INIT_CARD_SYNC	(1 << 10)	/* init card in sync mode */
 #define MMC_CAP2_RPMBPART_NOACC	(1 << 11)	/* RPMB partition no access */
 #define MMC_CAP2_POLL_R1B_BUSY	(1 << 12)	/* host poll R1B busy*/
+#define MMC_CAP2_LED_SUPPORT	(1 << 13)	/* led support */
 
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 	unsigned int        power_notify_type;
