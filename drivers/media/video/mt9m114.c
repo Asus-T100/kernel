910a24b614b97d7bde27f14ff7f871ae3833e2d7
Ilkka Koskinen <ilkka.koskinen@intel.com>
2012-12-12 16:20:01 +0200

    [FORKLIFT FROM K3.0] mt9m114: Driver update from 3.0
    BZ: 79949
    
    The latest patch included is
    318886d mt9m114: remove poll loop from streamon
    
    Change-Id: I2e23c867c813e3a4cdd121ccef798666152f0bac
    Signed-off-by: Ilkka Koskinen <ilkka.koskinen@intel.com>
    Reviewed-on: http://android.intel.com:8080/85226
    Reviewed-by: Koski, Anttu <anttu.koski@intel.com>
    Tested-by: Koski, Anttu <anttu.koski@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/media/video/mt9m114.c
+++ drivers/media/video/mt9m114.c
@@ -414,38 +414,6 @@ static int mt9m114_write_reg_array(struct i2c_client *client,
 	return 0;
 }
 
-
-static int mt9m114_wait_3a(struct v4l2_subdev *sd)
-{
-	struct i2c_client *client = v4l2_get_subdevdata(sd);
-	int timeout = 35;
-	int ret;
-	unsigned int status_exp, status_wb;
-
-	while (timeout--) {
-		ret = mt9m114_read_reg(client, MISENSOR_16BIT,
-			MISENSOR_AE_TRACK_STATUS, &status_exp);
-		if (ret)
-			return ret;
-		if (!(status_exp & MISENSOR_AE_READY)) {
-			msleep(20);
-			continue;
-		}
-		ret = mt9m114_read_reg(client,
-			MISENSOR_16BIT, MISENSOR_AWB_STATUS, &status_wb);
-		if (ret)
-			return ret;
-		if (status_wb & MISENSOR_AWB_STEADY) {
-			dev_dbg(&client->dev, "ae/awb stablize retry count  %d.\n",
-				  (35-timeout));
-			return 0;
-		}
-		msleep(20);
-	}
-
-	return -EINVAL;
-}
-
 static int mt9m114_wait_state(struct i2c_client *client, int timeout)
 {
 	int ret;
@@ -874,6 +842,32 @@ static int mt9m114_s_freq(struct v4l2_subdev *sd, s32  val)
 	return ret;
 }
 
+static int mt9m114_g_2a_status(struct v4l2_subdev *sd, s32 *val)
+{
+	struct i2c_client *client = v4l2_get_subdevdata(sd);
+	int ret;
+	unsigned int status_exp, status_wb;
+	*val = 0;
+
+	ret = mt9m114_read_reg(client, MISENSOR_16BIT,
+			MISENSOR_AE_TRACK_STATUS, &status_exp);
+	if (ret)
+		return ret;
+
+	ret = mt9m114_read_reg(client, MISENSOR_16BIT,
+			MISENSOR_AWB_STATUS, &status_wb);
+	if (ret)
+		return ret;
+
+	if (status_exp & MISENSOR_AE_READY)
+		*val = V4L2_2A_STATUS_AE_READY;
+
+	if (status_wb & MISENSOR_AWB_STEADY)
+		*val |= V4L2_2A_STATUS_AWB_READY;
+
+	return 0;
+}
+
 static struct mt9m114_control mt9m114_controls[] = {
 	{
 		.qc = {
@@ -952,6 +946,20 @@ static struct mt9m114_control mt9m114_controls[] = {
 			.flags = 0,
 		},
 		.tweak = mt9m114_s_freq,
+	},
+	{
+		.qc = {
+			.id = V4L2_CID_2A_STATUS,
+			.type = V4L2_CTRL_TYPE_BITMASK,
+			.name = "AE and AWB status",
+			.minimum = 0,
+			.maximum = V4L2_2A_STATUS_AE_READY |
+				V4L2_2A_STATUS_AWB_READY,
+			.step = 1,
+			.default_value = 0,
+			.flags = 0,
+		},
+		.query = mt9m114_g_2a_status,
 	},
 
 };
@@ -1175,12 +1183,6 @@ static int mt9m114_s_stream(struct v4l2_subdev *sd, int enable)
 			return ret;
 
 		ret = mt9m114_set_streaming(sd);
-		/*
-		 * here we wait for sensor's 3A algorithm to be
-		 * stablized, as to fix still capture bad 3A output picture
-		 */
-		if (mt9m114_wait_3a(sd))
-			v4l2_warn(c, "3A can not finish!");
 	} else {
 		ret = mt9m114_set_suspend(sd);
 	}
