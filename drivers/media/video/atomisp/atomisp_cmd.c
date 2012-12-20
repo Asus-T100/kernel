66f539ab84ab1dcaa37cb1e8e495a5e2773be6af
Zhi Jin <zhi.jin@intel.com>
2012-12-20 16:49:04 +0200

    [PORT FROM K3.0] atomisp: set the digital zoom in atomisp_set_fmt()
    BZ: 76225
    
    Zoom must be recalculated by calling atomisp_digital_zoom()
    when switching resolution.
    
    Orig-Change-Id: Idbf5568965cb1ff20e0b255f269b627b4afbc2b3
    Originally from: Zhi Jin <zhi.jin@intel.com>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Tuukka Toivonen <tuukka.toivonen@intel.com>
    Signed-off-by: Jianxu Zheng <jian.xu.zheng@intel.com>
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>
    
    Change-Id: Ie727272094db1f2168eacc68412b199f55eaa621
    Reviewed-on: http://android.intel.com:8080/85338
    Reviewed-by: Ailus, Sakari <sakari.ailus@intel.com>
    Tested-by: Koski, Anttu <anttu.koski@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>
    Reviewed-on: http://android.intel.com:8080/88009
    Reviewed-by: Cohen, David A <david.a.cohen@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Tested-by: Patale, Pavan <pavan.patale@intel.com>

--- drivers/media/video/atomisp/atomisp_cmd.c
+++ drivers/media/video/atomisp/atomisp_cmd.c
@@ -3597,14 +3597,15 @@ int atomisp_set_fmt(struct video_device *vdev, struct v4l2_format *f)
 	unsigned int padding_w = pad_w,
 		     padding_h = pad_h;
 	struct v4l2_streamparm sensor_parm;
-	int zoom = 0;
 	int ret;
+	__s32 zoom = 0;
 
 	if ((f->type != V4L2_BUF_TYPE_VIDEO_CAPTURE) &&
 	    (f->type != V4L2_BUF_TYPE_PRIVATE)) {
 		v4l2_err(&atomisp_dev, "Wrong v4l2 buf type\n");
 		return -EINVAL;
 	}
+	atomisp_digital_zoom(isp, 0, &zoom);
 
 	format_bridge = get_atomisp_format_bridge(pixelformat);
 	if (format_bridge == NULL)
