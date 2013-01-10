36257a6755c0da4da765dc6ada4488362567331c
Vincent Becker <vincentx.becker@intel.com>
2013-01-10 10:06:26 +0100

    [AUDIO] SSP modem driver configuration at 48Khz and master mode
    BZ: 66934
    
    To use the SSP driver for mixing and record during CSV call,
    the I2S configuration is in master mode and 48Khz. The kernel
    config switch used for this purpose is SND_INTEL_ALSA_SSP_MODEM_MASTER_MODE.
    
    Change-Id: Iaf18f70b77e2c79d2c2848b8788e7238be4c1693
    Signed-off-by: Vincent Becker <vincentx.becker@intel.com>
    Reviewed-on: http://android.intel.com:8080/86007
    Reviewed-by: De Chivre, Renaud <renaud.de.chivre@intel.com>
    Tested-by: Barthes, FabienX <fabienx.barthes@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- sound/pci/intel_mid_i2s/intel_alsa_ifx_hw_private.h
+++ sound/pci/intel_mid_i2s/intel_alsa_ifx_hw_private.h
@@ -31,6 +31,43 @@
 const struct intel_mid_i2s_settings
 	a_alsa_ifx_stream_settings[INTEL_ALSA_IFX_SND_CARD_MAX_DEVICES] = {
 		{
+#ifdef CONFIG_SND_INTEL_ALSA_SSP_MODEM_MASTER_MODE
+		  .mode = SSP_IN_NETWORK_MODE,
+		  .rx_fifo_interrupt = SSP_RX_FIFO_OVER_INT_ENABLE,
+		  .tx_fifo_interrupt = SSP_TX_FIFO_UNDER_INT_ENABLE,
+		  .frame_format = PSP_FORMAT,
+		  .master_mode_clk_selection = SSP_ONCHIP_CLOCK,
+		  .frame_rate_divider_control = 1,
+		  .master_mode_standard_freq = SSP_FRM_FREQ_48_000,
+		  .data_size = 32,
+		  .tx_tristate_phase = TXD_TRISTATE_LAST_PHASE_OFF,
+		  .tx_tristate_enable = TXD_TRISTATE_ON,
+		  .slave_clk_free_running_status =
+					SLAVE_SSPCLK_ON_DURING_TRANSFER_ONLY,
+		  .sspslclk_direction = SSPSCLK_MASTER_MODE,
+		  .sspsfrm_direction = SSPSFRM_MASTER_MODE,
+		  .ssp_duplex_mode = RX_WITHOUT_TX_MODE,
+		  .ssp_trailing_byte_mode = SSP_TRAILING_BYTE_HDL_BY_IA,
+		  .ssp_tx_dma = SSP_TX_DMA_ENABLE,
+		  .ssp_rx_dma = SSP_RX_DMA_ENABLE,
+		  .ssp_rx_timeout_interrupt_status = SSP_RX_TIMEOUT_INT_DISABLE,
+		  .ssp_trailing_byte_interrupt_status =
+					SSP_TRAILING_BYTE_INT_DISABLE,
+		  .ssp_loopback_mode_status = SSP_LOOPBACK_OFF,
+		  .ssp_rx_fifo_threshold = 8,
+		  .ssp_tx_fifo_threshold = 7,
+		  .ssp_frmsync_timing_bit = NEXT_FRMS_ASS_AFTER_END_OF_T4,
+		  .ssp_frmsync_pol_bit = SSP_FRMS_ACTIVE_LOW,
+		  .ssp_end_transfer_state = SSP_END_DATA_TRANSFER_STATE_LOW,
+		  .ssp_serial_clk_mode = SSP_CLK_MODE_0,
+		  .ssp_psp_T1 = 6,
+		  .ssp_psp_T2 = 2,
+		  .ssp_psp_T4 = 0,
+		  .ssp_psp_T5 = 14,
+		  .ssp_psp_T6 = 16,
+		  .ssp_active_tx_slots_map = 0x01,
+		  .ssp_active_rx_slots_map = 0x01,
+#else
 		  .mode = SSP_IN_NETWORK_MODE,
 		  .rx_fifo_interrupt = SSP_RX_FIFO_OVER_INT_ENABLE,
 		  .tx_fifo_interrupt = SSP_TX_FIFO_UNDER_INT_ENABLE,
@@ -65,6 +102,7 @@ const struct intel_mid_i2s_settings
 		  .ssp_psp_T6 = 1,
 		  .ssp_active_tx_slots_map = 0x01,
 		  .ssp_active_rx_slots_map = 0x01
+#endif
 		}
 };
 
