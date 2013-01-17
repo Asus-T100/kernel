979e5234eb897dd001d667bc8d04b1a19e6b3e21
Bin Gao <bin.gao@intel.com>
2013-01-17 10:59:29 -0800

    [7/8]: mrst_max3111: add merrifield specific platform data to kernel 3.4
    BZ: 81158
    
    Add merrifield specific platform data to kernel 3.4.
    Pieces are ported and extracted from various commites of K 3.0
    
    Change-Id: Iba5870430d55c473d3d5165705e181ad3091b6d6
    Signed-off-by: Bin Gao <bin.gao@intel.com>
    Reviewed-on: http://android.intel.com:8080/87402
    Reviewed-by: Kuppuswamy, Sathyanarayanan <sathyanarayanan.kuppuswamy@intel.com>
    Tested-by: Ng, Cheon-woei <cheon-woei.ng@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_max3111.c
+++ arch/x86/platform/intel-mid/device_libs/platform_max3111.c
@@ -12,18 +12,43 @@
 
 #include <linux/gpio.h>
 #include <linux/spi/spi.h>
+#include <linux/lnw_gpio.h>
+#include <linux/spi/intel_mid_ssp_spi.h>
 #include <asm/intel-mid.h>
 #include "platform_max3111.h"
+
+void __init *mrfl_platform_data(void *info);
+
+static struct intel_mid_ssp_spi_chip chip = {
+	.burst_size = DFLT_FIFO_BURST_SIZE,
+	.timeout = DFLT_TIMEOUT_VAL,
+	/* UART DMA is not supported in VP */
+	.dma_enabled = false,
+};
 
 void __init *max3111_platform_data(void *info)
 {
 	struct spi_board_info *spi_info = info;
-	int intr = get_gpio_by_name("max3111_int");
+	int intr;
+
+	if ((intel_mrfl_identify_sim() == INTEL_MRFL_CPU_SIMULATION_VP) ||
+	    (intel_mrfl_identify_sim() == INTEL_MRFL_CPU_SIMULATION_SLE) ||
+	    (INTEL_MID_BOARD(1, PHONE, MRFL)))
+		return mrfl_platform_data(info);
 
 	spi_info->mode = SPI_MODE_0;
-	if (intr == -1)
-		return NULL;
-	spi_info->irq = intr + INTEL_MID_IRQ_OFFSET;
+	intr = get_gpio_by_name("max3111_int");
+	if (intr != -1)
+		spi_info->irq = intr + INTEL_MID_IRQ_OFFSET;
 	return NULL;
 }
 
+void __init *mrfl_platform_data(void *info)
+{
+	struct spi_board_info *spi_info = info;
+	spi_info->mode = SPI_MODE_0;
+	spi_info->controller_data = &chip;
+	spi_info->bus_num = FORCE_SPI_BUS_NUM;
+	spi_info->chip_select = FORCE_CHIP_SELECT;
+	return NULL;
+}
