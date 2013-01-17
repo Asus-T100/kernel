26919d201ba671d05fabab3f0cc10f2b6128d1ad
Li, Ning <ning.li@intel.com>
2013-01-17 22:24:42 +0800

    scu_ipc: fix critical kw issue
    BZ: 80961
    
    'wbuf' array elements are used uninitialized in the function.
    Initialize it before using it.
    
    Change-Id: Iccf6b1af84b8a1a7ca8f8ee6194d0421ed13c1c5
    Signed-off-by: Li, Ning <ning.li@intel.com>
    Reviewed-on: http://android.intel.com:8080/87228
    Reviewed-by: Chen, Jason CJ <jason.cj.chen@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Reviewed-by: Yang, Bin <bin.yang@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/platform/x86/intel_scu_ipc.c
+++ drivers/platform/x86/intel_scu_ipc.c
@@ -324,7 +324,7 @@ int intel_scu_ipc_raw_cmd(u32 cmd, u32 sub, u8 *in, u8 inlen, u32 *out,
 		u32 outlen, u32 dptr, u32 sptr)
 {
 	int i, err;
-	u32 wbuf[4];
+	u32 wbuf[4] = { 0 };
 
 	if (ipcdev.pdev == NULL)
 		return -ENODEV;
