fc7ad1aa2cd8e0329a4b322494b12a89173fcba6
Chetan Rao <chetan.rao@intel.com>
2013-01-22 13:07:19 +0200

    Revert "[REVERT ME] MR1: Temporarily support old build script"
    BZ: 72539
    
    This reverts commit da18ae32268884cb785b1a08ca9cd68963dc74c0.
    
    Change-Id: I6a6c605c910a30970d70d3357605af753f5570c3
    Signed-off-by: Chetan Rao <chetan.rao@intel.com>
    Reviewed-on: http://android.intel.com:8080/87922
    Reviewed-by: Fert, Laurent <laurent.fert@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>
    Reviewed-on: http://android.intel.com:8080/88326
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Kuppuswamy, Sathyanarayanan <sathyanarayanan.kuppuswamy@intel.com>
    Tested-by: Ng, Cheon-woei <cheon-woei.ng@intel.com>

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -11,16 +11,8 @@ ifeq ($(BOARD_USE_64BIT_KERNEL),true)
 KERNEL_BUILD_FLAGS := -B
 endif
 
-# Temporary: stay compatible with old version of the script before it is ported to MR1
-ifeq (mrst, $(filter mrst,$(TARGET_BOARD_PLATFORM)))
-#   > MR1 compatible - TODO: Remove after build script is ported to MR1
-menuconfig get_kernel_from_source: $(MINIGZIP)
-	+TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" DIFFCONFIGS="$(DIFFCONFIGS)" vendor/intel/support/kernel-build.sh $(KERNEL_BUILD_FLAGS) -c $(TARGET_DEVICE) -o $@
-else
-#   > new script
 menuconfig get_kernel_from_source: $(MINIGZIP)
 	+TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" TARGET_DEVICE="$(TARGET_DEVICE)" TARGET_BOARD_PLATFORM="$(TARGET_BOARD_PLATFORM)" DIFFCONFIGS="$(DIFFCONFIGS)" vendor/intel/support/kernel-build.sh $(KERNEL_BUILD_FLAGS) -o $@
-endif
 
 # This rule is useful for creating a kernel that will be
 # shared with a tree that does not have kernel source.
