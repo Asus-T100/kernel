1222f79539dd81c79cbcca47eebd08e5af596937
Christoph Lameter <cl@linux.com>
2013-01-23 21:45:48 +0000

    [PORT FROM UPSTREAM] slub: avoid race in slab_alloc() on preempt kernel
    BZ: 142119
    
    Original commit:
      slub: tid must be retrieved from the percpu area of the current processor
    
      As Steven Rostedt has pointer out: rescheduling could occur on a
      different processor after the determination of the per cpu pointer and
      before the tid is retrieved. This could result in allocation from the
      wrong node in slab_alloc().
    
      The effect is much more severe in slab_free() where we could free to the
      freelist of the wrong page.
    
      The window for something like that occurring is pretty small but it is
      possible.
    
    Change-Id: Ia3e785b410e1030c8c445e4cc735696a1cd2b35e
    Signed-off-by: Christoph Lameter <cl@linux.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>
    Signed-off-by: Quentin Casasnovas <quentinx.casasnovas@intel.com>
    (cherry picked from commit 7cccd80b4397699902aced1ad3d692d384aaab77)
    Reviewed-on: http://android.intel.com:8080/136231
    Reviewed-by: Lewicki, Alexandre <alexandre.lewicki@intel.com>
    Reviewed-by: Koskinen, Ilkka <ilkka.koskinen@intel.com>
    Reviewed-by: De Cesco, Jonathan <jonathan.de.cesco@intel.com>
    Reviewed-by: Gross, Mark <mark.gross@intel.com>
    Reviewed-by: Cohen, David A <david.a.cohen@intel.com>
    Tested-by: Lestienne, ChristopheX <christophex.lestienne@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- mm/slub.c
+++ mm/slub.c
@@ -2319,7 +2319,14 @@ redo:
 	 * enabled. We may switch back and forth between cpus while
 	 * reading from one cpu area. That does not matter as long
 	 * as we end up on the original cpu again when doing the cmpxchg.
+	 *
+	 * Preemption is disabled for the retrieval of the tid because that
+	 * must occur from the current processor. We cannot allow rescheduling
+	 * on a different processor between the determination of the pointer
+	 * and the retrieval of the tid.
 	 */
+	preempt_disable();
+
 	c = __this_cpu_ptr(s->cpu_slab);
 
 	/*
@@ -2329,7 +2336,7 @@ redo:
 	 * linked list in between.
 	 */
 	tid = c->tid;
-	barrier();
+	preempt_enable();
 
 	object = c->freelist;
 	if (unlikely(!object || !node_match(c, node)))
@@ -2575,10 +2582,11 @@ redo:
 	 * data is retrieved via this pointer. If we are on the same cpu
 	 * during the cmpxchg then the free will succedd.
 	 */
+	preempt_disable();
 	c = __this_cpu_ptr(s->cpu_slab);
 
 	tid = c->tid;
-	barrier();
+	preempt_enable();
 
 	if (likely(page == c->page)) {
 		set_freepointer(s, object, c->freelist);
