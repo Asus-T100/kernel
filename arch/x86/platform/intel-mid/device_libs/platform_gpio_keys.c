a9acd7d22aaf1f7620d30ad9d74bcecabb15828f
Li, Ning <ning.li@intel.com>
2013-01-23 22:12:00 +0800

    gpio_keys: add camera shutter button
    BZ: 81691
    
    Add camera shutter keys to gpio key list.
    "camera0_sb1" for KEY_CAMERA,
    "camera0_sb2" for KEY_CAMERA_FOCUS.
    
    Change-Id: I6c79724b6df91b911078c2e2ab92d8c168acd2a2
    Signed-off-by: Li, Ning <ning.li@intel.com>
    Reviewed-on: http://android.intel.com:8080/88133
    Reviewed-by: Han, Bin B <bin.b.han@intel.com>
    Reviewed-by: Yang, Bin <bin.yang@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_gpio_keys.c
+++ arch/x86/platform/intel-mid/device_libs/platform_gpio_keys.c
@@ -45,6 +45,8 @@ static struct gpio_keys_button gpio_button[] = {
 	{KEY_VOLUMEUP,		-1, 1, "volume_up",	EV_KEY, 0, 20},
 	{KEY_VOLUMEDOWN,	-1, 1, "volume_down",	EV_KEY, 0, 20},
 	{KEY_MUTE,		-1, 1, "mute_enable",	EV_KEY, 0, 20},
+	{KEY_CAMERA,            -1, 1, "camera0_sb1",   EV_KEY, 0, 20},
+	{KEY_CAMERA_FOCUS,      -1, 1, "camera0_sb2",   EV_KEY, 0, 20},
 };
 
 static struct gpio_keys_platform_data gpio_keys = {
