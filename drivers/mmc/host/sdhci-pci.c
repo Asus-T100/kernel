512972a2169eda84059ac87b6d5310dd6017f5ed
Chuanxiao Dong <chuanxiao.dong@intel.com>
2013-01-24 15:53:41 +0800

    [PORT FROM K3.0]mmc: host: enable cache feature for CLV host
    BZ: 64410
    
    Enable cache feature for CTP eMMC host controller
    
    Change-Id: I972b3b07f7447e0186122777bc0424ee56dfd030
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-on: http://android.intel.com:8080/88389
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/mmc/host/sdhci-pci.c
+++ drivers/mmc/host/sdhci-pci.c
@@ -311,7 +311,8 @@ static int mfd_emmc_probe_slot(struct sdhci_pci_slot *slot)
 	case PCI_DEVICE_ID_INTEL_CLV_EMMC0:
 		sdhci_alloc_panic_host(slot->host);
 		slot->host->mmc->caps |= MMC_CAP_1_8V_DDR;
-		slot->host->mmc->caps2 |= MMC_CAP2_INIT_CARD_SYNC;
+		slot->host->mmc->caps2 |= MMC_CAP2_INIT_CARD_SYNC |
+					MMC_CAP2_CACHE_CTRL;
 		slot->host->quirks2 |= SDHCI_QUIRK2_V2_0_SUPPORT_DDR50;
 		/*
 		 * CLV host controller has a special POWER_CTL register,
