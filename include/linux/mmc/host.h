29a2747f1f4a058dd219bba897e1c0f6036897a8
Chuanxiao Dong <chuanxiao.dong@intel.com>
2013-01-24 13:55:50 +0800

    [PORT FROM K3.0][BACKPORT]mmc: core: Fixup broken suspend and eMMC4.5 power off notify
    BZ: 42245
    
    This patch fixes up the broken suspend sequence for eMMC with sleep
    support. Additionally it reworks the eMMC4.5 Power Off Notification
    feature so it fits together with the existing sleep feature.
    
    The CMD0 based re-initialization of the eMMC at resume is re-introduced
    to maintain compatiblity for devices using sleep.
    
    A host shall use MMC_CAP2_POWEROFF_NOTIFY to enable the Power Off
    Notification feature. We might be able to remove this cap later on,
    if we think that Power Off Notification always is preferred over
    sleep, even if the host is not able to cut the eMMC VCCQ power.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Saugata Das <saugata.das@linaro.org>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    
    orignal commit id e6c085863f97f0a8f009753e1baaf83e4aac7b42 in mmc-next
    
    Change-Id: I44ae42321c13a9edead4b79ceae91cba29cff0df
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Reviewed-on: http://android.intel.com:8080/88385
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -279,10 +279,6 @@ struct mmc_host {
 #define MMC_CAP2_LED_SUPPORT	(1 << 13)	/* led support */
 
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
-	unsigned int        power_notify_type;
-#define MMC_HOST_PW_NOTIFY_NONE		0
-#define MMC_HOST_PW_NOTIFY_SHORT	1
-#define MMC_HOST_PW_NOTIFY_LONG		2
 
 #ifdef CONFIG_MMC_CLKGATE
 	int			clk_requests;	/* internal reference counter */
