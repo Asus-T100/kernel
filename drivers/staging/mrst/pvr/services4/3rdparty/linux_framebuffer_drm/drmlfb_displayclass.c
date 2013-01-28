f92594334366845cc5bbcef4e03f43307ef006e7
Lei Zhang <lei.zhang@intel.com>
2013-01-28 11:57:44 +0800

    [PORT FROM MAIN] Display: fix bootanimation video doesn't show
    BZ: 82665
    
    This patch fixs the bootanimation display issue in boot process. If
    boot animation is changed to use video clips and need to be showed
    through overlay, it needs to set sprite layer to be transparent and
    blank.
    
    Change-Id: Icd3f84f3247309574aa1b79127282dd78ca66c51
    Signed-off-by: Lei Zhang <lei.zhang@intel.com>
    Reviewed-on: http://android.intel.com:8080/88816
    Reviewed-by: Tong, BoX <box.tong@intel.com>
    Tested-by: Tong, BoX <box.tong@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>
    Reviewed-on: http://android.intel.com:8080/90461
    Tested-by: Yang, Fei <fei.yang@intel.com>

--- drivers/staging/mrst/pvr/services4/3rdparty/linux_framebuffer_drm/drmlfb_displayclass.c
+++ drivers/staging/mrst/pvr/services4/3rdparty/linux_framebuffer_drm/drmlfb_displayclass.c
@@ -104,6 +104,7 @@ static IMG_BOOL MRSTLFBFlip(MRSTLFB_DEVINFO *psDevInfo,
 		memset(psDevInfo->sSystemBuffer.sCPUVAddr, 0,
 				psDevInfo->sSystemBuffer.ui32BufferSize);
 		FirstCleanFlag = 0;
+		DRMLFBFlipBlackScreen(psDevInfo, IMG_TRUE);
 	}
 	return IMG_TRUE;
 }
@@ -1665,6 +1666,7 @@ static IMG_BOOL ProcessFlip2(IMG_HANDLE hCmdCookie,
 	if (FirstCleanFlag == 1) {
 		memset(psDevInfo->sSystemBuffer.sCPUVAddr, 0,
 				psDevInfo->sSystemBuffer.ui32BufferSize);
+		DRMLFBFlipBlackScreen(psDevInfo, IMG_TRUE);
 		FirstCleanFlag = 0;
 	}
 
