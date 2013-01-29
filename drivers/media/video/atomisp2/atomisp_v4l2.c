f4f0c6d40ad4c6e15327ee12faa69c15e32aaad6
Bin Han <bin.b.han@intel.com>
2013-01-29 15:54:40 +0800

    atomisp2: add V4L2_MBUS_FMT_SRGGB10_1X10 into atomisp_output_fmts table
    BZ: 83500
    
    Change-Id: Id9fe4dffec2f406e9b6d859d8b18d61bf2a22905
    Signed-off-by: Bin Han <bin.b.han@intel.com>
    Reviewed-on: http://android.intel.com:8080/89103
    Reviewed-by: Tao, Jing <jing.tao@intel.com>
    Tested-by: Koski, Anttu <anttu.koski@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/media/video/atomisp2/atomisp_v4l2.c
+++ drivers/media/video/atomisp2/atomisp_v4l2.c
@@ -211,6 +211,12 @@ const struct atomisp_format_bridge atomisp_output_fmts[] = {
 		.sh_fmt = SH_CSS_FRAME_FORMAT_RAW,
 		.description = "Bayer 10"},
 	{
+		.pixelformat = V4L2_PIX_FMT_SRGGB10,
+		.depth = 16,
+		.mbus_code = V4L2_MBUS_FMT_SRGGB10_1X10,
+		.sh_fmt = SH_CSS_FRAME_FORMAT_RAW,
+		.description = "Bayer 10"},
+	{
 		.pixelformat = V4L2_PIX_FMT_SBGGR12,
 		.depth = 16,
 		.mbus_code = V4L2_MBUS_FMT_SBGGR12_1X12,
