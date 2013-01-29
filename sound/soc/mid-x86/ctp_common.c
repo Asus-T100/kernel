cd9fb9960a35edaff5767ce067e536e3aa4fb99c
Neil DI FOLCO <neil.di.folco@intel.com>
2013-01-29 09:44:44 +0100

    [AUDIO] Reduce ctp_common.c log level
    BZ: 83527
    
    Change log level from "info" to "debug" in order to avoid too many kernel logs by default.
    
    Change-Id: I97c90f2912e6fba12aebbf3421ee0d4c350c18e3
    Signed-off-by: Neil DI FOLCO <neil.di.folco@intel.com>
    Reviewed-on: http://android.intel.com:8080/89115
    Reviewed-by: Centelles, Sylvain <sylvain.centelles@intel.com>
    Reviewed-by: De Chivre, Renaud <renaud.de.chivre@intel.com>
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Tested-by: Barthes, FabienX <fabienx.barthes@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- sound/soc/mid-x86/ctp_common.c
+++ sound/soc/mid-x86/ctp_common.c
@@ -236,7 +236,7 @@ int ctp_set_bias_level(struct snd_soc_card *card,
 		if (!strcmp(codec->name, "snd-soc-dummy"))
 			return 0;
 	} else {
-		pr_info("In %s dapm context has no associated codec or it is dummy codec.", __func__);
+		pr_debug("In %s dapm context has no associated codec or it is dummy codec.", __func__);
 		return 0;
 	}
 
@@ -278,7 +278,7 @@ int ctp_set_bias_level_post(struct snd_soc_card *card,
 		if (!strcmp(codec->name, "snd-soc-dummy"))
 			return 0;
 	} else {
-		pr_info("In %s dapm context has no associated codec or it is dummy codec.", __func__);
+		pr_debug("In %s dapm context has no associated codec or it is dummy codec.", __func__);
 		return 0;
 	}
 
