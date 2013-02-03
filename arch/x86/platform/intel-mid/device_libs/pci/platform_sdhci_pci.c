b54becdf4c28952d878bedaae2903770d2c82d00
Yunpeng Gao <yunpeng.gao@intel.com>
2013-02-03 21:38:02 +0800

    mmc: enable eMMC support on Merrifield VP and HVP
    BZ: 83795
    
    This patch enable eMMC support on Merrifield VP
    and HVP Pre-Silicon platform.
    
    Signed-off-by: Yunpeng Gao <yunpeng.gao@intel.com>
    
    Change-Id: I95b460b470ffcd26915e084d028f2bb1405de0f1
    Reviewed-on: http://android.intel.com:8080/89917
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: Sun, Jianhua <jianhua.sun@intel.com>
    Reviewed-by: Gao, Yunpeng <yunpeng.gao@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- arch/x86/platform/intel-mid/device_libs/pci/platform_sdhci_pci.c
+++ arch/x86/platform/intel-mid/device_libs/pci/platform_sdhci_pci.c
@@ -258,18 +258,52 @@ static struct sdhci_pci_data *get_sdhci_platform_data(struct pci_dev *pdev)
 		switch (PCI_FUNC(pdev->devfn)) {
 		case 0:
 			pdata = &mrfl_sdhci_pci_data[EMMC0_INDEX];
+			/*
+			 * The current eMMC device simulation in Merrifield
+			 * VP only implements boot partition 0, does not
+			 * implements boot partition 1. And the VP will
+			 * crash if eMMC boot partition 1 is accessed
+			 * during kernel boot. So, we just disable boot
+			 * partition support for Merrifield VP platform.
+			 */
+			if (intel_mrfl_identify_sim() ==
+					INTEL_MRFL_CPU_SIMULATION_VP)
+				pdata->platform_quirks |=
+					PLFM_QUIRK_NO_EMMC_BOOT_PART;
 			break;
 		case 1:
 			pdata = &mrfl_sdhci_pci_data[EMMC1_INDEX];
+			if (intel_mrfl_identify_sim() ==
+					INTEL_MRFL_CPU_SIMULATION_VP)
+				pdata->platform_quirks |=
+					PLFM_QUIRK_NO_EMMC_BOOT_PART;
+			/*
+			 * Merrifield HVP platform only implements
+			 * eMMC0 host controller in its FPGA, and
+			 * does not implements other 3 Merrifield
+			 * SDHCI host controllers.
+			 */
+			if (intel_mrfl_identify_sim() ==
+					INTEL_MRFL_CPU_SIMULATION_HVP)
+				pdata->platform_quirks |=
+					PLFM_QUIRK_NO_HOST_CTRL_HW;
 			break;
 		case 2:
 			pdata = &mrfl_sdhci_pci_data[SD_INDEX];
+			if (intel_mrfl_identify_sim() ==
+					INTEL_MRFL_CPU_SIMULATION_HVP)
+				pdata->platform_quirks |=
+					PLFM_QUIRK_NO_HOST_CTRL_HW;
 			break;
 		case 3:
 			pdata = &mrfl_sdhci_pci_data[SDIO_INDEX];
+			if (intel_mrfl_identify_sim() ==
+					INTEL_MRFL_CPU_SIMULATION_HVP)
+				pdata->platform_quirks |=
+					PLFM_QUIRK_NO_HOST_CTRL_HW;
 			break;
 		default:
-			pr_err("File %s function %s: Invalid PCI device function number (%d)\n",
+			pr_err("%s func %s: Invalid PCI Dev func no. (%d)\n",
 				__FILE__, __func__, PCI_FUNC(pdev->devfn));
 			break;
 		}
