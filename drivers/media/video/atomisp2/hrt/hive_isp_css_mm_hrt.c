7295b98995fef4f2175df40193e3e95aa19250cc
Antti Laakso <antti.laakso@intel.com>
2013-02-04 16:31:32 +0200

    [PORT FROM K3.0] atomisp2: remove unused code from hive_isp_css_mm_hrt.c
    BZ: 72619
    
    Remove #if 0'ed code.
    
    Change-Id: I2171b6b930ca60f1b0bed8507c0f1b62eb4f0ead
    Orig-Change-Id: Ic40ca3e45eced4bd015d70bc539d116362e0257c
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>
    Acked-by: Tuukka Toivonen <tuukka.toivonen@intel.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-on: http://android.intel.com:8080/90105
    Reviewed-by: Ailus, Sakari <sakari.ailus@intel.com>
    Reviewed-by: Koskinen, Ilkka <ilkka.koskinen@intel.com>
    Tested-by: Patale, Pavan <pavan.patale@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>
    Reviewed-on: http://android.intel.com:8080/90824
    Reviewed-by: Chotard, Celine <celine.chotard@intel.com>
    Tested-by: Chotard, Celine <celine.chotard@intel.com>

--- drivers/media/video/atomisp2/hrt/hive_isp_css_mm_hrt.c
+++ drivers/media/video/atomisp2/hrt/hive_isp_css_mm_hrt.c
@@ -177,37 +177,6 @@ void *hrt_isp_css_mm_calloc_cached(size_t bytes)
 	return ptr;
 }
 
-#if 0
-int hrt_isp_css_mm_load_int(void *virt_addr, int *data)
-{
-	return hrt_isp_css_mm_load(virt_addr, data, sizeof(*data));
-}
-
-int hrt_isp_css_mm_load_short(void *virt_addr, short *data)
-{
-	return hrt_isp_css_mm_load(virt_addr, data, sizeof(*data));
-}
-
-int hrt_isp_css_mm_load_char(void *virt_addr, char *data)
-{
-	return hrt_isp_css_mm_load(virt_addr, data, sizeof(*data));
-}
-
-int hrt_isp_css_mm_store_char(void *virt_addr, char data)
-{
-	return hrt_isp_css_mm_store(virt_addr, &data, sizeof(data));
-}
-
-int hrt_isp_css_mm_store_short(void *virt_addr, short data)
-{
-	return hrt_isp_css_mm_store(virt_addr, &data, sizeof(data));
-}
-
-int hrt_isp_css_mm_store_int(void *virt_addr, int data)
-{
-	return hrt_isp_css_mm_store(virt_addr, &data, sizeof(data));
-}
-#endif
 void *hrt_isp_css_virt_to_phys(void *virt_addr)
 {
 	return (void *)(u32)hmm_virt_to_phys(virt_addr);
