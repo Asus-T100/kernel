3bbc895291ee87177f2e2c7014693b425ccb8f1f
Frederic Berat <fredericx.berat@intel.com>
2013-02-05 20:32:25 +0100

    [SCALABILITY] [Telephony/MCD]: Create platform device function
    BZ: 81304
    
    Scalability effort. In order to get platform and device information at runtime
    for the modem control driver, a new function is created that will associate
    modem information with board information, based on platform IDs.
    
    This patch also update PMIC adresses and timing values for MRFLD and CTP
    platforms.
    
    Change-Id: I33fc5ec7802eff1fb542acf2fe0f3cd10eb4e4c6
    Signed-off-by: Frederic Berat <fredericx.berat@intel.com>
    Reviewed-on: http://android.intel.com:8080/90364
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Betous, GuillaumeX <guillaumex.betous@intel.com>
    Reviewed-by: Tenoutit, FaouazX <faouazx.tenoutit@intel.com>
    Reviewed-by: Pillet, VincentX <vincentx.pillet@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ arch/x86/platform/intel-mid/device_libs/platform_modem_ctrl.c
@@ -0,0 +1,200 @@
+/*
+ * platform_modem_crl.c: modem control platform data initilization file
+ *
+ * (C) Copyright 2008 Intel Corporation
+ * Author:
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ */
+
+#include <linux/gpio.h>
+#include <linux/lnw_gpio.h>
+#include <asm/intel-mid.h>
+#include <linux/mdm_ctrl_board.h>
+#include <linux/mdm_ctrl.h>
+
+/* MFLD generic infos */
+static struct mdm_ctrl_pdata mfld_mid_info = {
+	.modem = MODEM_6260,
+	.chipctrl = 0x0E0,
+	.chipctrlon = 0x4,
+	.chipctrloff = 0x2,
+	.chipctrl_mask = 0xF8,
+	.pre_pwr_down_delay = 60,
+	.pwr_down_duration = 20000,
+	.early_pwr_on = true,
+	.early_pwr_off = false
+};
+
+/* CTP generic infos */
+static struct mdm_ctrl_pdata ctp_mid_info = {
+	.modem = MODEM_6360,
+	.chipctrl = 0x100,
+	.chipctrlon = 0x10,
+	.chipctrloff = 0x10,
+	.chipctrl_mask = 0x00,
+	.pre_pwr_down_delay = 650,
+	.pwr_down_duration = 20000,
+	.early_pwr_on = false,
+	.early_pwr_off = true
+};
+
+/* MRFLD generic infos */
+static struct mdm_ctrl_pdata mrfld_mid_info = {
+	.modem = MODEM_7160,
+	.chipctrl = 0x31,
+	.chipctrlon = 0x2,
+	.chipctrloff = 0x0,
+	.chipctrl_mask = 0xFC,
+	.pre_pwr_down_delay = 650,
+	.pwr_down_duration = 20000,
+	.early_pwr_on = false,
+	.early_pwr_off = true
+};
+
+/* Non-supported platforms generic infos*/
+static struct mdm_ctrl_pdata mdm_ctrl_dummy_info = {
+	.is_mdm_ctrl_disabled = true
+};
+
+/* IMC XMM6260 generic infos*/
+static struct mdm_ctrl_device_info mdm_ctrl_6260_info = {
+	.pre_on_delay = 200,
+	.on_duration = 60,
+	.pre_wflash_delay = 30,
+	.pre_cflash_delay = 60,
+	.flash_duration = 60,
+	.warm_rst_duration = 60
+};
+
+/* IMC XMM6360 generic infos*/
+/* FIXME: create 6360 specific functions ?
+ * Verify if PMIC value should be stored on MRFL
+*/
+static struct mdm_ctrl_device_info mdm_ctrl_6360_info = {
+	.pre_on_delay = 200,
+	.on_duration = 60,
+	.pre_wflash_delay = 30,
+	.pre_cflash_delay = 60,
+	.flash_duration = 60,
+	.warm_rst_duration = 60
+};
+
+/* IMC XMM7160 generic infos*/
+static struct mdm_ctrl_device_info mdm_ctrl_7160_info = {
+	.pre_on_delay = 200,
+	.on_duration = 60,
+	.pre_wflash_delay = 30,
+	.pre_cflash_delay = 60,
+	.flash_duration = 60,
+	.warm_rst_duration = 60
+};
+
+static int mdm_ctrl_is_supported_ctp(void)
+{
+	/* FIXME: Revisit on IFWI update*/
+	return INTEL_MID_BOARD(1, PHONE, CLVTP) &&
+		(INTEL_MID_BOARD(2, PHONE, CLVTP, RHB, PRO) ||
+			INTEL_MID_BOARD(2, PHONE, CLVTP, RHB, ENG)) &&
+		!SPID_HARDWARE_ID(CLVTP, PHONE, RHB, CCVV2VB) &&
+		!SPID_HARDWARE_ID(CLVTP, PHONE, RHB, VBDV1) &&
+		!SPID_HARDWARE_ID(CLVTP, PHONE, RHB, VV3AVB) &&
+		!SPID_HARDWARE_ID(CLVTP, PHONE, RHB, VV3BVB) &&
+		!SPID_HARDWARE_ID(CLVTP, PHONE, RHB, VV3CVB) &&
+		!SPID_HARDWARE_ID(CLVTP, PHONE, RHB, VBDV1A) &&
+		!SPID_HARDWARE_ID(CLVTP, PHONE, RHB, VBDV1B);
+}
+
+static int mdm_ctrl_is_supported_mfld(void)
+{
+	return INTEL_MID_BOARD(1, PHONE, MFLD) ||
+		INTEL_MID_BOARD(2, TABLET, MFLD, SLP, PRO) ||
+		INTEL_MID_BOARD(2, TABLET, MFLD, SLP, ENG);
+}
+
+static int mdm_ctrl_is_supported_mrfld(void)
+{
+	return INTEL_MID_BOARD(1, PHONE, MRFL);
+}
+
+void *modem_ctrl_platform_data(void *data)
+{
+	static struct mdm_ctrl_pdata *mdm_ctrl_info;
+
+	int gpio_rst_out = get_gpio_by_name(GPIO_RST_OUT);
+	int gpio_pwr_on  = get_gpio_by_name(GPIO_PWR_ON);
+	int gpio_rst_bbn = get_gpio_by_name(GPIO_RST_BBN);
+	int gpio_cdump   = get_gpio_by_name(GPIO_CDUMP);
+	int gpio_cdump_mrfl   = get_gpio_by_name(GPIO_CDUMP_MRFL);
+
+	int is_ctpscalelt = *(int *)data;
+
+	pr_info("mdm ctrl: platform data setup\n");
+
+	if (mdm_ctrl_is_supported_mfld()) {
+
+		mfld_mid_info.gpio_rst_out = gpio_rst_out;
+		mfld_mid_info.gpio_pwr_on = gpio_pwr_on;
+		mfld_mid_info.gpio_rst_bbn = gpio_rst_bbn;
+		mfld_mid_info.gpio_cdump = gpio_cdump;
+
+		pr_info("mdm ctrl: Getting MFLD datas\n");
+		mdm_ctrl_info = (void *)&mfld_mid_info;
+		mdm_ctrl_info->device_data = (void *)&mdm_ctrl_6260_info;
+		mdm_ctrl_info->is_mdm_ctrl_disabled = false;
+
+	} else if (mdm_ctrl_is_supported_ctp()) {
+
+		ctp_mid_info.gpio_rst_out = gpio_rst_out;
+		ctp_mid_info.gpio_pwr_on = gpio_pwr_on;
+		ctp_mid_info.gpio_rst_bbn = gpio_rst_bbn;
+		ctp_mid_info.gpio_cdump = gpio_cdump;
+
+		pr_info("mdm ctrl: Getting CTP datas\n");
+		mdm_ctrl_info = (void *)&ctp_mid_info;
+		mdm_ctrl_info->device_data = (void *)&mdm_ctrl_6360_info;
+		mdm_ctrl_info->is_mdm_ctrl_disabled = false;
+
+		/* FIXME: Workaround for ctpscalelt. Waiting for IFWI update*/
+		if (is_ctpscalelt) {
+			pr_info("mdm ctrl: Getting CTPLT datas\n");
+			mdm_ctrl_info->modem = MODEM_6268;
+			mdm_ctrl_info->device_data = (void *)
+							&mdm_ctrl_6260_info;
+		}
+
+	} else if (mdm_ctrl_is_supported_mrfld()) {
+
+		mrfld_mid_info.gpio_rst_out = gpio_rst_out;
+		mrfld_mid_info.gpio_pwr_on = gpio_pwr_on;
+		mrfld_mid_info.gpio_rst_bbn = gpio_rst_bbn;
+		mrfld_mid_info.gpio_cdump = gpio_cdump_mrfl;
+
+		pr_info("mdm ctrl: Getting MRFLD datas\n");
+		mdm_ctrl_info = (void *)&mrfld_mid_info;
+		mdm_ctrl_info->device_data = (void *)&mdm_ctrl_7160_info;
+		mdm_ctrl_info->is_mdm_ctrl_disabled = false;
+
+	} else {
+		mdm_ctrl_info = (void *)&mdm_ctrl_dummy_info;
+	}
+
+	pr_info("mdm ctrl: platform data setup done\n");
+
+	if (!mdm_ctrl_info->is_mdm_ctrl_disabled) {
+		pr_info("mdm ctrl: GPIO list : rst_out:%d,"\
+				" pwr_on:%d, rst_bbn:%d, cdump:%d\n",
+				mdm_ctrl_info->gpio_rst_out,
+				mdm_ctrl_info->gpio_pwr_on,
+				mdm_ctrl_info->gpio_rst_bbn,
+				mdm_ctrl_info->gpio_cdump);
+	} else {
+		pr_info("mdm ctrl disabled\n");
+	}
+
+	return mdm_ctrl_info;
+}
+
---
+++ include/linux/mdm_ctrl_board.h
@@ -0,0 +1,95 @@
+/*
+ * mdm_ctrl_board.h
+ *
+ * Header for the Modem control driver.
+ *
+ * Copyright (C) 2010, 2011 Intel Corporation. All rights reserved.
+ *
+ * Contact: Frederic BERAT <fredericx.berat@intel.com>
+ *          Faouaz TENOUTIT <faouazx.tenoutit@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+#ifndef __MDM_CTRL_BOARD_H__
+#define __MDM_CTRL_BOARD_H__
+
+/* Supported Modem IDs*/
+#define MODEM_6260 6260
+#define MODEM_6268 6268
+#define MODEM_6360 6360
+#define MODEM_7160 7160
+
+/* GPIO names */
+#define GPIO_RST_OUT	"ifx_mdm_rst_out"
+#define GPIO_PWR_ON	"ifx_mdm_pwr_on"
+#define GPIO_RST_BBN	"ifx_mdm_rst_pmu"
+#define GPIO_CDUMP	"modem-gpio2"
+#define GPIO_CDUMP_MRFL	"MODEM_CORE_DUMP"
+
+extern void *modem_ctrl_platform_data(void *data) __attribute__((weak));
+
+/* struct mdm_ctrl_pdata
+ * @modem_name: Modem name, used to select correct sequences
+ * @chipctrl: PMIC base address
+ * @chipctrlon: Modem power on PMIC value
+ * @chipctrloff: Modem power off PMIC value
+ * @pre_pwr_down_delay:Delay before powering down the modem (us)
+ * @pwr_down_duration:Powering down duration (us)
+ * @gpio_rst_out: Reset out gpio (self reset indicator)
+ * @gpio_pwr_on: Power on gpio (ON1 - Power up pin)
+ * @gpio_rst_bbn: RST_BB_N gpio (Reset pin)
+ * @gpio_cdump: CORE DUMP indicator
+ * @device_data: Device related data
+ * @archdata: Architecture-dependent device data
+ */
+struct mdm_ctrl_pdata {
+	int			modem;
+	int			chipctrl;
+	int			chipctrlon;
+	int			chipctrloff;
+	int			chipctrl_mask;
+	int			pre_pwr_down_delay;
+	int			pwr_down_duration;
+	int			gpio_rst_out;
+	int			gpio_pwr_on;
+	int			gpio_rst_bbn;
+	int			gpio_cdump;
+	bool			early_pwr_on;
+	bool			early_pwr_off;
+	bool			is_mdm_ctrl_disabled;
+	void			*device_data;
+	struct dev_archdata	*archdata;
+};
+
+/* struct mdm_ctrl_device_info - Board and modem infos
+ *
+ * @pre_on_delay:Delay before pulse on ON1 (us)
+ * @on_duration:Pulse on ON1 duration (us)
+ * @pre_wflash_delay:Delay before flashing window, after warm_reset (ms)
+ * @pre_cflash_delay:Delay before flashing window, after cold_reset (ms)
+ * @flash_duration:Flashing window durtion (ms)int  Not used ?
+ * @warm_rst_duration:Warm reset duration (ms)
+ */
+struct mdm_ctrl_device_info {
+	int			pre_on_delay;
+	int			on_duration;
+	int			pre_wflash_delay;
+	int			pre_cflash_delay;
+	int			flash_duration;
+	int			warm_rst_duration;
+};
+
+#endif /* __MDM_CTRL_BOARD_H__ */
