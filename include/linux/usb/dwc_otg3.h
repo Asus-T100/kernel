d48fc63cb500321febd71d0156a4754ab1eaad96
yu wang <yu.y.wang@intel.com>
2013-02-07 13:07:57 +0800

    REVERTME: dwc-otg: Prevent enter Cx state if USB active as host/device mode.
    BZ: 83703
    
    Enter Cx state will cause big impact for USB performance. And it cause
    lots of unexpected timeout issues. This patch will prevent enter Cx
    state when USB under host or device mode.
    
    Change-Id: Ia5af4e912af4e9d2e65f7bdc8ad4689e0d57741b
    Signed-off-by: yu wang <yu.y.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/90739
    Reviewed-by: Mansoor, Illyas <illyas.mansoor@intel.com>
    Tested-by: Gao, Yunpeng <yunpeng.gao@intel.com>
    Reviewed-by: Gao, Yunpeng <yunpeng.gao@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- include/linux/usb/dwc_otg3.h
+++ include/linux/usb/dwc_otg3.h
@@ -414,6 +414,11 @@ struct dwc_otg2 {
 	struct otg_bc_cap charging_cap;
 	struct notifier_block nb;
 	struct delayed_work sdp_check_work;
+
+	/* pm request to prevent enter Cx state
+	 * Because it have big impact to USB performance
+	 * */
+	struct pm_qos_request_list *qos;
 };
 
 /* Invalid SDP checking timeout */
