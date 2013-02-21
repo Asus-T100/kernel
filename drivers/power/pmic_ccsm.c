feebca0cee0a57b0a93f71f44938c2195eae2c77
ysiyer <yegnesh.s.iyer@intel.com>
2013-02-21 11:47:01 +0530

    EM/pmic_ccsm: Fix klocwork issues 95731, 95732 and 95733
    BZ: 86535
    
    95731: Check added from pmic_read_tt and pmic_write_tt functions
    in update_zone_cc, such that rval will not be accessed without
    initializing.
    95732: pmic_thread_handler to return IRQ_NONE if allocation for
    evt is null.
    95733: pmic_check_initial_events function to return -1 if allocation
    fails. Added return value check in the caller function which returns
    error incase pmic_check_initial_events returns error.
    
    Change-Id: I668975950c53b4a84c80e769df35f78ad05d6868
    Signed-off-by: ysiyer <yegnesh.s.iyer@intel.com>
    Reviewed-on: http://android.intel.com:8080/93008
    Reviewed-by: Tc, Jenny <jenny.tc@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: Kallappa Manjanna, MadhukumarX <madhukumarx.kallappa.manjanna@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/power/pmic_ccsm.c
+++ drivers/power/pmic_ccsm.c
@@ -669,13 +669,19 @@ EXPORT_SYMBOL(pmic_enable_charging);
 
 static inline int update_zone_cc(int zone, u8 reg_val)
 {
+	int ret;
 	u8 rval;
 	u8 addr_cc = TT_CHRCCHOTVAL_ADDR - zone;
 
 	dev_dbg(chc.dev, "%s:%X=%X\n", __func__, addr_cc, reg_val);
-	pmic_write_tt(addr_cc, reg_val);
+	ret = pmic_write_tt(addr_cc, reg_val);
+	if (unlikely(ret))
+		return ret;
 
-	pmic_read_tt(addr_cc, &rval);
+	ret = pmic_read_tt(addr_cc, &rval);
+	if (unlikely(ret))
+		return ret;
+
 	if (rval != reg_val) {
 		dev_err(chc.dev,
 			"%s:Error in writing to TT reg :%x Wrote=%X:Read=%X\n",
@@ -985,6 +991,11 @@ static irqreturn_t pmic_thread_handler(int id, void *data)
 	int ret;
 
 	evt = kzalloc(sizeof(*evt), GFP_ATOMIC);
+	if (evt == NULL) {
+		dev_dbg(chc.dev, "Error allocating evt structure in fn:\n",
+			__func__);
+		return IRQ_NONE;
+	}
 
 	pmic_intr = ioread16(chc.pmic_intr_iomap);
 	evt->chgrirq0_int = (u8)pmic_intr;
@@ -1250,6 +1261,11 @@ static int pmic_check_initial_events(void)
 	int ret;
 
 	evt = kzalloc(sizeof(struct pmic_event), GFP_KERNEL);
+	if (evt == NULL) {
+		dev_dbg(chc.dev, "Error allocating evt structure in fn:\n",
+			__func__);
+		return -1;
+	}
 
 	ret = intel_scu_ipc_ioread8(SCHGRIRQ0_ADDR, &evt->chgrirq0_stat);
 	evt->chgrirq0_int = evt->chgrirq0_stat;
@@ -1378,7 +1394,12 @@ static int pmic_chrgr_probe(struct platform_device *pdev)
 		goto req_irq_failed;
 	}
 
-	pmic_check_initial_events();
+	retval = pmic_check_initial_events();
+	if (unlikely(retval)) {
+		dev_err(&pdev->dev,
+			"Error posting initial events\n");
+		goto req_irq_failed;
+	}
 
 	/* unmask charger interrupts in second level IRQ register*/
 	retval = intel_scu_ipc_update_register(MCHGRIRQ0_ADDR, 0x00,
