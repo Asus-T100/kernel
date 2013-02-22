ec5c397289258ce7729782d6ae644b88aea5d281
feng wang <feng.a.wang@intel.com>
2013-02-22 16:02:43 +0800

    usb hsic: fix collission issue in hsic enable
    BZ: 83817
    
    Fix some collision issue in hsic enable interface
    to make it more rebostness. The main collision
    found are the hsic_enable request with the aux
    work triggered by the IRQ.
    
    Change-Id: I66484fa9a89764bef32a3cfd35c1f0eddd1f9c15
    Signed-off-by: feng wang <feng.a.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/93209
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Gao, Yunpeng <yunpeng.gao@intel.com>
    Tested-by: Wang, Ran <ran.wang@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- include/linux/usb/ehci-tangier-hsic-pci.h
+++ include/linux/usb/ehci-tangier-hsic-pci.h
@@ -2,8 +2,9 @@
 #define EHCI_TANGIER_HSIC_PCI_h
 #include <linux/notifier.h>
 
-#define HSIC_AUX_N	173
+#define HSIC_AUX_GPIO_NAME    "usb_hsic_aux1"
 #define HSIC_HUB_RESET_TIME   10
+#define HSIC_ENABLE_SIZE      2
 #define PM_BASE		0xff00b000
 #define PM_STS		0x00
 #define PM_CMD		0x04
@@ -22,7 +23,17 @@
 
 struct hsic_tangier_priv {
 	struct delayed_work  hsic_aux;
+	wait_queue_head_t    aux_wq;
 	struct mutex         hsic_mutex;
+	unsigned             hsic_mutex_init:1;
+	unsigned             aux_wq_init:1;
+	unsigned             hsic_aux_irq_enable:1;
+	unsigned             hsic_aux_finish:1;
+	unsigned             hsic_enable_created:1;
+	unsigned             hsic_lock_init:1;
+	unsigned             hsic_stopped:1;
+
+	unsigned             aux_gpio;
 	spinlock_t           hsic_lock;
 };
 
@@ -31,12 +42,6 @@ enum {
 	REMOVE
 };
 
-#define MODEM_7160_STAGE_ONE_VID	0x8087
-#define MODEM_7160_STAGE_ONE_PID	0x0716
-#define MODEM_7160_STAGE_TWO_VID	0x1519
-#define MODEM_7160_STAGE_TWO_PID	0x0443
-#define MODEM_PWR_ON                   181
-#define MODEM_RESET_BB_N               182
 #define HSIC_DPHY_D3_STATE_MASK		0xE00
 #define PM_REGISTER_LENGHT			0x200
 #define PM_SSC0_HSIC_D3_MODE		(0x3 << 16)
