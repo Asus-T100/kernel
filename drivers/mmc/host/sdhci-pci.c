f0cb7a41acab9240a09e71fcfc73968bebdb06b7
Yunpeng Gao <yunpeng.gao@intel.com>
2013-03-05 18:57:34 +0800

    mmc: disable eMMC DDR if High Speed disabled
    BZ: 86751
    
    According to eMMC Spec, DDR can be enabled only
    when High speed enabled already. If High speed
    not enabled, then DDR should not be enabled, too.
    So, submit this patch to disable eMMC DDR if
    High speed mode has been disabled.
    
    Change-Id: I8e722dd6991c5614f6f75faf02754c263b1a982c
    Signed-off-by: Yunpeng Gao <yunpeng.gao@intel.com>
    Reviewed-on: http://android.intel.com:8080/95102
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/mmc/host/sdhci-pci.c
+++ drivers/mmc/host/sdhci-pci.c
@@ -417,8 +417,10 @@ static int intel_mrfl_mmc_probe_slot(struct sdhci_pci_slot *slot)
 	slot->host->mmc->caps2 |= MMC_CAP2_POWEROFF_NOTIFY |
 			MMC_CAP2_POLL_R1B_BUSY | MMC_CAP2_INIT_CARD_SYNC;
 
-	if (slot->data->platform_quirks & PLFM_QUIRK_NO_HIGH_SPEED)
+	if (slot->data->platform_quirks & PLFM_QUIRK_NO_HIGH_SPEED) {
 		slot->host->quirks2 |= SDHCI_QUIRK2_DISABLE_HIGH_SPEED;
+		slot->host->mmc->caps &= ~MMC_CAP_1_8V_DDR;
+	}
 
 	if (slot->data->platform_quirks & PLFM_QUIRK_NO_EMMC_BOOT_PART)
 		slot->host->mmc->caps2 |= MMC_CAP2_BOOTPART_NOACC;
