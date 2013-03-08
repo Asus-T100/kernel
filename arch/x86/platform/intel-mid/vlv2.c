c04fe2c0f09e88d9c62d0dbc508ece840b130d12
Bin Gao <bin.gao@intel.com>
2013-03-07 18:55:37 -0800

    [REVERTME] vlv2: add WA for PCI interrupt routing issue
    BZ: 91374
    
    ACPI DSDT table doesn't have correct PCI interrupt routing information
    for some devices, so here we have a kernel workaround to fix this issue.
    The workaround can be easily removed by not appending "vlv2" paramerter
    to kernel command line.
    
    Change-Id: Ie92e91d3b640d6c36dfee9944c6cd239e1cb1ffb
    Signed-off-by: Bin Gao <bin.gao@intel.com>
    Signed-off-by: Guilhem Imberton <guilhem.imberton@intel.com>
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>
    Reviewed-on: http://android.intel.com:8080/95466
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Kuppuswamy, Sathyanarayanan <sathyanarayanan.kuppuswamy@intel.com>
    Reviewed-by: Cohen, David A <david.a.cohen@intel.com>
    Tested-by: Ng, Cheon-woei <cheon-woei.ng@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- arch/x86/platform/intel-mid/vlv2.c
+++ arch/x86/platform/intel-mid/vlv2.c
@@ -22,7 +22,65 @@
 #include <asm/processor.h>
 #include <asm/pci_x86.h>
 
+#define ILB_BASE	0xfed08000
+#define ILB_SIZE	0xa0
+#define ILB_IR		0x20
+#define PCI_DEV_NUM(x)	((x >> 3) & 0x1f)
+#define IR_INTA(ir)	(ir & 0xf)
+
+#define PIRQ2IRQ(x)	(x + 16)
+
 extern struct legacy_pic default_legacy_pic;
+
+/* Propagate PCI IRQ# */
+static int vlv2_pci_enable_irq(struct pci_dev *pdev)
+{
+
+	u8 ir_val, dev;
+	void __iomem *ilb_mem;
+	struct io_apic_irq_attr irq_attr;
+
+	ilb_mem = ioremap_nocache(ILB_BASE, ILB_SIZE);
+	if (ilb_mem == NULL) {
+		pr_err("%s(): can't map ILB_BASE(0x%x)\n",
+			__func__, ILB_BASE);
+		return -EIO;
+	}
+
+	dev = PCI_DEV_NUM(pdev->devfn);
+	ir_val = ioread8(ilb_mem + ILB_IR + PCI_DEV_NUM(pdev->devfn) * 2);
+
+	iounmap(ilb_mem);
+
+	/* map INTA# only */
+	pdev->irq = PIRQ2IRQ(IR_INTA(ir_val));
+	pci_write_config_byte(pdev, PCI_INTERRUPT_LINE, pdev->irq);
+
+	irq_attr.ioapic = mp_find_ioapic(pdev->irq);
+	irq_attr.ioapic_pin = pdev->irq;
+	irq_attr.trigger = 1; /* level */
+	irq_attr.polarity = 1; /* active low */
+	io_apic_set_pci_routing(&pdev->dev, pdev->irq, &irq_attr);
+
+	return 0;
+}
+
+/*
+ * ACPI DSDT table doesn't have correct PCI interrupt routing information
+ * for some devices, so here we have a kernel workaround to fix this issue.
+ * The workaround can be easily removed by not appending "vlv2" paramerter
+ * to kernel command line.
+ */
+static int __init vlv2_pci_irq_fixup(void)
+{
+	if (intel_mid_identify_cpu() == INTEL_MID_CPU_CHIP_VALLEYVIEW2) {
+		pr_info("VLV2: fix up PCI interrupt routing\n");
+		pcibios_enable_irq = vlv2_pci_enable_irq;
+	}
+
+	return 0;
+}
+fs_initcall(vlv2_pci_irq_fixup);
 
 /* Restore x86_init to default */
 static void __init valleyview2_arch_setup(void)
