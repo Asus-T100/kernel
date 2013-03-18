6bab9de7139284907b45b93e5a938bcb677884bb
Shobhit Kumar <shobhit.kumar@intel.com>
2013-03-18 14:54:07 +0530

    drm/i915: Intel dsi support header
    BZ: 105082
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    
    Change-Id: Iebcd136cfbd666945c839a4a8730e0481bfb7d90
    v2: Cleaned an backported on kernel 3.4
    Signed-off-by: Shobhit Kumar <shobhit.kumar@intel.com>
    Reviewed-on: http://android.intel.com:8080/109596
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Stimson, Dale B <dale.b.stimson@intel.com>
    Tested-by: Stimson, Dale B <dale.b.stimson@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ drivers/gpu/drm/i915/intel_dsi.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright © 2013 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including withouti limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Author: Jani Nikula <jani.nikula@intel.com>  */
+
+#ifndef _INTEL_DSI_H
+#define _INTEL_DSI_H
+
+#include <drm/drmP.h>
+#include <drm/drm_crtc.h>
+#include "intel_drv.h"
+
+struct intel_dsi_device {
+	const char *name;
+	int type;
+	unsigned int lane_count;
+	const struct intel_dsi_dev_ops *dev_ops;
+	void *dev_priv;
+};
+
+/* XXX: this is just copy-paste from dvo for now */
+struct intel_dsi_dev_ops {
+	bool (*init)(struct intel_dsi_device *dsi);
+
+	void (*create_resources)(struct intel_dsi_device *dsi);
+
+	void (*dpms)(struct intel_dsi_device *dsi, bool enable);
+
+	int (*mode_valid)(struct intel_dsi_device *dsi,
+			  struct drm_display_mode *mode);
+
+	bool (*mode_fixup)(struct intel_dsi_device *dsi,
+			   const struct drm_display_mode *mode,
+			   struct drm_display_mode *adjusted_mode);
+
+	void (*prepare)(struct intel_dsi_device *dsi);
+
+	void (*commit)(struct intel_dsi_device *dsi);
+
+	void (*mode_set)(struct intel_dsi_device *dsi,
+			 struct drm_display_mode *mode,
+			 struct drm_display_mode *adjusted_mode);
+
+	enum drm_connector_status (*detect)(struct intel_dsi_device *dsi);
+
+	bool (*get_hw_state)(struct intel_dsi_device *dev);
+
+	struct drm_display_mode *(*get_modes)(struct intel_dsi_device *dsi);
+
+	void (*destroy) (struct intel_dsi_device *dsi);
+
+	void (*dump_regs)(struct intel_dsi_device *dsi);
+};
+
+struct intel_dsi {
+	struct intel_encoder base;
+
+	struct intel_dsi_device dev;
+
+	struct drm_display_mode *panel_fixed_mode;
+
+	/* XXX: are hs mode and channel properties of intel_dsi or
+	 * intel_dsi_device?
+	 */
+	bool hs; /* if true, use HS mode, otherwise LP */
+	int channel;
+};
+
+static inline struct intel_dsi *enc_to_intel_dsi(struct drm_encoder *encoder)
+{
+	return container_of(encoder, struct intel_dsi, base.base);
+}
+
+#endif /* _INTEL_DSI_H */
