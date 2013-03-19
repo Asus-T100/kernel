698c1cd2b9bea630b040aad2ad20f74005bfb729
xiaoming wang <xiaoming.wang@intel.com>
2013-03-19 15:03:39 -0400

    [PORT FROM R42L][sst]: block_list has been destroyed in sst_wake_up_block
    BZ: 93234
    
    sst_process_reply_mfld -> sst_wake_up_block
    &ctx->block_list be destroyed while doing list_for_each_entry in sst_wake_up_block.
    So add ctx->block_lock to guard it.
    
    Change-Id: Ib0e7e14e102f1d03e8ab35088e96aad09be8aac1
    Orig-Change-Id: Ib0bd3c0cab634c8b89f5fee48cc3bab88973a5aa
    Signed-off-by: xiaoming wang <xiaoming.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/97217
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Tenoutit, FaouazX <faouazx.tenoutit@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- sound/soc/mid-x86/sst/intel_sst_ipc.c
+++ sound/soc/mid-x86/sst/intel_sst_ipc.c
@@ -65,6 +65,7 @@ int sst_wake_up_block(struct intel_sst_drv *ctx, int result,
 	struct sst_block *block = NULL;
 
 	pr_debug("in %s\n", __func__);
+	spin_lock(&ctx->block_lock);
 	list_for_each_entry(block, &ctx->block_list, node) {
 		pr_debug("Block ipc %d, drv_id %d\n", block->msg_id, block->drv_id);
 		if (block->msg_id == ipc && block->drv_id == drv_id) {
@@ -73,11 +74,12 @@ int sst_wake_up_block(struct intel_sst_drv *ctx, int result,
 			block->data = data;
 			block->size = size;
 			block->condition = true;
+			spin_unlock(&ctx->block_lock);
 			wake_up(&ctx->wait_queue);
 			return 0;
 		}
 	}
-
+	spin_unlock(&ctx->block_lock);
 	pr_debug("Block not found or a response is received for a short message for ipc %d, drv_id %d\n",
 			ipc, drv_id);
 	return -EINVAL;
@@ -88,15 +90,18 @@ int sst_free_block(struct intel_sst_drv *ctx, struct sst_block *freed)
 	struct sst_block *block = NULL, *__block;
 
 	pr_debug("in %s\n", __func__);
+	spin_lock(&ctx->block_lock);
 	list_for_each_entry_safe(block, __block, &ctx->block_list, node) {
 		if (block == freed) {
 			list_del(&freed->node);
 			kfree(freed->data);
 			freed->data = NULL;
 			kfree(freed);
+			spin_unlock(&ctx->block_lock);
 			return 0;
 		}
 	}
+	spin_unlock(&ctx->block_lock);
 	return -EINVAL;
 }
 
