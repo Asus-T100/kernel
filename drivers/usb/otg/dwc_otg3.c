15da066dffc9a3301cff37e7ceffc06b15df1610
yu wang <yu.y.wang@intel.com>
2013-03-19 14:35:34 +0800

    dwc3-otg: Add one more PCI ID to match dwc3-otg driver.
    BZ: 94036
    
    Add one more PCI ID to match dwc3-otg driver. It is for the synopsys
    USB3 OTG controller on Intel TNG and ANN platform.
    Signed-off-by: yu wang <yu.y.wang@intel.com>
    
    Change-Id: I321953d8b8c57de3d92cea626ddf8b1ac1ef45f1
    Reviewed-on: http://android.intel.com:8080/97228
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Tang, Jianqiang <jianqiang.tang@intel.com>
    Reviewed-by: Wu, Hao <hao.wu@intel.com>
    Reviewed-by: Gao, Yunpeng <yunpeng.gao@intel.com>
    Tested-by: Li, GuangleiX <guangleix.li@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/usb/otg/dwc_otg3.c
+++ drivers/usb/otg/dwc_otg3.c
@@ -1942,6 +1942,10 @@ static DEFINE_PCI_DEVICE_TABLE(pci_ids) = {
 	},
 	{ PCI_DEVICE_CLASS(((PCI_CLASS_SERIAL_USB << 8) | 0x80), ~0),
 		.vendor = PCI_VENDOR_ID_INTEL,
+		.device = PCI_DEVICE_ID_DWC,
+	},
+	{ PCI_DEVICE_CLASS(((PCI_CLASS_SERIAL_USB << 8) | 0x80), ~0),
+		.vendor = PCI_VENDOR_ID_INTEL,
 		.device = PCI_DEVICE_ID_DWC_VLV,
 	},
 	{ /* end: all zeroes */ }
