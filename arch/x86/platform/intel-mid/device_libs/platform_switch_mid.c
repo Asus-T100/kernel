1dc1a6b0614d5e3498450682d5397e7a7bbe2a0e
Christophe Charrier <christophex.charrier@intel.com>
2013-04-11 11:09:02 +0200

    Revert "Audio: Fix to load correct switch driver based on sfi entry"
    BZ: 97907
    
    Jack detection with wolfson codec uses switch arizona, whereas
    for cs42l73 switch intel-mid driver is loaded. With both the driver
    loaded, sysfs entry creation creates conflict and switch arizona
    driver fails to load.
    
    Parse the SFI entry to identify the codec type and if the identified
    codec type is wolfson codec prevent the switch-mid device allocation.
    
    Orig-Change-Id: I016f51b89ab452543b48faa88d9fe285c1ae7c7d
    
    This reverts commit d51175bff0c23120d36b053682d8fca2af799352.
    
    Change-Id: I6ea9378430c428d6cd83a760d2f2b04883509357
    Signed-off-by: Christophe Charrier <christophex.charrier@intel.com>
    Reviewed-on: http://android.intel.com:8080/101190

--- arch/x86/platform/intel-mid/device_libs/platform_switch_mid.c
+++ arch/x86/platform/intel-mid/device_libs/platform_switch_mid.c
@@ -24,37 +24,9 @@ static struct platform_device switch_device = {
 	.id		= -1,
 };
 
-static char *wm_codecs[] = {"wm5102"};
-
-static int __init sfi_parse_check_wm(struct sfi_table_header *table)
-{
-	struct sfi_table_simple *sb;
-	struct sfi_device_table_entry *pentry;
-	int num, i, j;
-
-	sb = (struct sfi_table_simple *)table;
-	num = SFI_GET_NUM_ENTRIES(sb,
-			struct sfi_device_table_entry);
-	pentry = (struct sfi_device_table_entry *) sb->pentry;
-	for (i = 0; i < num; i++, pentry++) {
-		for (j = 0; j < ARRAY_SIZE(wm_codecs); j++) {
-			if (!strncmp(pentry->name, wm_codecs[j], SFI_NAME_LEN))
-				return 1;
-		}
-	}
-	return 0;
-}
-
 static int __init switch_mid_init(void)
 {
 	int err;
-
-	err = sfi_table_parse(SFI_SIG_DEVS, NULL, NULL, sfi_parse_check_wm);
-	if (err) {
-		pr_info("This board uses wm codec, not loading switch-mid\n");
-		return 0;
-	}
-
 	err = platform_device_register(&switch_device);
 	if (err < 0)
 		pr_err("Fail to register switch-mid platform device.\n");
