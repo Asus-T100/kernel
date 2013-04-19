b1d7aa3d722207f2e84154d2913c66184be52c48
Omair Mohammed Abdullah <omair.m.abdullah@linux.intel.com>
2013-04-19 09:02:30 +0530

    audio: byt: add acpi support for machine and codec drivers
    BZ: 101921
    
    Baytrail doesn't have any SFI tables, the machine and codec devices are ACPI
    devices present in the system DSDT.
    
    This patch adds ACPI support for the corresponding drivers and changes the
    device IDs accordingly.
    
    Change-Id: I0efb9458dccf18ab48369cb382e92716d30ca603
    Signed-off-by: Omair Mohammed Abdullah <omair.m.abdullah@linux.intel.com>
    Reviewed-on: http://android.intel.com:8080/102776
    Reviewed-by: Abdullah, Omair M <omair.m.abdullah@intel.com>
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Ernst, Eric <eric.ernst@intel.com>
    Tested-by: Joshi, SantoshX <santoshx.joshi@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_byt_audio.c
+++ arch/x86/platform/intel-mid/device_libs/platform_byt_audio.c
@@ -12,16 +12,14 @@
 
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
-#include <linux/scatterlist.h>
 #include <linux/init.h>
 #include <linux/sfi.h>
 #include <linux/platform_device.h>
 #include <asm/intel-mid.h>
-#include <asm/intel_mid_remoteproc.h>
 #include <asm/platform_sst_audio.h>
 #include <asm/platform_byt_audio.h>
 
-#define LPE_IRQ	29
+#define SST_IRQ	29
 
 static struct byt_audio_platform_data byt_audio_pdata = {
 	.spid = &spid,
@@ -42,7 +40,7 @@ static inline int byt_program_ioapic(int irq, int trigger, int polarity)
 	return io_apic_set_pci_routing(NULL, irq, &irq_attr);
 }
 
-void *byt_audio_platform_data(void *info)
+static int __init byt_audio_platform_init(void)
 {
 	struct platform_device *pdev;
 	int ret;
@@ -52,34 +50,35 @@ void *byt_audio_platform_data(void *info)
 	ret = add_sst_platform_device();
 	if (ret < 0) {
 		pr_err("%s failed to sst_platform device\n", __func__);
-		return NULL;
+		return 0;
 	}
 
 	/* configure SST IRQ */
-	ret = byt_program_ioapic(LPE_IRQ, 0, 1);
+	ret = byt_program_ioapic(SST_IRQ, 0, 1);
 	if (ret) {
 		pr_err("%s: ioapic programming failed", __func__);
-		return NULL;
+		return 0;
 	}
 
 	pdev = platform_device_alloc("byt_rt5642", -1);
 	if (!pdev) {
 		pr_err("failed to allocate byt_rt5642 platform device\n");
-		return NULL;
+		return 0;
 	}
 
 	ret = platform_device_add(pdev);
 	if (ret) {
 		pr_err("failed to add byt_rt5642 platform device\n");
 		platform_device_put(pdev);
-		return NULL;
+		return 0;
 	}
 	if (platform_device_add_data(pdev, &byt_audio_pdata,
 				     sizeof(byt_audio_pdata))) {
 		pr_err("failed to add byt_rt5642 platform data\n");
 		platform_device_put(pdev);
-		return NULL;
+		return 0;
 	}
 
-	return NULL;
+	return 0;
 }
+device_initcall(byt_audio_platform_init);
