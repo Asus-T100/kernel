a110146fac2928ebc003c0230789628b89da918b
Patrick Combes <patrick.combes@intel.com>
2013-04-25 12:10:01 +0200

    Revert "[REVERTME] BT LPM: Disable LPM mode for BYT"
    BZ: 104747
    
    This reverts commit 896cf9951358766e97bc9cec991f27a2a132e87a.
    
    Change-Id: I561f0cb7ba839de6129304c91e9271d11a47fb5d
    Signed-off-by: Patrick Combes <patrick.combes@intel.com>
    Reviewed-on: http://android.intel.com:8080/105009
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Gros, Julien <julien.gros@intel.com>
    Reviewed-by: Vamour, Cedric <cedric.vamour@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/misc/bcm-lpm/bcm_bt_lpm.c
+++ drivers/misc/bcm-lpm/bcm_bt_lpm.c
@@ -441,11 +441,9 @@ static int bcm43xx_bluetooth_probe(struct platform_device *pdev)
 		goto err_rfkill_register;
 
 #ifdef LPM_ON
-	if (intel_mid_identify_cpu() != INTEL_MID_CPU_CHIP_VALLEYVIEW2) {
-		ret = bcm_bt_lpm_init(pdev);
-		if (ret)
-			goto err_lpm_init;
-	}
+	ret = bcm_bt_lpm_init(pdev);
+	if (ret)
+		goto err_lpm_init;
 #endif
 
 	return ret;
