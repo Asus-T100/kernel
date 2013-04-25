093b3c7f0d8daababff8cd5478e48b2154cdff6f
Jukka Kaartinen <jukka.o.kaartinen@intel.com>
2013-04-25 11:45:54 +0300

    atomisp2: Start watchdog in streamon
    BZ: 101082
    
    Fixes bug where watchdog is not set on when streaming on is called and
    sensor fails to start sending frames.
    
    Change-Id: I5932cad0b9e9c261bc190ece7273e6c76e5f25aa
    Signed-off-by: Jukka Kaartinen <jukka.o.kaartinen@intel.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-on: http://android.intel.com:8080/104893
    Reviewed-by: Ailus, Sakari <sakari.ailus@intel.com>
    Reviewed-by: Koskinen, Ilkka <ilkka.koskinen@intel.com>
    Tested-by: Koski, Anttu <anttu.koski@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/media/video/atomisp2/atomisp_ioctl.c
+++ drivers/media/video/atomisp2/atomisp_ioctl.c
@@ -1382,8 +1382,6 @@ static int atomisp_streamon(struct file *file, void *fh,
 		isp->wdt_duration = ATOMISP_ISP_FILE_TIMEOUT_DURATION;
 	else
 		isp->wdt_duration = ATOMISP_ISP_TIMEOUT_DURATION;
-	if (atomisp_buffers_queued(isp))
-		mod_timer(&isp->wdt, jiffies + isp->wdt_duration);
 	isp->fr_status = ATOMISP_FRAME_STATUS_OK;
 	isp->sw_contex.invalid_frame = false;
 	isp->params.dis_proj_data_valid = false;
@@ -1426,6 +1424,9 @@ start_sensor:
 		atomisp_reset(isp);
 		ret = -EINVAL;
 	}
+
+	if (atomisp_buffers_queued(isp))
+		mod_timer(&isp->wdt, jiffies + isp->wdt_duration);
 
 out:
 	mutex_unlock(&isp->mutex);
