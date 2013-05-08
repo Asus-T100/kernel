e7c1f1670ce3a7a80a4e1a55ebc28e4d6991d0cb
Vincent Tinelli <vincent.tinelli@intel.com>
2013-05-08 13:02:54 +0000

    spid: add BYT values
    BZ: 88805
    
    add real spid values for BYT instead of fake ones
    
    Change-Id: I3aaa0d0af8eeeb92c0f3c9d792af15c3181c235f
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>
    Reviewed-on: http://android.intel.com:8080/106804
    Reviewed-by: Chouleur, Sylvain <sylvain.chouleur@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Tested-by: Ng, Cheon-woei <cheon-woei.ng@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- arch/x86/platform/intel-mid/intel_mid_sfi.c
+++ arch/x86/platform/intel-mid/intel_mid_sfi.c
@@ -32,8 +32,8 @@ static struct sfi_table_oemb byt_oemb_table = {
 		.customer_id = CUSTOMER_INTEL,
 		.vendor_id = VENDOR_INTEL,
 		.platform_family_id = INTEL_BYT_TABLET,
-		.product_line_id = INTEL_BYT_TABLET_TBD_ENG,
-		.hardware_id = BYT_TABLET_TBD_TBD0,
+		.product_line_id = INTEL_BYT_TABLET_BLK_ENG,
+		.hardware_id = BYT_TABLET_FRCB_VV3,
 	},
 };
 
