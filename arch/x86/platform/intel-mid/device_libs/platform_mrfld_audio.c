ecf71279a2fa467aee8bf7f10390bff8fc71f0a5
Vinod Koul <vinod.koul@intel.com>
2013-05-15 18:19:10 +0530

    audio: merr: add pdata handler for mrfld_wm8958 device
    BZ: 103983
    
    Change-Id: I1b607c6f4c2c3131eed0766276145573a665024e
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Reviewed-on: http://android.intel.com:8080/108630
    Reviewed-by: Nilofer, Samreen <samreen.nilofer@intel.com>
    Reviewed-by: Vinnakota, Lakshmi N <lakshmi.n.vinnakota@intel.com>
    Tested-by: Gupta, ArvindX K <arvindx.k.gupta@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_mrfld_audio.c
+++ arch/x86/platform/intel-mid/device_libs/platform_mrfld_audio.c
@@ -81,3 +81,52 @@ void *merfld_audio_platform_data(void *info)
 
 	return NULL;
 }
+
+void *merfld_wm8958_audio_platform_data(void *info)
+{
+	struct platform_device *pdev;
+	int ret;
+
+	ret = add_sst_platform_device();
+	if (ret < 0) {
+		pr_err("%s failed to sst_platform device\n", __func__);
+		return NULL;
+	}
+
+	pdev = platform_device_alloc("hdmi-audio", -1);
+	if (!pdev) {
+		pr_err("failed to allocate hdmi-audio platform device\n");
+		return NULL;
+	}
+
+	ret = platform_device_add(pdev);
+	if (ret) {
+		pr_err("failed to add hdmi-audio platform device\n");
+		platform_device_put(pdev);
+		return NULL;
+	}
+
+	pdev = platform_device_alloc("mrfld_wm8958", -1);
+	if (!pdev) {
+		pr_err("failed to allocate mrfld_wm8958 platform device\n");
+		return NULL;
+	}
+
+	ret = platform_device_add(pdev);
+	if (ret) {
+		pr_err("failed to add mrfld_wm8958 platform device\n");
+		platform_device_put(pdev);
+		return NULL;
+	}
+	if (platform_device_add_data(pdev, &mrfld_audio_pdata,
+				     sizeof(mrfld_audio_pdata))) {
+		pr_err("failed to add mrfld_wm8958 platform data\n");
+		platform_device_put(pdev);
+		return NULL;
+	}
+
+	register_rpmsg_service("rpmsg_mrfld_wm8958_audio", RPROC_SCU,
+				RP_MSIC_MRFLD_AUDIO);
+
+	return NULL;
+}
