b9c8dd997f086b77948b519b8e771af34f27369a
Piotr Luc <piotr.luc@intel.com>
2013-05-17 17:24:05 +0200

    drm/i9i5: Disable WaApplyL3ControlAndL3ChickenMode for VLV B0
    BZ: 105775
    
    Disable WaApplyL3ControlAndL3ChickenMode workaround for VLV B0 as it cause
    GPU hangs for some benchmarks.
    
    Change-Id: I72f772f211333349748f93efcc0a533228757f65
    Signed-off-by: Piotr Luc <piotr.luc@intel.com>
    Reviewed-on: http://android.intel.com:8080/108235
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Stimson, Dale B <dale.b.stimson@intel.com>
    Tested-by: Stimson, Dale B <dale.b.stimson@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -3711,7 +3711,14 @@ static void valleyview_init_clock_gating(struct drm_device *dev)
 		   GEN7_CSC1_RHWO_OPT_DISABLE_IN_RCC);
 
 	/* WaApplyL3ControlAndL3ChickenMode requires those two on Ivy Bridge */
-	I915_WRITE(GEN7_L3CNTLREG1, I915_READ(GEN7_L3CNTLREG1) | GEN7_L3AGDIS);
+	if (IS_VALLEYVIEWP_M(dev)) {
+		I915_WRITE(GEN7_L3CNTLREG1,
+			I915_READ(GEN7_L3CNTLREG1) & (~GEN7_L3AGDIS));
+	} else {
+		I915_WRITE(GEN7_L3CNTLREG1,
+			I915_READ(GEN7_L3CNTLREG1) | GEN7_L3AGDIS);
+	}
+
 	I915_WRITE(GEN7_L3_CHICKEN_MODE_REGISTER, GEN7_WA_L3_CHICKEN_MODE);
 
 	/* WaDisableDopClockGating */
