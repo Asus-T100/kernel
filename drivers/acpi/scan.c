307670552fdb2522a49cb71bf86c78c83ae4b6d8
Sathyanarayanan Kuppuswamy <sathyanarayanan.kuppuswamy@intel.com>
2013-06-19 13:35:11 -0700

    [REVERT ME] acpi: Remove AMCR0F28 from platform device list
    BZ: 117537
    
    RT5642 driver creates kernel panic if enumerated through ACPI
    framework. In sound/soc/intel/board/byt_bl_rt5642.c, snd_byt_mc_probe()
    tries to access platform data without any NULL pointer checks. But
    dev.platform_data will be NULL if the platform device is enumerated
    through ACPI framework.
    
    So as a temproray fix this patch removes AMCR0F28 from
    platform device list.
    
    This patch needs to be reverted once proper fix is added to
    rt5642 driver.
    
    Change-Id: I3c8d8330136e42346ee4c0f59a09075d050939e6
    Signed-off-by: Sathyanarayanan Kuppuswamy <sathyanarayanan.kuppuswamy@intel.com>
    Reviewed-on: http://android.intel.com:8080/114455
    Reviewed-by: Gross, Mark <mark.gross@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Tested-by: Ranostay, Matt <matt.ranostay@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -37,7 +37,6 @@ static const struct acpi_device_id acpi_platform_device_ids[] = {
 	{ "PNP0D40" },
 	{ "BCM2E1A" },
 	{ "LPE0F28", 0 },
-	{ "AMCR0F28", 0 },
 
 	/* Haswell LPSS devices */
 	{ "INT33C0", 0 },
