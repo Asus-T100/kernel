1f3f0c183c2336663cd8c232e9bd6f144240defe
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2013-06-28 05:33:59 +0530

    BYT-M/EC: add EC driver(non ACPI) support for BYT-M
    BZ: 112457
    
    This patch adds the EC driver access support for baytrail
    platform and also handles the SCI events from EC.
    
    Change-Id: Ia18853dbc63e301ebbbf96d9d75106ade1a42c29
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    Reviewed-on: http://android.intel.com:8080/115942
    Reviewed-by: Tc, Jenny <jenny.tc@intel.com>
    Tested-by: Koosalapur, Siddalingesh S <siddalingesh.s.koosalapur@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ arch/x86/include/asm/intel_byt_ec.h
@@ -0,0 +1,73 @@
+
+#ifndef __INTEL_BYT_EC_H_
+#define __INTEL_BYT_EC_H_
+
+/*
+ * SCI NOTIFICATIONS: EC When generates an event or interrupts
+ * EC driver has to query the EC and based on the event EC driver
+ * has to notify the corresponding consumer driver.
+ */
+#define BYT_EC_SCI_SMB			0x01	/* SMBus event notification */
+#define BYT_EC_SCI_DEVINSERTION		0x20	/* Device bay insertion SCI */
+#define BYT_EC_SCI_DEVREMOVAL		0x21	/* Device bay removal SCI */
+#define BYT_EC_SCI_NEWCARD		0x22	/* NewCard insertion event SCI */
+#define BYT_EC_SCI_ACINSERTION		0x30	/* AC insertion SCI */
+#define BYT_EC_SCI_ACREMOVAL		0x31	/* AC removal SCI */
+#define BYT_EC_SCI_BATTERY		0x32	/* Battery event SCI */
+#define BYT_EC_SCI_BATTERY_PRSNT	0x33	/* Battery insertion or removal SCI */
+#define BYT_EC_SCI_DOCKED		0x40	/* Dock complete SCI */
+#define BYT_EC_SCI_UNDOCKED		0x41	/* Undock complete SCI */
+#define BYT_EC_SCI_UNDOCKREQUEST	0x42	/* Undocking request SCI */
+#define BYT_EC_SCI_EXPCARDPRSNT		0x43	/* Express card insertion/removal SCI */
+
+#define BYT_EC_SCI_RING			0x50	/* SCI from ring indicate event */
+#define BYT_EC_SCI_LID			0x51	/* SCI from lid */
+#define BYT_EC_SCI_HOTKEY		0x52	/* SCI from keyboard hotkey */
+#define BYT_EC_SCI_VB			0x53	/* SCI from virtual battery switch */
+#define BYT_EC_SCI_PWRBTN		0x54	/* SCI from EL power button */
+#define BYT_EC_SCI_RESUME		0x55	/* SCI from Resuming from S3 */
+
+#define BYT_EC_SCI_BT_PWR_OFF		0x60	/* SCI from Bluetooth power off */
+#define BYT_EC_SCI_BT_PWR_ON		0x61	/* SCI from Bluetooth power on */
+#define BYT_EC_SCI_ALS			0x70	/* SCI from ALS lux change */
+#define BYT_EC_SCI_BURST_ACK		0x90	/* SCI burst acknowledge byte */
+#define BYT_EC_SCI_GYSRVL		0xB0	/* SCI for Geyserville break event */
+#define BYT_EC_SCI_THERMAL		0xF0	/* Thermal transition SCI */
+#define BYT_EC_SCI_THERMTRIP		0xF1	/* Thermal trip point transition SCI */
+
+/* Legacy Event from EC */
+#define BYT_EC_SCI_AON_UP		0xD0	/* SCI from AON Up button */
+#define BYT_EC_SCI_AON_DOWN		0xD1	/* SCI from AON Down button */
+#define BYT_EC_SCI_AON_SELECT		0xD2	/* SCI from AON Select button */
+#define BYT_EC_SCI_AON_ESC		0xD3	/* SCI from AON Escape button */
+#define BYT_EC_SCI_AON_GESC		0xD4	/* SCI from AON Global Escape button */
+
+#ifdef CONFIG_INTEL_BYT_EC
+extern int byt_ec_read_byte(u8 addr, u8 *val);
+extern int byt_ec_write_byte(u8 addr, u8 val);
+extern int byt_ec_read_word(u8 addr, u16 *val);
+extern int byt_ec_write_word(u8 addr, u16 val);
+extern void byt_ec_evt_register_notify(struct notifier_block *nb);
+extern void byt_ec_evt_unregister_notify(struct notifier_block *nb);
+#else
+static int byt_ec_read_byte(u8 addr, u8 *val)
+{
+	return 0;
+}
+static int byt_ec_write_byte(u8 addr, u8 val)
+{
+	return 0;
+}
+static int byt_ec_read_word(u8 addr, u16 *val)
+{
+	return 0;
+}
+static int byt_ec_write_word(u8 addr, u16 val)
+{
+	return 0;
+}
+static void byt_ec_evt_register_notify(struct notifier_block *nb) { }
+static void byt_ec_evt_unregister_notify(struct notifier_block *nb) { }
+#endif
+
+#endif	/* __INTEL_BYT_EC_H_ */
