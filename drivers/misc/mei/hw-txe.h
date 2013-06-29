50355fb2af2c1c7722b0d35b7444032dd4633ffb
Tomas Winkler <tomas.winkler@intel.com>
2013-06-30 02:12:06 +0300

    mei: txe: add function for setting satt2 register
    BZ: 120100
    
    satt2 register enable a range of the physical memory to be
    used for dma transactions
    
    Change-Id: I9c14d680348e861855583b3335fe6d126238fdbf
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Reviewed-on: http://android.intel.com:8080/116246
    Reviewed-by: Allyn, Mark A <mark.a.allyn@intel.com>
    Tested-by: Allyn, Mark A <mark.a.allyn@intel.com>
    Reviewed-by: Noziska, Patrick J <patrick.j.noziska@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/misc/mei/hw-txe.h
+++ drivers/misc/mei/hw-txe.h
@@ -62,6 +62,8 @@ irqreturn_t mei_txe_irq_quick_handler(int irq, void *dev_id);
 irqreturn_t mei_txe_irq_thread_handler(int irq, void *dev_id);
 int mei_txe_aliveness_set_sync(struct mei_device *dev, u32 req);
 
+int mei_txe_setup_satt2(struct mei_device *dev, phys_addr_t addr, u32 range);
+
 int mei_txe_aliveness_set_sync(struct mei_device *dev, u32 req);
 
 #endif /* _MEI_HW_TXE_H_ */
