85ffd7f472469c453cb97b4e59c3438c0bb6e487
Durgadoss R <durgadoss.r@intel.com>
2013-07-10 17:28:43 +0530

    EM/THERMAL: Conditionally include PMIC APIs
    BZ: 120964
    
    This patch makes the PMIC read/write APIs
    available depending on Crystal cove driver's
    availability. This helps to use these APIs
    in platform data layer (device libs) such that
    they do not throw compilation errors on platforms
    where crystal cove PMIC driver is not used.
    
    Change-Id: I7d5f291604140a199b2b7c0488171dc5190b1fa7
    Signed-off-by: Durgadoss R <durgadoss.r@intel.com>
    Reviewed-on: http://android.intel.com:8080/117952
    Reviewed-by: Li, Ning <ning.li@intel.com>
    Reviewed-by: B M, Shravan K <shravan.k.b.m@intel.com>
    Reviewed-by: Yang, Bin <bin.yang@intel.com>
    Reviewed-by: Tc, Jenny <jenny.tc@intel.com>
    Tested-by: Koosalapur, Siddalingesh S <siddalingesh.s.koosalapur@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- include/linux/mfd/intel_mid_pmic.h
+++ include/linux/mfd/intel_mid_pmic.h
@@ -1,11 +1,21 @@
 #ifndef __CRYSTAL_COVE_H__
 #define __CRYSTAL_COVE_H__
 
+#ifdef CONFIG_CRYSTAL_COVE
 int intel_mid_pmic_readb(int reg);
 int intel_mid_pmic_writeb(int reg, u8 val);
 int intel_mid_pmic_setb(int reg, u8 mask);
 int intel_mid_pmic_clearb(int reg, u8 mask);
 int intel_mid_pmic_set_pdata(const char *name, void *data, int len);
-
+#else
+static inline int intel_mid_pmic_readb(int reg) { return 0; }
+static inline int intel_mid_pmic_writeb(int reg, u8 val) { return 0; }
+static inline int intel_mid_pmic_setb(int reg, u8 mask) { return 0; }
+static inline int intel_mid_pmic_clearb(int reg, u8 mask) { return 0; }
+static inline int intel_mid_pmic_set_pdata(const char *name,
+				void *data, int len) {
+	return 0;
+}
+#endif
 #endif
 
