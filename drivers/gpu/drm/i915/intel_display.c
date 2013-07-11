3a5e04a3837e2483e5f72c8adc760a5d184b55ba
Uma Shankar <uma.shankar@intel.com>
2013-07-11 16:38:33 +0530

    drm/i915: Added modeset notification to HAD
    BZ: 121604
    
    Modeset event is sent from Display driver to HDMI Audio driver
    when a new mode is selected during extended video mode scenario.
    
    Change-Id: I1a8004f2d83c6268b808452b2a74e769725cb7ca
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Reviewed-on: http://android.intel.com:8080/118195
    Reviewed-by: Kumar, Shobhit <shobhit.kumar@intel.com>
    Tested-by: Sun, Hang L <hang.l.sun@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -5240,7 +5240,10 @@ static int i9xx_crtc_mode_set(struct drm_crtc *crtc,
 	if (IS_VALLEYVIEW(dev) && intel_pipe_has_type(crtc,
 		INTEL_OUTPUT_HDMI)) {
 		dev_priv->tmds_clock_speed = adjusted_mode->clock;
+		mid_hdmi_audio_signal_event(dev_priv->dev,
+			HAD_EVENT_MODE_CHANGING);
 	}
+
 	return ret;
 }
 
