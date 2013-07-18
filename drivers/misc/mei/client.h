4947e06d5a4ca72d78987ca687f987535eba9ddd
Tomas Winkler <tomas.winkler@intel.com>
2013-07-18 21:10:21 +0300

    mei: wake also writers on reset
    BZ: 140048
    
    wake writers otherwise might have the processes waiting
    endlessly on wait_tx during reset
    
    Change-Id: I8dfc9d5dcce74d86acf2284a7d1ece5818a0b95d
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Reviewed-on: http://android.intel.com:8080/135136
    Reviewed-by: Allyn, Mark A <mark.a.allyn@intel.com>
    Tested-by: Allyn, Mark A <mark.a.allyn@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/misc/mei/client.h
+++ drivers/misc/mei/client.h
@@ -105,7 +105,7 @@ void mei_host_client_init(struct work_struct *work);
 
 
 void mei_cl_all_disconnect(struct mei_device *dev);
-void mei_cl_all_read_wakeup(struct mei_device *dev);
+void mei_cl_all_wakeup(struct mei_device *dev);
 void mei_cl_all_write_clear(struct mei_device *dev);
 
 #define MEI_CL_FMT "cl:host=%02d me=%02d "
