d3fc013a89ec3156fc11c1613a8b4b099791a9d9
Richard A. Griffiths <Richard.A.Griffiths@intel.com>
2013-07-23 15:51:54 -0700

    intel_mid_dma: fix klocwork critical issue
    BZ: 125479
    
    check for NULL pointer before dereferncing.
    
    Change-Id: I53a22af7e88d985328965e550430db8539030b42
    Signed-off-by: Richard A. Griffiths <Richard.A.Griffiths@intel.com>
    Reviewed-on: http://android.intel.com:8080/120182
    Reviewed-by: Griffiths, Richard A <richard.a.griffiths@intel.com>
    Reviewed-by: Noziska, Patrick J <patrick.j.noziska@intel.com>
    Tested-by: Ranostay, Matt <matt.ranostay@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- drivers/dma/intel_mid_dma.c
+++ drivers/dma/intel_mid_dma.c
@@ -1549,15 +1549,15 @@ static irqreturn_t intel_mid_dma_interrupt(int irq, void *data)
 	u32 tfr_status, err_status, block_status;
 	u32 isr;
 
-	/* On Baytrail, the DMAC is sharing IRQ with other devices */
-	if (is_byt_lpio_dmac(mid) && mid->state == SUSPENDED)
-		return IRQ_NONE;
-
 	/*DMA Interrupt*/
 	pr_debug("MDMA:Got an interrupt on irq %d\n", irq);
 	if (!mid) {
 		pr_err("ERR_MDMA:null pointer mid\n");
 		return -EINVAL;
+	} else {
+	/* On Baytrail, the DMAC is sharing IRQ with other devices */
+	if (is_byt_lpio_dmac(mid) && mid->state == SUSPENDED)
+		return IRQ_NONE;
 	}
 
 	/* Read the interrupt status registers */
