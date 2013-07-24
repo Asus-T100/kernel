c4413ad6cf751f3ff629dcf8f128570e64c8c279
Du, Changbin <changbinx.du@intel.com>
2013-07-24 23:31:47 +0800

    usb: penwell_otg: fix missing overcurrent warning
    BZ: 125837
    
    OTG driver doesn't send overcurrent warning if overcurrent occurs
    when otg is in OTG_STATE_A_WAIT_VRISE state. This may occur if
    user plugs OTG cable which already has device connected.
    
    Change-Id: I7f07e1b90e820c646b3dd21452e337e27c593035
    Signed-off-by: Du, Changbin <changbinx.du@intel.com>
    Reviewed-on: http://android.intel.com:8080/120419
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Tang, Richard <richard.tang@intel.com>
    Reviewed-by: Griffiths, Richard A <richard.a.griffiths@intel.com>
    Tested-by: Wang, Ran <ran.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/usb/otg/penwell_otg.c
+++ drivers/usb/otg/penwell_otg.c
@@ -3738,6 +3738,10 @@ static void penwell_otg_work(struct work_struct *work)
 			penwell_otg_del_timer(TA_WAIT_VRISE_TMR);
 
 			if (!hsm->a_vbus_vld) {
+				/* vbus can't rise to vbus vld, overcurrent */
+				penwell_otg_notify_warning(
+					USB_WARNING_VBUS_INVALID);
+
 				/* Turn off VBUS */
 				otg_set_vbus(iotg->otg.otg, false);
 
