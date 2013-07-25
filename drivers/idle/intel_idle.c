afa3ba941acca07efd7584cfe48185d8b8d3a1b8
mdubutx <marie.celinex.dubut@intel.com>
2013-07-25 17:54:13 +0200

    Revert "intel_idle: deprecate s0i2 and use s0i1 on MRFLD"
    BZ: 120073
    
    This reverts commit 2da562570e9b348f7dc99f16baf1144d760698fa.
    This reverts patch http://android.intel.com:8080/#/c/118830
    
    Change-Id: I05bb197557fbbc5de9ad206fa5a5743fbf171b34
    Signed-off-by: mdubutx <marie.celinex.dubut@intel.com>
    Reviewed-on: http://android.intel.com:8080/120559

--- drivers/idle/intel_idle.c
+++ drivers/idle/intel_idle.c
@@ -205,16 +205,22 @@ static struct cpuidle_state mrfld_cstates[MWAIT_MAX_NUM_CSTATES] = {
 		.name = "S0i1-ATM",
 		.desc = "MWAIT 0x60",
 		.flags = CPUIDLE_FLAG_TIME_VALID | CPUIDLE_FLAG_TLB_FLUSHED,
-		.exit_latency = 800,
-		.target_residency = 1335,
+		.exit_latency = 1200,
+		.target_residency = 4000,
 		.enter = &intel_idle },
-	{ /* MWAIT C8-S0i2 */ }, /* S0i2 is depcreated on Merrifield */
+	{ /* MWAIT C8-S0i2 */
+		.name = "S0i2-ATM",
+		.desc = "MWAIT 0x62",
+		.flags = CPUIDLE_FLAG_TIME_VALID | CPUIDLE_FLAG_TLB_FLUSHED,
+		.exit_latency = 2000,
+		.target_residency = 8000,
+		.enter = &intel_idle },
 	{ /* MWAIT C9-S0i3 */
 		.name = "S0i3-ATM",
 		.desc = "MWAIT 0x64",
 		.flags = CPUIDLE_FLAG_TIME_VALID | CPUIDLE_FLAG_TLB_FLUSHED,
 		.exit_latency = 10000,
-		.target_residency = 370000,
+		.target_residency = 20000,
 		.enter = &intel_idle },
 };
 #endif
