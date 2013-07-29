be6c23f16cd3d5013f95cf59d9ce5e7b0826f038
Patrick Combes <patrick.combes@intel.com>
2013-07-29 09:52:00 +0200

    [BT] Cleanup bcm_bt_lpm_platform_data structure
    BZ: 125652
    
    With ACPI introduction on Baytrail platforms, the fields of structure
    bcm_bt_lpm_platform_data related to UART port are no longer used (see
    patch If115acccf15841bf3b2914377cac83b15e8ac7cf).
    
    This patch removes the fields from the platform data structure.
    
    Change-Id: I9558762c2fd15ec686c89ee994666e7a83cf9b2f
    Signed-off-by: Patrick Combes <patrick.combes@intel.com>
    Reported-by: Frode Isaksen <fisaksex@intel.com>
    Reviewed-on: http://android.intel.com:8080/121611
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Aquilone, RaffaeleX <raffaelex.aquilone@intel.com>
    Reviewed-by: Gros, Julien <julien.gros@intel.com>
    Reviewed-by: Ernst, Eric <eric.ernst@intel.com>
    Tested-by: Lestienne, ChristopheX <christophex.lestienne@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- arch/x86/include/asm/bcm_bt_lpm.h
+++ arch/x86/include/asm/bcm_bt_lpm.h
@@ -14,33 +14,13 @@
 #ifndef BCM_BT_LMP_H
 #define BCM_BT_LMP_H
 
-#include <linux/serial_core.h>
-#include <net/bluetooth/bluetooth.h>
-#include <net/bluetooth/hci_core.h>
-
-/* Uart driver must call this every time it beings TX, to ensure
-* this driver keeps WAKE asserted during TX. Called with uart
-* spinlock held. */
-extern void bcm_bt_lpm_exit_lpm_locked(struct device *dev,
-							struct hci_dev *hdev);
-
 struct bcm_bt_lpm_platform_data {
 	int gpio_wake;		/* CPU -> BCM wakeup gpio */
 	int gpio_host_wake;	/* BCM -> CPU wakeup gpio */
 	int int_host_wake;	/* BCM -> CPU wakeup irq */
 	int gpio_enable;	/* GPIO enable/disable BT/FM */
 
-	int port;			/* UART port to use with BT/FM */
-	/*
-	 * Callback to request the uart driver to clock off.
-	 * Called with uart spinlock held.
-	 */
-	void (*uart_disable)(struct device *tty);
-	/*
-	 * Callback to request the uart driver to clock on.
-	 * Called with uart spinlock held.
-	 */
-	void (*uart_enable)(struct device *tty);
+	int port;		/* UART port to use with BT/FM */
 };
 
 #endif
