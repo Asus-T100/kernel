14f6f57d2642d998ca2f9084cadc05114d6e4db9
Axel Haslam <axelx.haslam@intel.com>
2013-07-31 15:27:27 +0200

    Revert "[REVERT ME] ACPI:device_pm error ENODEV stop system entering S3"
    BZ:127480
    
    This reverts commit 6cccf58751ba9a435b72e6d6cb4d0d10d3385f8b.
    
    Change-Id: Id9d5e165d43a7febb6054a68c68712287965f8bd
    Signed-off-by: Axel Haslam <axelx.haslam@intel.com>
    Reviewed-on: http://android.intel.com:8080/126240
    Reviewed-by: De Coninck, MickaelX <mickaelx.de.coninck@intel.com>
    Tested-by: De Coninck, MickaelX <mickaelx.de.coninck@intel.com>

--- drivers/acpi/device_pm.c
+++ drivers/acpi/device_pm.c
@@ -559,14 +559,7 @@ EXPORT_SYMBOL_GPL(acpi_subsys_prepare);
 int acpi_subsys_suspend_late(struct device *dev)
 {
 	int ret = pm_generic_suspend_late(dev);
-	if (!ret)
-		ret = acpi_dev_suspend_late(dev);
-	/* no device should not stop entering S3 */
-	if (ret == -ENODEV) {
-		dev_err(dev, "error  -%d, ignore it enter S3\n", ret);
-		ret = 0;
-	}
-	return ret;
+	return ret ? ret : acpi_dev_suspend_late(dev);
 }
 EXPORT_SYMBOL_GPL(acpi_subsys_suspend_late);
 
