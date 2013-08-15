e6ceb6bd94da903ffa52be1fb043d8c41c895b9e
Tao, Jing <jing.tao@intel.com>
2013-08-15 19:50:37 +0800

    Revert "atomisp2: workaround for offline qvga timeout issue"
    BZ: 129691
    
    Revert this WA since ISP firwware has fixed qvga offline still capture issue.
    
    This reverts commit 8fd6ecbd775008a961ef8c13190a9ca67313da8a.
    
    Change-Id: I596b5e00d1def67fe41c5f28845d75252db0409a
    Signed-off-by: Tao, Jing <jing.tao@intel.com>
    Reviewed-on: http://android.intel.com:8080/126469
    Reviewed-by: Gong, Shuguang <shuguang.gong@intel.com>
    Tested-by: Gong, Shuguang <shuguang.gong@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/media/video/atomisp2/atomisp_driver/atomisp_cmd.c
+++ drivers/media/video/atomisp2/atomisp_driver/atomisp_cmd.c
@@ -3111,14 +3111,6 @@ int atomisp_try_fmt(struct video_device *vdev, struct v4l2_format *f,
 						 f->fmt.pix.width,
 						 f->fmt.pix.height);
 			}
-			/*WORKAROUND: for qvga offline still capture, isp
-			 * would timeout for 8MP output from sensor.
-			 * but won't timeout for 720p sensor output*/
-			if (f->fmt.pix.width == 320
-				&& f->fmt.pix.height == 240) {
-				snr_mbus_fmt.width = 1280;
-				snr_mbus_fmt.height = 720;
-			}
 		}
 	}
 #endif
@@ -3605,14 +3597,6 @@ static int atomisp_set_fmt_to_snr(struct atomisp_sub_device *asd,
 				    DIV_ROUND_UP(2448 *
 						 f->fmt.pix.width,
 						 f->fmt.pix.height);
-			}
-			/*WORKAROUND: for qvga offline still capture, isp
-			 * would timeout for 8MP output from sensor.
-			 * but won't timeout for 720p sensor output*/
-			if (f->fmt.pix.width == 320
-				&& f->fmt.pix.height == 240) {
-				ffmt.width = 1280;
-				ffmt.height = 720;
 			}
 		}
 	}
