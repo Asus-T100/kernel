faef48a4e8c56c377e97386d42640352e84fe30e
hollie <Hollie_Hung@asus.com>
2013-08-23 17:25:03 +0800

    [T100TA][Battery][Spec][NA] Add battery driver
    Change-Id: I522d9fc7a4fe16904eecc69b80ecfb9a5f0400a9

---
+++ arch/x86/platform/intel-mid/device_libs/platform_byt_battery.c
@@ -0,0 +1,21 @@
+
+#include <linux/i2c.h>
+#include <linux/kernel.h>
+#include <linux/power/byt_battery.h>
+
+static struct byt_platform_data byt_pdata;
+
+static struct i2c_board_info __initdata byt_i2c_device = {
+	I2C_BOARD_INFO("asus_byt", 0x66),
+	.platform_data = &byt_pdata,
+};
+
+static int __init byt_i2c_init(void)
+{
+	byt_pdata.gpio = 0x18;
+	int x = i2c_register_board_info(1, &byt_i2c_device, 1);
+	printk(KERN_ALERT"i2c_register_board_info : %d \n", x);
+	return x;
+}
+module_init(byt_i2c_init);
+
---
+++ include/linux/power/byt_battery.h
@@ -0,0 +1,46 @@
+#ifndef __BYT_BATTERY_H_
+#define __BYT_BATTERY_H_
+
+
+#pragma pack(1)
+struct byt_battery 
+{
+	u16	Control;
+	u16	AtRate;
+	u16	AtRateTimeToEmpty;
+	u16	Temperature;
+	u16	Voltage;
+	u16	Flags;
+	u16	NominalAvailableCapacity;
+	u16	FullAvailableCapacity;
+	u16	RemainingCapacity; //0x10
+	u16	FullChargeCapacity;
+	u16	AverageCurrent;
+	u16	TimeToEmpty;
+	u16	TimeToFull;
+	u16	StandbyCurrent;
+	u16	StandbyTimeToEmpty;
+	u16	MaxLoadCurrent;
+	u16	MaxLoadTimeToEmpty;//0x20
+    	u16	AvailableEnergy;
+    	u16	AveragePower;
+    	u16	TimeToEmptyAtConstantPower;
+	u16	DesignCapacity;
+    	u16	CycleCount;
+	u16	StateOfCharge;
+    	u8	ChargerStatusRegister;
+    	u8	ChargeCurrentLimit;
+    	u32	BatteryTripPoint;
+	u8	SkinTemp0;
+	u16	SkinTemp0Aux0;
+    	u16	SkinTemp0Aux1;
+};
+#pragma pack(0)
+
+
+struct byt_platform_data {
+	int gpio;
+};
+
+
+#endif 
