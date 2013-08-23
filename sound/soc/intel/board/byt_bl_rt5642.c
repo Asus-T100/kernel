883bfc9d18e7a06c7a7a7d68cce091b37eaa8359
Baron_Huang <Baron_Huang@asus.com>
2013-08-23 12:37:51 +0800

    [T100TA][Audio][Fix][NA]open micbias
    Change-Id: Ica08e4c9e9f26df0a86483688dffa5b61187110a

--- sound/soc/intel/board/byt_bl_rt5642.c
+++ sound/soc/intel/board/byt_bl_rt5642.c
@@ -218,7 +218,10 @@ static const struct snd_soc_dapm_widget byt_dapm_widgets[] = {
 static const struct snd_soc_dapm_route byt_audio_map[] = {
 	{"IN2P", NULL, "Headset Mic"},
 	{"IN2N", NULL, "Headset Mic"},
-	{"DMIC1", NULL, "Int Mic"},
+	//{"DMIC1", NULL, "Int Mic"}, //<asus-baron20130823->
+	{"micbias1", NULL, "Int Mic"}, //<asus-baron20130823+>
+	{"IN1P", NULL, "micbias1"}, //<asus-baron20130823+>
+	{"IN1N", NULL, "micbias1"},	//<asus-baron20130823+>
 	{"Headphone", NULL, "HPOL"},
 	{"Headphone", NULL, "HPOR"},
 	{"Ext Spk", NULL, "SPOLP"},
