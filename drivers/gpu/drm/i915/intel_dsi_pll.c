c677be03d208cc3e19f4029e2521aca7c57b83ed
Yogesh Mohan Marimuthu <yogesh.mohan.marimuthu@intel.com>
2013-09-13 12:08:28 +0530

    drm/i915: I915_READ call is missed in disabling ref clock
    BZ: 138077
    
    The if condition to check if both pipes are off missed the I915_READ
    function call. Due to this ref clock was always enabled for MIPI. Fixing
    this issue.
    
    Change-Id: Ic95610a1befa43ec356b10a1d8cf5673c152761f
    Signed-off-by: Yogesh Mohan Marimuthu <yogesh.mohan.marimuthu@intel.com>
    Signed-off-by: Bhat, Gajanan <gajanan.bhat@intel.com>
    Reviewed-on: http://android.intel.com:8080/131885
    Reviewed-by: Kumar, Shobhit <shobhit.kumar@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Tested-by: Sun, Hang L <hang.l.sun@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/gpu/drm/i915/intel_dsi_pll.c
+++ drivers/gpu/drm/i915/intel_dsi_pll.c
@@ -390,12 +390,8 @@ int intel_disable_dsi_pll(struct intel_dsi *intel_dsi)
 	intel_cck_write32_bits(dev_priv, 0x48, 0x00000000, 0x80000000);
 	udelay(500);
 
-	/* FIXME: DSI PLL is disable before pipe is disabled, because of this
-	 * ref clock will not be disabled when only mipi panel is connected.
-	 * Need to fix this.
-	 */
-	if ((PIPECONF(PIPE_A) & PIPECONF_ENABLE == 0) &&
-		(PIPECONF(PIPE_B) & PIPECONF_ENABLE == 0)
+	if ((I915_READ(PIPECONF(PIPE_A)) & PIPECONF_ENABLE == 0) &&
+		(I915_READ(PIPECONF(PIPE_B)) & PIPECONF_ENABLE == 0)
 		)
 		I915_WRITE_BITS(_DPLL_A, 0x00000000, DPLL_REFA_CLK_ENABLE_VLV);
 
