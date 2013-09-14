d0667fef489ac08ee1f24fe11045f5f37c5ed5b1
Shashank Sharma <shashank.sharma@intel.com>
2013-09-14 14:27:11 +0530

    drm/i915: Save color manager status
    BZ: 141802
    Save color manager component status (Gamma, CSC, Hue, Contrast)
    while going to suspend, re-apply while resuming.
    
    Change-Id: Iad8a0c2ce19b91a08b3f069f533e717d00a7190b
    Signed-off-by: Shashank Sharma <shashank.sharma@intel.com>
    Reviewed-on: http://android.intel.com:8080/132722
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Kumar, Shobhit <shobhit.kumar@intel.com>
    Tested-by: Sun, Hang L <hang.l.sun@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/gpu/drm/i915/intel_clrmgr.h
+++ drivers/gpu/drm/i915/intel_clrmgr.h
@@ -39,20 +39,68 @@ struct HueSaturationlut {
 	u32 val;
 };
 
+
+/* CSC correction */
+#define CSC_MAX_COEFF_COUNT		6
+#define CLR_MGR_PARSE_MAX		128
+#define PIPECONF_GAMMA			(1<<24)
+#define GAMMA_CORRECT_MAX_COUNT 256
+#define GAMMA_SP_MAX_COUNT		6
+/* Gamma correction defines */
+#define GAMMA_MAX_VAL			1024
+#define SHIFTBY6(val) (val<<6)
+#define PIPEA_GAMMA_MAX_RED		0x70010
+#define PIPEA_GAMMA_MAX_GREEN	0x70014
+#define PIPEA_GAMMA_MAX_BLUE		0x70018
+/* Sprite gamma correction regs */
+#define GAMMA_SPA_GAMC0			0x721F4
+#define GAMMA_SPA_GAMC1			0x721F0
+#define GAMMA_SPA_GAMC2			0x721EC
+#define GAMMA_SPA_GAMC3			0x721E8
+#define GAMMA_SPA_GAMC4			0x721E4
+#define GAMMA_SPA_GAMC5			0x721E0
+
+#define GAMMA_SPB_GAMC0			0x721F4
+#define GAMMA_SPB_GAMC1			0x721F0
+#define GAMMA_SPB_GAMC2			0x721EC
+#define GAMMA_SPB_GAMC3			0x721E8
+#define GAMMA_SPB_GAMC4			0x721E4
+#define GAMMA_SPB_GAMC5			0x721E0
+
+#define GAMMA_SPA_CNTRL			0x72180
+#define GAMMA_SPB_CNTRL			0x72280
+#define GAMMA_ENABLE_SPR			(1<<30)
+#define GAMMA_SP_MAX_COUNT			6
+#define NO_SPRITE_REG				4
+
+
+/* Color manager features */
+enum ClrMgrFeatures {
+	ClrMgrCsc = 1,
+	ClrMgrGamma,
+	ClrMgrContrBright,
+	ClrMgrHueSat,
+};
+
+/* Required for sysfs entry calls */
 extern u32 CSCSoftlut[CSC_MAX_COEFF_COUNT];
 extern u32 gammaSoftlut[GAMMA_CORRECT_MAX_COUNT];
 extern u32 gammaSpriteSoftlut[GAMMA_SP_MAX_COUNT];
 
-extern int parse_clrmgr_input(uint *dest, char *src, int max, int read);
-extern int do_intel_enable_CSC(struct drm_device *dev, void *data,
+/* Prototypes */
+int parse_clrmgr_input(uint *dest, char *src, int max, int read);
+int do_intel_enable_CSC(struct drm_device *dev, void *data,
 						struct drm_crtc *crtc);
-extern bool intel_pipe_has_type(struct drm_crtc *crtc, int type);
-extern void do_intel_disable_CSC(struct drm_device *dev,
+bool intel_pipe_has_type(struct drm_crtc *crtc, int type);
+void do_intel_disable_CSC(struct drm_device *dev,
 						struct drm_crtc *crtc);
-extern int intel_crtc_enable_gamma(struct drm_crtc *crtc, u32 identifier);
-extern int intel_crtc_disable_gamma(struct drm_crtc *crtc, u32 identifier);
-extern int intel_sprite_cb_adjust(drm_i915_private_t *dev_priv,
+int intel_crtc_enable_gamma(struct drm_crtc *crtc, u32 identifier);
+int intel_crtc_disable_gamma(struct drm_crtc *crtc, u32 identifier);
+int intel_sprite_cb_adjust(drm_i915_private_t *dev_priv,
 		struct ContBrightlut *cb_ptr);
-extern int intel_sprite_hs_adjust(drm_i915_private_t *dev_priv,
+int intel_sprite_hs_adjust(drm_i915_private_t *dev_priv,
 		struct HueSaturationlut *hs_ptr);
+void intel_save_clr_mgr_status(struct drm_device *dev);
+bool intel_restore_clr_mgr_status(struct drm_device *dev);
+
 #endif
