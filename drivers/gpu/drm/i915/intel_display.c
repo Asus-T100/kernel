4a8c37c45d3f7ca6d8fcb95706cdd00d5e61f8e6
Yogesh Mohan Marimuthu <yogesh.mohan.marimuthu@intel.com>
2013-10-15 18:19:32 +0530

    drm/i915: Add hdmi level shifter feature support
    BZ: 144862
    
    The EV has new recomendation for HDMI Pre-Emp and vswing settings to
    save more power. The EV has provided new settings for each HDMI
    Pre-Emp, Vswing configuration. Implement these settings in this
    patch.
    
    Please refer to VLV2A0_DP_eDP_HDMI_DPIO_driver_vbios_notes_10.docx
    document availabe at VLV HAS shared folder for more details.
    
    The value for VLV_DDI0_TX_3 + DPIO_TX_4 register is not documented in
    VLV2A0_DP_eDP_HDMI_DPIO_driver_vbios_notes_10.docx. The values were
    got offline.
    
    The link to the RCR for this issue is https://vthsd.fm.intel.com/hsd/pcgsw
    /default.aspx#rcr/default.aspx?rcr_id=1024325
    
    Change-Id: Ia6fe0e04c09e133b058e392cfe935e034f35d995
    Signed-off-by: Yogesh Mohan Marimuthu <yogesh.mohan.marimuthu@intel.com>
    Signed-off-by: Victor Liao <victor.liao@intel.com>
    Reviewed-on: http://android.intel.com:8080/138593
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Sun, Hang L <hang.l.sun@intel.com>
    Tested-by: Sun, Hang L <hang.l.sun@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -3800,7 +3800,97 @@ static void intel_crtc_dpms_overlay(struct intel_crtc *intel_crtc, bool enable)
 	 */
 }
 
-static void vlv_pll_enable_reset(struct drm_crtc *crtc)
+static void vlv_set_hdmi_level_shifter_settings(struct drm_crtc *crtc,
+				struct drm_display_mode *adjusted_mode)
+{
+	struct drm_device *dev = crtc->dev;
+	struct drm_i915_private *dev_priv = dev->dev_private;
+	u32 de_emp_reg_val = 0;
+	u32 transcale_reg_val = 0;
+	u32 pre_emp_reg_val = 0;
+	u32 clk_de_emp_reg_val = 0;
+	u8 pre_emp_vswing_setting = 0;
+
+	/*
+	 * FIXME: Need to get HDMI pre-emp, vswing settings from VBT.
+	 * definitions:
+	 * 0 = 1000MV_2DB
+	 * 1 = 1000MV_0DB
+	 * 2 = 800MV_0DB
+	 * 3 = 600MV_2DB
+	 * 4 = 600MV_0DB
+	 */
+	/*
+	 * As per EV requirement need to set 1000MV_0DB for pixel clock
+	 * < 74.250 Mhz
+	 */
+	if (adjusted_mode->clock < 74250)
+		pre_emp_vswing_setting = 1;
+	else
+		/* Customize the below variable as per customer requirement */
+		pre_emp_vswing_setting = 0;
+
+	/*FIXME: The Application notes doesn't have pcs_ctrl_reg_val for
+	 * settings 1V_0DB, 0.8V_0DB, 0.6V_0DB. The pcs_ctrl_reg_val value
+	 * for these is selected from higher demp_vswing settings for which
+	 * the data is given.
+	 */
+	switch (pre_emp_vswing_setting) {
+	case 0:
+		de_emp_reg_val = 0x2B245F5F;
+		transcale_reg_val = 0x5578B83A;
+		clk_de_emp_reg_val = 0x2B247878;
+		pre_emp_reg_val = 0x2000;
+		break;
+	case 1:
+		de_emp_reg_val = 0x2B405555;
+		transcale_reg_val = 0x5580A03A;
+		clk_de_emp_reg_val = 0x2B405555;
+		pre_emp_reg_val = 0x4000;
+		break;
+	case 2:
+		de_emp_reg_val = 0x2B245555;
+		transcale_reg_val = 0x5560B83A;
+		clk_de_emp_reg_val = 0x2B245555;
+		pre_emp_reg_val = 0x4000;
+		break;
+	case 3:
+		de_emp_reg_val = 0x2B406262;
+		transcale_reg_val = 0x5560B83A;
+		clk_de_emp_reg_val = 0x2B407878;
+		pre_emp_reg_val = 0x2000;
+		break;
+	case 4:
+		de_emp_reg_val = 0x2B404040;
+		transcale_reg_val = 0x5548B83A;
+		clk_de_emp_reg_val = 0x2B404040;
+		pre_emp_reg_val = 0x4000;
+		break;
+	default:
+		DRM_ERROR("Incorrect pre-emp vswing setting\n");
+		break;
+	}
+
+
+	/* FIXME: Enable support for HDMI-C.
+	 * Need to fix this in many other places too.
+	 */
+	intel_dpio_write(dev_priv, VLV_DDI0_GRP_TX + DPIO_TX_5, 0x00000000);
+	intel_dpio_write(dev_priv, VLV_DDI0_GRP_TX + DPIO_TX_4,
+							de_emp_reg_val);
+	intel_dpio_write(dev_priv, VLV_DDI0_GRP_TX + DPIO_TX_2,
+							transcale_reg_val);
+	intel_dpio_write(dev_priv, VLV_DDI0_GRP_TX + DPIO_TX_3, 0x0c782040);
+	intel_dpio_write(dev_priv, VLV_DDI0_TX_3 + DPIO_TX_4,
+							clk_de_emp_reg_val);
+	intel_dpio_write(dev_priv, VLV_DDI0_GRP_PCS + DPIO_PCS_B, 0x00030000);
+	intel_dpio_write(dev_priv, VLV_DDI0_GRP_PCS + DPIO_PCS_9,
+							pre_emp_reg_val);
+	intel_dpio_write(dev_priv, VLV_DDI0_GRP_TX + DPIO_TX_5, 0x80000000);
+}
+
+static void vlv_pll_enable_reset(struct drm_crtc *crtc,
+				struct drm_display_mode *adjusted_mode)
 {
 	struct drm_device *dev = crtc->dev;
 	struct drm_i915_private *dev_priv = dev->dev_private;
@@ -3815,14 +3905,7 @@ static void vlv_pll_enable_reset(struct drm_crtc *crtc)
 		val |= (1<<20);
 		intel_dpio_write(dev_priv, DPIO_DATA_CHANNEL1, val);
 
-		intel_dpio_write(dev_priv, 0x8294, 0x00000000);
-		intel_dpio_write(dev_priv, 0x8290, 0x2b245f5f);
-		intel_dpio_write(dev_priv, 0x8288, 0x5578b83a);
-		intel_dpio_write(dev_priv, 0x828c, 0x0c782040);
-		intel_dpio_write(dev_priv, 0x690, 0x2b247878);
-		intel_dpio_write(dev_priv, 0x822c, 0x00030000);
-		intel_dpio_write(dev_priv, 0x8224, 0x00002000);
-		intel_dpio_write(dev_priv, 0x8294, 0x80000000);
+		vlv_set_hdmi_level_shifter_settings(crtc, adjusted_mode);
 
 		intel_dpio_write(dev_priv, 0x8238, 0x00760018);
 		intel_dpio_write(dev_priv, 0x825c, 0x00400888);
@@ -4722,7 +4805,7 @@ static void vlv_update_pll(struct drm_crtc *crtc,
 		POSTING_READ(DPLL_MD(pipe));
 	}
 
-	vlv_pll_enable_reset(crtc);
+	vlv_pll_enable_reset(crtc, adjusted_mode);
 
 	if (intel_pipe_has_type(crtc, INTEL_OUTPUT_DISPLAYPORT) ||
 		intel_pipe_has_type(crtc, INTEL_OUTPUT_EDP))
