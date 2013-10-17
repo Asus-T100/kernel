e626fe4fd54db7ac89b97c81420761462871a154
yucheng_hung <yucheng_hung@asus.com>
2013-10-17 20:46:38 +0800

    Revert "[REVERTME]pci: WORKAROUND for ISP power-up/down through ACPI"
    This reverts commit 69f43218d20e68e4d7cfb83a6c705e6ab59a704b.

--- drivers/pci/pci-acpi.c
+++ drivers/pci/pci-acpi.c
@@ -18,7 +18,6 @@
 #include <linux/pci-acpi.h>
 #include <linux/pm_runtime.h>
 #include <linux/pm_qos.h>
-#include <linux/intel_mid_pm.h>
 #include "pci.h"
 
 /**
@@ -133,10 +132,6 @@ acpi_status pci_acpi_remove_pm_notifier(struct acpi_device *dev)
 static pci_power_t acpi_pci_choose_state(struct pci_dev *pdev)
 {
 	int acpi_state, d_max;
-	pci_power_t target_state = byt_pci_choose_state(pdev);
-
-	if (target_state != PCI_POWER_ERROR)
-		return target_state;
 
 	if (pdev->no_d3cold)
 		d_max = ACPI_STATE_D3_HOT;
@@ -165,9 +160,6 @@ static bool acpi_pci_power_manageable(struct pci_dev *dev)
 {
 	acpi_handle handle = DEVICE_ACPI_HANDLE(&dev->dev);
 
-	if (byt_pci_power_manageable(dev))
-		return true;
-
 	return handle ? acpi_bus_power_manageable(handle) : false;
 }
 
@@ -182,11 +174,7 @@ static int acpi_pci_set_power_state(struct pci_dev *dev, pci_power_t state)
 		[PCI_D3hot] = ACPI_STATE_D3,
 		[PCI_D3cold] = ACPI_STATE_D3
 	};
-	int error;
-
-	error = byt_pci_set_power_state(dev, state);
-	if (!error)
-		return 0;
+	int error = -EINVAL;
 
 	/* If the ACPI device has _EJ0, ignore the device */
 	if (!handle || ACPI_SUCCESS(acpi_get_handle(handle, "_EJ0", &tmp)))
