b916e05518d1653394f6bda8a1f117864532b354
Adrian Hunter <adrian.hunter@intel.com>
2015-03-24 15:40:38 +0200

    mmc: sdhci: Support maximum DMA latency request via PM QOS
    Add support for setting a maximum DMA latency via the
    PM QOS framework.
    
    Drivers can set host->dma_latency to the desired value
    otherwise the initial value (PM_QOS_DEFAULT_VALUE)
    will result in no PM QOS request being added.
    
    It may be that there isn't time between consecutive
    I/O requests to reach deeper C-states. To address
    that the driver can set host->lat_cancel_delay which
    is the delay before cancelling the DMA latency request
    when it is known that there is another request on
    the way.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -19,6 +19,7 @@
 #include <linux/io.h>
 
 #include <linux/mmc/host.h>
+#include <linux/pm_qos.h>
 
 /*
  * Controller registers
@@ -432,6 +433,12 @@ struct sdhci_host {
 	struct mmc_host *mmc;	/* MMC structure */
 	struct mmc_host_ops mmc_host_ops;	/* MMC host ops */
 	u64 dma_mask;		/* custom DMA mask */
+
+	struct pm_qos_request pm_qos_req;
+	int dma_latency;
+	int lat_cancel_delay;
+	int consecutive_req;
+	bool pm_qos_set;
 
 #if defined(CONFIG_LEDS_CLASS) || defined(CONFIG_LEDS_CLASS_MODULE)
 	struct led_classdev led;	/* LED control */
