8b66eca0e6bc7ea662a2f834ccf219f2fd598570
pinkflozd <lk@varjanta.com>
2015-08-02 22:34:12 +0200

    NOATIME and NODIRATIME as default

--- fs/namespace.c
+++ fs/namespace.c
@@ -2739,9 +2739,10 @@ long do_mount(const char *dev_name, const char __user *dir_name,
 	if (retval)
 		goto dput_out;
 
-	/* Default to relatime unless overriden */
+	/* Default to relatime unless overriden 
 	if (!(flags & MS_NOATIME))
 		mnt_flags |= MNT_RELATIME;
+	*/
 
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
@@ -2750,9 +2751,9 @@ long do_mount(const char *dev_name, const char __user *dir_name,
 		mnt_flags |= MNT_NODEV;
 	if (flags & MS_NOEXEC)
 		mnt_flags |= MNT_NOEXEC;
-	if (flags & MS_NOATIME)
+	//if (flags & MS_NOATIME)
 		mnt_flags |= MNT_NOATIME;
-	if (flags & MS_NODIRATIME)
+	//if (flags & MS_NODIRATIME)
 		mnt_flags |= MNT_NODIRATIME;
 	if (flags & MS_STRICTATIME)
 		mnt_flags &= ~(MNT_RELATIME | MNT_NOATIME);
