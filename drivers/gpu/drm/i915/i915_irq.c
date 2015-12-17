c6a08a2212e6f53f5db74f4d5cd2fb29eeb54396
pinkflozd <lk@varjanta.com>
2015-12-17 20:12:58 +0100

    Revert "enable the legacy turbo back"
    This reverts commit 1b01af93a059176ea4c95fafc011f32b5fdc830d.

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -4131,7 +4131,12 @@ void intel_irq_init(struct drm_i915_private *dev_priv)
 	INIT_WORK(&dev_priv->rps.work, gen6_pm_rps_work);
 	INIT_WORK(&dev_priv->l3_parity.error_work, ivybridge_parity_work);
 
-	dev_priv->pm_rps_events = GEN6_PM_RPS_EVENTS;
+	/* Let's track the enabled rps events */
+	if (IS_VALLEYVIEW(dev_priv) && !IS_CHERRYVIEW(dev_priv))
+		/* WaGsvRC0ResidencyMethod:vlv */
+		dev_priv->pm_rps_events = GEN6_PM_RP_DOWN_EI_EXPIRED | GEN6_PM_RP_UP_EI_EXPIRED;
+	else
+		dev_priv->pm_rps_events = GEN6_PM_RPS_EVENTS;
 
 	INIT_DELAYED_WORK(&dev_priv->gpu_error.hangcheck_work,
 			  i915_hangcheck_elapsed);
