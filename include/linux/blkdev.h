15eb9e6f1c9f2d2285d55918ccecfd39edbe765c
Steven Barrett <damentz@liquorix.net>
2016-03-15 18:17:03 +0100

    ZEN: Implement zen-tune v4.5
    Zen-tune used to use a low background and dirty ratio so we would begin
    writeback as soon as possible. However, even under 'regular' heavy load,
    the system would stall regardless of how low the writeback numbers were.
    
    The Linux kernel, when under heavy IO load, tends to wait for all requests
    to the scheduler to complete before loading the next group. This causes
    lengthy stalls that make the system unusable interactively. As you
    increase BLKDEV_MAX_RQ, the system stalls increase. Reducing this number
    to as low as 16 doesn't impact throughput _that_ much, but mostly fixes
    all stalls, such as the UI and mouse freezing.
    
    Use a reduced NCQ queue depth to prevent the storage device from causing
    latency spikes during heavy IO. This has the side effect of reducing
    throughput only a bit, but significantly reducing the chance that entire
    system stalls occur.

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -39,7 +39,11 @@ struct blk_flush_queue;
 struct pr_ops;
 
 #define BLKDEV_MIN_RQ	4
+#ifdef CONFIG_ZEN_INTERACTIVE
+#define BLKDEV_MAX_RQ	16
+#else
 #define BLKDEV_MAX_RQ	128	/* Default maximum */
+#endif
 
 /*
  * Maximum number of blkcg policies allowed to be registered concurrently.
