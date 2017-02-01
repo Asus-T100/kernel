9542774e73a2542d1de848fa953879b53290146c
Luka Karinja <luka.karinja@gmail.com>
2017-02-01 12:47:01 +0100

    Revert "SiSTouchscreen3"
    This reverts commit 5ab61ba62136cf430bfce7d50f0646d13d0909df.

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -208,11 +208,7 @@ static struct mt_class mt_classes[] = {
 		.quirks = MT_QUIRK_ALWAYS_VALID |
 			MT_QUIRK_IGNORE_DUPLICATES |
 			MT_QUIRK_HOVERING |
-			MT_QUIRK_CONTACT_CNT_ACCURATE |
-			MT_QUIRK_NOT_SEEN_MEANS_UP,
-		.export_all_inputs = true,
-		.sn_pressure = 32,
-		.maxcontacts = 10 },
+			MT_QUIRK_CONTACT_CNT_ACCURATE },
 	{ .name = MT_CLS_EXPORT_ALL_INPUTS,
 		.quirks = MT_QUIRK_ALWAYS_VALID |
 			MT_QUIRK_CONTACT_CNT_ACCURATE,
@@ -622,10 +618,6 @@ static void mt_complete_slot(struct mt_device *td, struct input_dev *input)
 	    td->num_received >= td->num_expected)
 		return;
 
-	if (td->curdata.x == 0xffff && td->curdata.x == td->curdata.y &&
-	    td->curdata.w == 0xffff && td->curdata.w == td->curdata.h)
-		goto inc_num_received;
-
 	if (td->curvalid || (td->mtclass.quirks & MT_QUIRK_ALWAYS_VALID)) {
 		int active;
 		int slotnum = mt_compute_slot(td, input);
@@ -669,7 +661,6 @@ static void mt_complete_slot(struct mt_device *td, struct input_dev *input)
 		}
 	}
 
-inc_num_received:
 	td->num_received++;
 }
 
@@ -1142,8 +1133,7 @@ static int mt_probe(struct hid_device *hdev, const struct hid_device_id *id)
 		return -ENOMEM;
 	}
 
-	if (id->vendor == HID_ANY_ID && id->product == HID_ANY_ID &&
-		id->group != HID_GROUP_MULTITOUCH_WIN_8)
+	if (id->vendor == HID_ANY_ID && id->product == HID_ANY_ID)
 		td->serial_maybe = true;
 
 	ret = hid_parse(hdev);
